716c4f7ba65a8f5d445fa0008f3a4717b8dba90f
Andrew Adcock <andrewadcock@gmail.com>
2020-06-01 16:06:21 -0400

    Rebuild Table of Contents using VerticalNav component

--- react/src/components/layout/TOC.js
+++ react/src/components/layout/TOC.js
@@ -1,39 +1,88 @@
 import React from "react";
+import { Link, VerticalNav } from "@cmsgov/design-system-core";
 
 const TOC = () => {
   return (
     <div className="toc">
-      <ul>
-        <li>Overview</li>
-        <li>Section 1: Snapshot of CHIP Programs</li>
-        <li>
-          Section 2: Program's Performance Measurment
-          <ul>
-            <li>Section 2A: Enrollment and Uninsured Data</li>
-            <li>
-              <a href="/2b">
-                Section 2B: State Strategic Objectives and Performance Goals
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          Section 3: Assessment of State Plan and Program Operation
-          <ul>
-            <li>Section 3A: Outreach</li>
-            <li>Section 3B: Substitution of Coverage (Crowd-out)</li>
-            <li>Section 3C: Eligibility</li>
-            <li>Section 3D: Cost Sharing</li>
-            <li>Section 3E: Employer Sponsored Insurance Program</li>
-            <li>Section 3F: Program Integrity</li>
-            <li>Section 3G: Dental Benefits</li>
-            <li>Section 3H: CHIPRA CAHPS Requirement</li>
-          </ul>
-        </li>
-        <li>Section 4: Program Financing</li>
-        <li>Section 5: Challenges and Accomplishments</li>
-        <li>Certify and Submit</li>
-      </ul>
+      <VerticalNav
+        selectedId="toc"
+        items={[
+          {
+            label: "Overview",
+            url: "javascript:void(0);",
+            id: "overview",
+          },
+          {
+            label: "Section 1: Snapshot of CHIP Programs",
+            url: "javascript:void(0);",
+          },
+          {
+            label: "Section 2: Program's Performance Measurement",
+            select: true,
+            items: [
+              {
+                label: "Section 2A: Enrollment and Uninsured Data",
+                url: "javascript:void(0);",
+              },
+              {
+                label:
+                  "Section 2B: State Strategic Objectives and Performance Goals",
+                url: "/2b",
+              },
+            ],
+          },
+          {
+            label: "Section 3: Assessment of State Plan and Program Operation",
+            select: true,
+            items: [
+              {
+                label: "Section 3A: Outreach",
+                url: "javascript:void(0);",
+              },
+              {
+                label: "Section 3B: Substitution of Coverage (Crowd-out)",
+                url: "javascript:void(0);",
+              },
+              {
+                label: "Section 3C: Eligibility",
+                url: "/3c",
+              },
+              {
+                label: "Section 3D: Cost Sharing",
+                url: "javascript:void(0);",
+              },
+              {
+                label: "Section 3E: Employer Sponsored Insurance Program",
+                url: "javascript:void(0);",
+              },
+              {
+                label: "Section 3F: Program Integrity",
+                url: "javascript:void(0);",
+              },
+              {
+                label: "Section 3G: Dental Benefits",
+                url: "javascript:void(0);",
+              },
+              {
+                label: "Section 3H: CHIPRA CAHPS Requirement",
+                url: "javascript:void(0);",
+              },
+            ],
+          },
+          {
+            label: "Section 4: Program Financing",
+            url: "javascript:void(0);",
+          },
+          {
+            label: "Section 5: Challenges and Accomplishments",
+            url: "javascript:void(0);",
+          },
+          {
+            label: "Certify and Submit",
+            url: "javascript:void(0);",
+          },
+        ]}
+      />
     </div>
   );
 };
