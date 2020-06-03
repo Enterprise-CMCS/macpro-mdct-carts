7992a80ef2fa62c1ec7c0f719b0b271140cb783d
Mike Dial <mdial@collabralink.com>
2020-06-03 09:26:21 -0400

    Introduce multi stage builds to significantly reduce image size.  Serve our compiled static code from an alpine nginx image to improve performance

---
+++ app/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sh destroy.sh
+
+docker-compose build
+docker-compose up -d
--- app/react/src/App.js
+++ app/react/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./App.css";
+import logo from './logo.svg';
 import Routes from "./reactRouter";
 
 function App() {
   return (
     <div className="App">
       <Routes />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          API_URL: {window._env_.API_URL}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
