0ea451bc0886df8b92b063d775533cb46f780a93
Andrew Adcock <andrewadcock@gmail.com>
2020-06-16 11:12:01 -0400

    Automatically close nav when clicking elsewhere on screen

--- app/react/src/components/layout/Header.js
+++ app/react/src/components/layout/Header.js
@@ -12,6 +12,16 @@ class Header extends Component {
     e.preventDefault();
     document.getElementById("menu-block").classList.toggle("open");
     document.getElementById("nav-user").classList.toggle("open");
+    let root = document.getElementById("root");
+
+    root.addEventListener(
+      "click",
+      () => {
+        document.getElementById("menu-block").classList.toggle("open");
+        document.getElementById("nav-user").classList.toggle("open");
+      },
+      false
+    );
   }
 
   render() {
