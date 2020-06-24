fb036f40a613bfba4934a466483cdd74c1a884ce
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2020-06-24 10:28:56 -0400

    tables and conditional questions based on percent changes added

--- app/react/src/components/sections/section2a/Section2A.js
+++ app/react/src/components/sections/section2a/Section2A.js
@@ -48,6 +48,7 @@ class Section2a extends Component {
       t2_y5_m2: ".2",
       p2_q1: "",
       p2_q2: "",
+      p2_q2__a: "",
       p2_q3: "",
       p2_q3__a: "",
       p2_q3__b: "",
@@ -131,22 +132,32 @@ class Section2a extends Component {
                     </table>
                     <p></p>
 
-                    {/* Show if  M-CHIP or S-CHIP percent change(s) are more than a 10% change (increase or decrease) */}
-                    <div className="question-container">
-                      <div className="question">
-                        1. What are some possible reasons why your state had more than a 10% change in enrollment numbers? 
-                      </div>
-                      <TextField
-                        hint="Maximum 7,500 characters"
-                        label=""
-                        multiline
-                        rows="6"
-                        name="p2_q2"
-                        value={this.state.p1_q1}
-                      />
+                    <div>
+                      {this.calcPercentChange(this.state.t1_s1,this.state.t1_s2) >= 10 || this.calcPercentChange(this.state.t1_s1,this.state.t1_s2) <= -10 
+                        || this.calcPercentChange(this.state.t1_m1,this.state.t1_m2) >= 10 || this.calcPercentChange(this.state.t1_m1,this.state.t1_m2) <= -10 ? (
+                        <div className="conditional">
+                          {/* Show if  M-CHIP or S-CHIP percent change(s) are more than a 10% change (increase or decrease) */}
+                          <div className="question-container">
+                            <div className="question">
+                              1. What are some possible reasons why your state had more than a 10% change in enrollment?
+                            </div>
+                            <TextField
+                              hint="Maximum 7,500 characters"
+                              label=""
+                              multiline
+                              rows="6"
+                              name="p1_q1"
+                              value={this.state.p1_q1}
+                            />
+                          </div>
+                        </div>
+                      ) : (
+                        <div>
+                          {/* Show if M-CHIP & S-CHIP percent changes are less than a 10% change */}  
+                          <p>Since your percent change didn't exceed 10%, you can skip to the next question.</p>
+                        </div>
+                      )}
                     </div>
-                    {/* Show if M-CHIP & S-CHIP percent changes are less than a 10% change */}
-                    <div><p>Since your percent change didn't exceed 10%, you can skip to the next question.</p></div>
 
                     <h3 className="part-header">
                       Part 2: Number of Uninsured Children
@@ -214,37 +225,77 @@ class Section2a extends Component {
                       </tbody>
                     </table>
                     <p></p>
+                    <table className="t3-percent-change-table" class="ds-c-table">
+                      <tbody>
+                        <tr>
+                          <th scope="row">Percent change between 2019 and 2020</th>
+                          <td><NumberFormat displayType="text" decimalScale="1" value={this.calcPercentChange(this.state.t2_y4_n1,this.state.t2_y5_n1)}/>%</td>
+                        </tr>
+                      </tbody>
+                    </table>
 
-                    {/* Show if  M-CHIP or S-CHIP percent change(s) are more than a 10% change (increase or decrease) */}
-                    <div className="question-container">
-                      <div className="question">
-                        1. What are some possible reasons why your state had more than a 10% change in enrollment?
-                      </div>
-                      <TextField
-                        hint="Maximum 7,500 characters"
-                        label=""
-                        multiline
-                        rows="6"
-                        name="p2_q1"
-                        value={this.state.p2_q1}
-                      />
+                    <div>
+                      {this.calcPercentChange(this.state.t2_y4_n1,this.state.t2_y5_n1) >= 10 || this.calcPercentChange(this.state.t2_y4_n1,this.state.t2_y5_n1) <= -10 ? (
+                        <div className="conditional">
+                          {/* Show if Number of Estimated number of uninsured children percent change is more than a 10% change (increase or decrease) */}
+                          <div className="question-container">
+                            <div className="question">
+                              1. What are some possible reasons why your state had more than a 10% change in enrollment?
+                            </div>
+                            <TextField
+                              hint="Maximum 7,500 characters"
+                              label=""
+                              multiline
+                              rows="6"
+                              name="p2_q1"
+                              value={this.state.p2_q1}
+                            />
+                          </div>
+                        </div>
+                      ) : (
+                        <div>
+                          {/* Show if Number of Estimated number of uninsured children percent change is less than a 10% change */}  
+                          <p>Since your percent change didn't exceed 10%, you can skip to the next question.</p>
+                        </div>
+                      )}
                     </div>
-                    {/* Show if M-CHIP & S-CHIP percent changes are less than a 10% change */}
-                    <div><p>Since your percent change didn't exceed 10%, you can skip to the next question.</p></div>
 
                     <div className="question-container">
                       <div className="question">
-                        2. Are there any reasons why the American Community Surbey estimates wouldn't be an accurate representation of the number of uninsured children
+                        2. Are there any reasons why the American Community Survey estimates wouldn't be an accurate representation of the number of uninsured children
                         in your state?
                       </div>
-                      <TextField
-                        hint="Maximum 7,500 characters"
-                        label=""
-                        multiline
-                        rows="6"
-                        name="p2_q2"
-                        value={this.state.p2_q2}
-                      />
+                      <div id="p2_q2">
+                        <ChoiceList
+                          choices={[
+                            {
+                              label: "Yes",
+                              value: "yes",
+                            },
+                            {
+                              label: "No",
+                              value: "no",
+                            },
+                          ]}
+                          className="p2_q2"
+                          label=""
+                          name="p2_q2"
+                          onChange={this.setConditional}
+                        />
+                        {this.state.p2_q2 === "yes" ? (
+                              <div className="conditional">
+                                <TextField
+                                  label="a) What are some reasons why the American Community Survey estimates might not be accurate?"
+                                  multiline
+                                  name="p2_q2__a"
+                                  rows="6"
+                                  value={this.state.p2_q2__a}
+                                />
+                              </div>
+                            ) : (
+                              ""
+                            )}
+                      </div>
                     </div>
 
                     <div className="question-container">
