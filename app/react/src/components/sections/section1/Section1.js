7eaca3b777470a16f6a7f7ab425bfe38eed2bb87
Andrew Adcock <andrewadcock@gmail.com>
2020-06-24 10:15:33 -0400

    Add section1 to the router; update navigation buttons; create section1

--- app/react/src/components/sections/section1/Section1.js
+++ app/react/src/components/sections/section1/Section1.js
@@ -20,7 +20,66 @@ class Section1 extends Component {
 
             <div className="main ds-l-col--9">
               <PageInfo />
-              <div className="section-content">Section 1</div>
+              <div className="section-content"><Tabs>
+                  <TabPanel id="tab-form" tab="Section 3C: Eligibility">
+                    <form>
+                      <div>
+                        <h3 className="part-header">
+                          Part 1: Eligibility Renewal and Retention
+                        </h3>
+                        <div className="question-container">
+                          <FillForm
+                            name="p1_q1"
+                            title={this.state.fillFormTitle}
+                            onClick={this.loadAnswers}
+                          />
+                          <div className="question">
+                            1. Do you have authority in your CHIP state plan to
+                            provide for presumptive eligibility, and have you
+                            implemented this?
+                          </div>
+                          <div id="p1_q1">
+                            <ChoiceList
+                              choices={[
+                                {
+                                  label: "Yes",
+                                  value: "yes",
+                                },
+                                {
+                                  label: "No",
+                                  value: "no",
+                                },
+                              ]}
+                              className="p1_q1"
+                              label=""
+                              name="p1_q1"
+                              onChange={this.setConditional}
+                              hint="Note: This question may not apply to Medicaid Expansion states."
+                            />
+                            {this.state.p1_q1 === "yes" ? (
+                              <div className="conditional">
+                                <TextField
+                                  label="What percentage of children are presumptively enrolled in CHIP pending a full eligibility determination?"
+                                  multiline
+                                  name="p1_q1__b"
+                                  rows="6"
+                                  value={this.state.p1_q1__b}
+                                />
+                                <TextField
+                                  hint="Maximum 7,500 characters"
+                                  label="Of those children who are presumptively enrolled, what percentage are determined fully eligible and enrolled in the program?"
+                                  multiline
+                                  name="p1_q1__c"
+                                  rows="6"
+                                  value={this.state.p1_q1__c}
+                                />
+                              </div>
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                        </div>
+                      </div>
 
               <div className="nav-buttons">
                 <NavigationButton
