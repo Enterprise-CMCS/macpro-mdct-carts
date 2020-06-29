1e29e1895bcb8460f08b64556366f09392855c4a
WAQF <awoodbury@collabralink.com>
2020-06-29 14:59:26 -0400

    comments added to helper function

--- app/react/src/components/Utils/helperFunctions.js
+++ app/react/src/components/Utils/helperFunctions.js
@@ -1,4 +1,5 @@
-// This function extracts just the unique ID from all objectives & goals, stopping at the underscore
+// This function extracts just the unique ID
+// from all objectives & goals, stopping at the underscore
 const sliceId = (id) => {
   let idString = id.toString();
   let num = idString.slice(idString.indexOf("_", idString.length - 1));
