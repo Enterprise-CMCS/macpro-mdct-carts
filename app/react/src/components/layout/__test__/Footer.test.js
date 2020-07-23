ffd31520c22830315f533d0461ece6d4c42ea28c
WAQF <awoodbury@collabralink.com>
2020-07-23 12:44:29 -0400

    rounding goal2b percentage to 2 decimal places, added TOC test file

--- app/react/src/components/layout/__test__/Footer.test.js
+++ app/react/src/components/layout/__test__/Footer.test.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Footer from "../Footer";
 
-import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
+import { shallow, mount } from "enzyme";
 
 describe("Footer Component", () => {
-  test("renders", () => {
-    const component = shallow(<Footer />);
+  const component = shallow(<Footer />);
+
+  it("renders", () => {
     expect(component.exists()).toBe(true);
+  });
+
+  it("has appropriate classnames", () => {
+    expect(component.exists(".footer")).toBe(true);
+  });
+
+  it("includes federal website disclaimer", () => {
+    expect(
+      component.find('[href="mailto:cartshelp@cms.hhs.gov"]').hasClass("help")
+    ).toBe(true);
+  });
+});
+
+describe("Footer Component, snapshot testing", () => {
+  const snapShot = renderer.create(<Footer />);
+
+  it("should match the snapshot", () => {
+    expect(snapShot.toJSON()).toMatchSnapshot();
   });
 });
--- app/react/src/components/layout/__test__/Header.test.js
+++ app/react/src/components/layout/__test__/Header.test.js
@@ -3,51 +3,55 @@ import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
 import { shallow, mount } from "enzyme";
 
-import configureStore from "redux-mock-store";
+import configureMockStore from "redux-mock-store";
 
 import Header from "../Header";
 
-const mockStore = configureStore([]);
+const mockStore = configureMockStore();
 
-describe("Header Component", () => {
-  // beforeEach(() => {
-
-  // });
-
-  it("should render with given store from redux", () => {
-    let store;
-    let component;
-    store = mockStore({
+describe("Header Component, enzyme testing", () => {
+  const store = mockStore({
+    stateUser: {
       currentUser: {
         role: "admin",
         state: { id: "NY", name: "New York" },
-        username: "grace.lafayette@state.gov",
+        username: "karen.dalton@state.gov",
       },
-    });
-
-    // store.dispatch = jest.fn();
-
-    component = mount(
-      <Provider store={store}>
-        <Header />
-      </Provider>
-    );
-    expect(component.toJSON()).toMatchSnapshot();
+    },
   });
 
-  it("opens and closes the dropdown when clicked ", () => {
-    // const component = shallow(<Header />);
-    // jest.spyOn(component, "toggleUserNav");
-    // component.find(".nav--dropdown__trigger").simulate("click");
-    // expect(component).toMatchSnapshot();
-    // expect(document.toggleUserNav).toHaveBeenCalled();
-    // expect(document.getElementById("menu-block").classList).toContain("open");
-    // expect(document.addEventListener.mock.calls[0][0]).toEqual("click");
-    // const handler = document.addEventListener.mock.calls[0][1];
-    // component.find(".nav--dropdown__trigger").simulate("click");
-    // expect(component.state().ariaExpanded).toBeFalsy();
-    // expect(document.removeEventListener).toHaveBeenCalled();
-    // expect(document.removeEventListener.mock.calls[0][0]).toEqual("click");
-    // expect(document.removeEventListener.mock.calls[0][1]).toEqual(handler);
+  const wrapper = mount(
+    <Provider store={store}>
+      <Header />
+    </Provider>
+  );
+  it("renders", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("renders with header classname", () => {
+    expect(wrapper.exists(".header")).toBe(true);
+  });
+});
+
+describe("Header Component, snapshot testing", () => {
+  const store = mockStore({
+    stateUser: {
+      currentUser: {
+        role: "admin",
+        state: { id: "NY", name: "New York" },
+        username: "karen.dalton@state.gov",
+      },
+    },
+  });
+
+  const snapShot = renderer.create(
+    <Provider store={store}>
+      <Header />
+    </Provider>
+  );
+
+  it("should render with given state from Redux store", () => {
+    expect(snapShot.toJSON()).toMatchSnapshot();
   });
 });
