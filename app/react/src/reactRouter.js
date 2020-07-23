65e8b5b231ec886716413cfbf25037a9dff8e4a7
Andrew Adcock <andrewadcock@gmail.com>
2020-07-23 15:04:44 -0400

    Add temporary route for section 1 that utilizes the fake api

--- app/react/src/reactRouter.js
+++ app/react/src/reactRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./components/sections/homepage/Homepage";
 import BasicInfo from "./components/sections/basicInfo/BasicInfo";
 import Section1 from "./components/sections/section1/Section1";
+import Section1Api from "./components/sections/section1api/Section1";
 import Section2a from "./components/sections/section2a/Section2A";
 import Section2b from "./components/sections/section2b/Section2B";
 import Section3a from "./components/sections/section3a/Section3A";
@@ -27,6 +28,7 @@ const Routes = () => (
           <Route exact path="/" component={Homepage} />
           <Route exact path="/basic-info" component={BasicInfo} />
           <Route exact path="/section1" component={Section1} />
+          <Route exact path="/section1-api" component={Section1Api} />
           <Route exact path="/section2/2a" component={Section2a} />
           <Route exact path="/section2/2b" component={Section2b} />
           <Route exact path="/section3/3a" component={Section3a} />
