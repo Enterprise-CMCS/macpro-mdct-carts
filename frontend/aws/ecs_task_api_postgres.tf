96ba1c724b39c155bf589f7bf336868fae9e8ad6
Mike Dial <mdial@collabralink.com>
2020-07-24 13:29:43 -0400

    networking typos

--- frontend/aws/ecs_task_api_postgres.tf
+++ frontend/aws/ecs_task_api_postgres.tf
@@ -60,8 +60,8 @@ resource "aws_security_group_rule" "api_postgres_egress" {
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  source_security_group_id = data.aws_ssm_parameter.api_postgres.value
-  security_group_id        = data.aws_ssm_parameter.postgres_security_group.value
+  source_security_group_id = data.aws_ssm_parameter.postgres_security_group.value
+  security_group_id        = aws_security_group.api_postgres.id
 }
 
 resource "aws_security_group_rule" "postgres_ingress_from_api_postgres" {
