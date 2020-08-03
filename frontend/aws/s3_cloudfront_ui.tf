5993c3d6b638d4638c0d807500fac12e20c86ce4
Ishmeal Baawuah <ibaawuah@ip-10-232-38-242.ec2.internal>
2020-08-03 13:31:55 -0400

    creating s3 bucket to host static website

--- frontend/aws/s3_cloudfront_ui.tf
+++ frontend/aws/s3_cloudfront_ui.tf
@@ -1,12 +1,1 @@
 
-resource "aws_s3_bucket" "ui" {
-  bucket = "carts-ui-${terraform.workspace}"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-
-  tags {
-    Name = "my-test-s3-terraform-bucket"
-  }
-}
