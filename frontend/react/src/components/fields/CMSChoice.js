812b772b8b36eecdc8498e9fc8b2234bd10cf802
Andrew Adcock <andrewadcock@gmail.com>
2020-08-05 11:29:21 -0400

    Add conditional logic

--- frontend/react/src/components/fields/CMSChoice.js
+++ frontend/react/src/components/fields/CMSChoice.js
@@ -1,87 +1,143 @@
-import React from "react";
+import React, { Component } from "react";
 import { Choice, TextField } from "@cmsgov/design-system-core";
 import FPL from "../layout/FPL";
 
-export const CMSChoice = (props) => {
-  // Determine if choice is checked
-  const isChecked = props.answer === props.value ? "checked" : null;
+class CMSChoice extends Component {
+  constructor(props) {
+    super(props);
 
-  // Create children
-  let fields = [];
-  if (props.conditional === props.value && props.children) {
-    props.children.map((item) => {
-      switch (item.type) {
-        case "text_long":
-          fields.push(
-            <>
-              <textarea
-                class="ds-c-field"
-                name={item.id}
-                value={item.answer.entry}
-                type="text"
-                name={item.id}
-                rows="6"
-              />
-            </>
-          );
-          break;
-        case "radio":
-          Object.entries(item.answer.options).map(function (key, index) {
-            const isCheckedChild =
-              key[1] === item.answer.entry ? "checked" : null;
-            return fields.push(
-              <>
-                {index === 0 ? (
-                  <legend className="ds-c-label">{item.label}</legend>
-                ) : null}
-                <Choice
-                  className="fpl-input"
-                  name={item.id}
-                  value={key[1]}
-                  type="radio"
-                  checked={isCheckedChild}
-                >
-                  {key[0]}
-                </Choice>
-              </>
-            );
-          });
-          break;
-        case "ranges":
-          return fields.push(<FPL label={item.label} />);
-          break;
-        case "money":
-          fields.push(
-            <>
-              <TextField
-                className="fpl-input"
-                label={item.label}
-                inputMode="currency"
-                mask="currency"
-                pattern="[0-9]*"
-                value={item.answer.entry}
-              />
-            </>
-          );
-          break;
-      }
-    });
+    this.sendData = this.sendData.bind(this);
   }
 
-  return (
-    <>
-      <Choice
-        class="ds-c-choice"
-        name={props.name}
-        value={props.value}
-        type={props.type}
-        checked={isChecked}
-        checkedChildren={
-          <div className="ds-c-choice__checkedChild">{fields}</div>
-        }
-      >
-        {props.label}
-      </Choice>
-    </>
-  );
-};
+  sendData = (evt) => {
+    this.props.onChange([evt.target.name, evt.target.value]);
+  };
+
+  render() {
+    // Determine if choice is checked
+    const isChecked = this.props.answer === this.props.value ? "checked" : null;
+
+    // Create children based on field type
+    let fields = [];
+
+    // If there is a conditional
+    if (this.props.conditional) {
+      // If there is a conditional value AND children are specified
+      if (
+        this.props.conditional.includes(this.props.value) &&
+        this.props.children
+      ) {
+        // Loop through subquestions
+        this.props.children.map((item) => {
+          // Set parent value to state, fallback to entered answer
+          let parentValue = this.props.valueFromParent
+            ? this.props.valueFromParent
+            : this.props.answer;
+
+          switch (item.type) {
+            case "text_long":
+              // Add to field to render array
+
+              if (
+                parentValue === item.context_data.conditional_display.toMatch
+              ) {
+                fields.push(
+                  <>
+                    <textarea
+                      class="ds-c-field"
+                      name={item.id}
+                      value={item.answer.entry}
+                      type="text"
+                      name={item.id}
+                      rows="6"
+                    />
+                  </>
+                );
+              }
+
+              break;
+            case "radio":
+              // Loop through available answers object
+              Object.entries(item.answer.options).map((key, index) => {
+                // If entry matches current answer, mark as checked
+                const isCheckedChild =
+                  key[1] === item.answer.entry ? "checked" : null;
+
+                // Add field to render array
+                if (
+                  parentValue === item.context_data.conditional_display.toMatch
+                ) {
+                  return fields.push(
+                    <>
+                      {index === 0 ? (
+                        <legend className="ds-c-label">{item.label}</legend>
+                      ) : null}
+                      {/* Output only matching answers */}
+
+                      <Choice
+                        className="fpl-input"
+                        name={item.id}
+                        value={key[1]}
+                        type="radio"
+                        checked={isCheckedChild}
+                      >
+                        {key[0]}
+                      </Choice>
+                    </>
+                  );
+                }
+              });
+              break;
+            case "ranges":
+              // Add field to render array
+              if (
+                parentValue === item.context_data.conditional_display.toMatch
+              ) {
+                return fields.push(<FPL label={item.label} />);
+              }
+              break;
+            case "money":
+              if (
+                parentValue === item.context_data.conditional_display.toMatch
+              ) {
+                fields.push(
+                  <>
+                    <TextField
+                      className="fpl-input"
+                      label={item.label}
+                      inputMode="currency"
+                      mask="currency"
+                      pattern="[0-9]*"
+                      value={item.answer.entry}
+                    />
+                  </>
+                );
+              }
+              break;
+          }
+        });
+      }
+    }
+
+    // Return Choice component after creating subquestion components
+    return (
+      <>
+        <Choice
+          class="ds-c-choice"
+          name={this.props.name}
+          value={this.props.value}
+          type={this.props.type}
+          checked={isChecked}
+          checkedChildren={
+            <div className="ds-c-choice__checkedChild">{fields}</div>
+          }
+          onChange={this.sendData}
+        >
+          {this.props.label}
+        </Choice>
+      </>
+    );
+  }
+}
+
+export default CMSChoice;
--- frontend/react/src/components/sections/section1api/questions/Questions1api.js
+++ frontend/react/src/components/sections/section1api/questions/Questions1api.js
@@ -3,87 +3,108 @@ import { connect } from "react-redux";
 import FPL from "../../../layout/FPL";
 import Data from "./../backend-json-section-1.json";
 import { Choice, ChoiceList, TextField } from "@cmsgov/design-system-core";
-import { CMSChoice } from "../../../fields/CMSChoice";
+import CMSChoice from "../../../fields/CMSChoice";
 
-function Questions1() {
-  // Get state program (temporary; will be set by API)
-  const stateProgram = "medicaid_exp_chip";
+class Questions1 extends Component {
+  constructor(props) {
+    super(props);
 
-  let createChoices;
-  return (
-    <form>
-      {/* Begin parsing through subsection */}
-      {Data.section.subsections.map((subsections) => (
-        <div className="section">
-          {/* Begin parsing through parts */}
-          {subsections.parts.map((part) => (
-            <div className="part">
-              <h3 className="part-title">{part.title}</h3>
+    this.state = { temp: "Here is the original stuff" };
 
-              {/* Determine if question should be shown */}
-              {!part.context_data.show_if_state_program_type_in.includes(
-                stateProgram
-              ) ? (
-                <div class="ds-c-alert ds-c-alert--hide-icon">
-                  <div class="ds-c-alert__body">
-                    <h3 class="ds-c-alert__heading">
-                      {part.context_data.skip_text}
-                    </h3>
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    console.log("Handle Change occured", evt);
+    this.setState({
+      temp: "This has been changed",
+      [evt[0]]: evt[1],
+    });
+  }
+
+  render() {
+    // Get state program (temporary; will be set by API)
+    const stateProgram = "combo"; // medicaid_exp_chip, separate_chip, combo
+
+    let valueFromParent;
+    return (
+      <form>
+        {/* Begin parsing through subsection */}
+        {Data.section.subsections.map((subsections) => (
+          <div className="section">
+            {/* Begin parsing through parts */}
+            {subsections.parts.map((part) => (
+              <div className="part">
+                <h3 className="part-title">{part.title}</h3>
+
+                {/* Determine if question should be shown */}
+                {!part.context_data.show_if_state_program_type_in.includes(
+                  stateProgram
+                ) ? (
+                  <div class="ds-c-alert ds-c-alert--hide-icon">
+                    <div class="ds-c-alert__body">
+                      <h3 class="ds-c-alert__heading">
+                        {part.context_data.skip_text}
+                      </h3>
+                    </div>
                   </div>
-                </div>
-              ) : (
-                part.questions.map((question) => (
-                  <div className="question">
-                    <fieldset className="ds-c-fieldset">
-                      {question.label}
-                      {question.type === "radio" || question.type === "checkbox"
-                        ? Object.entries(question.answer.options).map(function (
-                            key,
-                            index
-                          ) {
-                            return (
-                              <CMSChoice
-                                name={question.id}
-                                value={key[1]}
-                                label={key[0]}
-                                type={question.type}
-                                answer={question.answer.entry}
-                                conditional={question.conditional}
-                                children={question.questions}
-                              />
-                            );
-                          })
-                        : null}
+                ) : (
+                  part.questions.map((question) => (
+                    <div className="question">
+                      <fieldset className="ds-c-fieldset">
+                        {question.label}
 
-                      {/* If textarea */}
-                      {question.type === "text_long" ? (
-                        <div>
-                          <textarea
-                            class="ds-c-field"
-                            name={question.id}
-                            value={question.answer.entry}
-                            type="text"
-                            name={question.id}
-                            rows="6"
-                          />
-                        </div>
-                      ) : null}
-                      {/* If FPL Range */}
-                      {question.type === "ranges" ? (
-                        <div>
-                          <FPL label={question.label} />
-                        </div>
-                      ) : null}
-                    </fieldset>
-                  </div>
-                ))
-              )}
-            </div>
-          ))}
-        </div>
-      ))}
-    </form>
-  );
+                        {question.type === "radio" ||
+                        question.type === "checkbox"
+                          ? Object.entries(question.answer.options).map(
+                              (key, index) => {
+                                return (
+                                  <CMSChoice
+                                    name={question.id}
+                                    value={key[1]}
+                                    label={key[0]}
+                                    type={question.type}
+                                    answer={question.answer.entry}
+                                    conditional={question.conditional}
+                                    children={question.questions}
+                                    valueFromParent={this.state[question.id]}
+                                    onChange={this.handleChange}
+                                  />
+                                );
+                              }
+                            )
+                          : null}
+
+                        {/* If textarea */}
+                        {question.type === "text_long" ? (
+                          <div>
+                            <textarea
+                              class="ds-c-field"
+                              name={question.id}
+                              value={question.answer.entry}
+                              type="text"
+                              name={question.id}
+                              rows="6"
+                            />
+                          </div>
+                        ) : null}
+                        {/* If FPL Range */}
+                        {question.type === "ranges" ? (
+                          <div>
+                            <FPL label={question.label} />
+                          </div>
+                        ) : null}
+                      </fieldset>
+                    </div>
+                  ))
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </form>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
