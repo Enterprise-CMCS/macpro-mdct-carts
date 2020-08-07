a1fd47140b09525dcb33acc16379ed2cce475b3b
Claire Annan <claire.annan@gsa.gov>
2020-08-07 12:28:02 -0400

    Lowercase data var. Only components and classes are uc. Linted spacing.

--- frontend/react/src/components/sections/section1api/questions/Questions1api.js
+++ frontend/react/src/components/sections/section1api/questions/Questions1api.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import FPL from "../../../layout/FPL";
-import Data from "./../backend-json-section-1.json";
+import data from "./../backend-json-section-1.json";
 import { Choice, ChoiceList, TextField } from "@cmsgov/design-system-core";
 import CMSChoice from "../../../fields/CMSChoice";
 import CMSLegend from "../../../fields/CMSLegend";
@@ -31,7 +31,7 @@ class Questions1 extends Component {
     return (
       <form>
         {/* Begin parsing through subsection */}
-        {Data.section.subsections.map((subsections) => (
+        {data.section.subsections.map((subsections) => (
           <div className="section">
             {/* Begin parsing through parts */}
             {subsections.parts.map((part) => (
@@ -42,22 +42,22 @@ class Questions1 extends Component {
                 {!part.context_data.show_if_state_program_type_in.includes(
                   stateProgram
                 ) ? (
-                  <div class="ds-c-alert ds-c-alert--hide-icon">
-                    <div class="ds-c-alert__body">
-                      <h3 class="ds-c-alert__heading">
-                        {part.context_data.skip_text}
-                      </h3>
+                    <div class="ds-c-alert ds-c-alert--hide-icon">
+                      <div class="ds-c-alert__body">
+                        <h3 class="ds-c-alert__heading">
+                          {part.context_data.skip_text}
+                        </h3>
+                      </div>
                     </div>
-                  </div>
-                ) : (
-                  part.questions.map((question) => (
-                    <div className="question">
-                      <fieldset className="ds-c-fieldset">
-                        <CMSLegend label={question.label} id={question.id} />
+                  ) : (
+                    part.questions.map((question) => (
+                      <div className="question">
+                        <fieldset className="ds-c-fieldset">
+                          <CMSLegend label={question.label} id={question.id} />
 
-                        {question.type === "radio" ||
-                        question.type === "checkbox"
-                          ? Object.entries(question.answer.options).map(
+                          {question.type === "radio" ||
+                            question.type === "checkbox"
+                            ? Object.entries(question.answer.options).map(
                               (key, index) => {
                                 return (
                                   <CMSChoice
@@ -74,31 +74,31 @@ class Questions1 extends Component {
                                 );
                               }
                             )
-                          : null}
+                            : null}
 
-                        {/* If textarea */}
-                        {question.type === "text_long" ? (
-                          <div>
-                            <textarea
-                              class="ds-c-field"
-                              name={question.id}
-                              value={question.answer.entry}
-                              type="text"
-                              name={question.id}
-                              rows="6"
-                            />
-                          </div>
-                        ) : null}
-                        {/* If FPL Range */}
-                        {question.type === "ranges" ? (
-                          <div>
-                            <FPL label={question.label} />
-                          </div>
-                        ) : null}
-                      </fieldset>
-                    </div>
-                  ))
-                )}
+                          {/* If textarea */}
+                          {question.type === "text_long" ? (
+                            <div>
+                              <textarea
+                                class="ds-c-field"
+                                name={question.id}
+                                value={question.answer.entry}
+                                type="text"
+                                name={question.id}
+                                rows="6"
+                              />
+                            </div>
+                          ) : null}
+                          {/* If FPL Range */}
+                          {question.type === "ranges" ? (
+                            <div>
+                              <FPL label={question.label} />
+                            </div>
+                          ) : null}
+                        </fieldset>
+                      </div>
+                    ))
+                  )}
               </div>
             ))}
           </div>
