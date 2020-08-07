c17c5b8fce5e37ad28af42881f75847aca5e158d
Claire Annan <claire.annan@gsa.gov>
2020-08-07 18:28:15 -0400

    Remove short text. Add fieldset wrapper around radios.

--- frontend/react/src/components/fields/FormType.js
+++ frontend/react/src/components/fields/FormType.js
@@ -1,16 +1,13 @@
-import React from "react";
-import { Choice, ChoiceList, TextField } from "@cmsgov/design-system-core";
-import FPL from "./../layout/FPL";
-import CMSChoice from "./../fields/CMSChoice";
-import CMSLegend from "./../fields/CMSLegend";
+import React from "react"
+import { Choice, ChoiceList, TextField } from "@cmsgov/design-system-core"
+import FPL from "./../layout/FPL"
+import CMSChoice from "./../fields/CMSChoice"
+import CMSLegend from "./../fields/CMSLegend"
 
 
 //FIXME: create named export to avoid ??? 
 export const FormType = question => {
   switch (question.type) {
-    case "text_short":
-      return "some tiny text"
-      break
     case "text_long":
       return (
         <div>
@@ -27,23 +24,27 @@ export const FormType = question => {
       break
     case "radio":
       return (
-        Object.entries(question.answer.options).map(
-          (key, index) => {
-            return (
-              <CMSChoice
-                name={question.id}
-                value={key[1]}
-                label={key[0]}
-                type={question.type}
-                answer={question.answer.entry}
-                conditional={question.conditional}
-                children={question.questions}
-              // valueFromParent={this.state[question.id]}
-              // onChange={this.handleChange}
-              />
-            );
+        <fieldset className="ds-c-fieldset">
+          <CMSLegend label={question.label} id={question.id} />
+          {Object.entries(question.answer.options).map(
+            (key, index) => {
+              return (
+                <CMSChoice
+                  name={question.id}
+                  value={key[1]}
+                  label={key[0]}
+                  type={question.type}
+                  answer={question.answer.entry}
+                  conditional={question.conditional}
+                  children={question.questions}
+                // valueFromParent={this.state[question.id]}
+                // onChange={this.handleChange}
+                />
+              );
+            }
+          )
           }
-        )
+        </fieldset>
       )
       break
     case "ranges":
