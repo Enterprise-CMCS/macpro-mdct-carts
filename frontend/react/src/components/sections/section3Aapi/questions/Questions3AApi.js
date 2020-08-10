913a639e6a2be59c7555d830b45d126abdca1483
Andrew Adcock <andrewadcock@gmail.com>
2020-08-10 14:19:42 -0400

    Minor tweaks: Remove commented code, unused props, etc

--- frontend/react/src/components/sections/section3Aapi/questions/Questions3AApi.js
+++ frontend/react/src/components/sections/section3Aapi/questions/Questions3AApi.js
@@ -4,6 +4,7 @@ import Data from "../backend-json-section-3.json";
 import FPL from "../../../layout/FPL";
 import CMSChoice from "../../../fields/CMSChoice";
 import CMSLegend from "../../../fields/CMSLegend";
+import { TextField } from "@cmsgov/design-system-core";
 
 // Get subsection of DATA
 const sectionData = Data.section.subsections[0];
@@ -25,11 +26,9 @@ class Questions3AApi extends Component {
   render() {
     const stateProgram = this.props.programType; // medicaid_exp_chip, separate_chip, combo
 
-    let valueFromParent;
     return (
       <form>
         {/* Begin parsing through subsection */}
-        {/* {Data.section.subsections.map((subsections) => ( */}
         <div className="section">
           {/* Begin parsing through parts */}
           {sectionData.parts.map((part) => (
@@ -51,10 +50,11 @@ class Questions3AApi extends Component {
                                 label={key[0]}
                                 type={question.type}
                                 answer={question.answer.entry}
-                                conditional={question.conditional}
+                                conditional={question.context_data}
                                 children={question.questions}
                                 valueFromParent={this.state[question.id]}
                                 onChange={this.handleChange}
+                                key={index}
                               />
                             );
                           }
@@ -64,22 +64,21 @@ class Questions3AApi extends Component {
                     {/* If textarea */}
                     {question.type === "text_long" ? (
                       <div>
-                        <textarea
+                        <TextField
                           class="ds-c-field"
                           name={question.id}
-                          value={question.answer.entry}
+                          value={question.answer.entry || ""}
                           type="text"
                           name={question.id}
                           rows="6"
+                          multiline
                         />
                       </div>
                     ) : null}
                     {/* If FPL Range */}
-                    {question.type === "ranges" ? (
-                      <div>
-                        <FPL label={question.label} />
-                      </div>
-                    ) : null}
+                    {question.type === "ranges" && (
+                      <FPL label={question.label} />
+                    )}
                   </fieldset>
                 </div>
               ))}
