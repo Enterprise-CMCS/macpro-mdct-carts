c357c58cb0b7aabed79197e46197779e87ce3ec6
Seth Sacher <ssacher@collabralink.com>
2020-08-11 10:59:21 -0400

    Logout from Okta

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -4,23 +4,14 @@ import "./App.scss";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import Home from "./Home";
+import config from './auth-config';
 
 function App() {
-  const CALLBACK_PATH = '/implicit/callback';
-
-  const config = {
-    clientId: '0oa4juv4poiQ6nDB6297',
-    issuer: 'https://test.idp.idm.cms.gov/oauth2/aus4itu0feyg3RJTK297',
-    redirectUri: 'http://localhost:81/implicit/callback',
-    scopes: ['openid', 'profile', 'email'],
-    pkce: true
-  };
-
   return (
     <Router>
-      <Security {...config}>
+      <Security {...config.oidc}>
         <SecureRoute path="/" component={Home} />
-        <Route path={CALLBACK_PATH} component={LoginCallback} />
+        <Route path={config.callback} component={LoginCallback} />
       </Security>
     </Router>
   );
