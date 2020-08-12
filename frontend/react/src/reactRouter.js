1de20e031afe23160d1cab5d6e115e1ee2d0fdea
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2020-08-12 09:35:45 -0400

    rename with api tag to match other dynamic render sections

--- frontend/react/src/reactRouter.js
+++ frontend/react/src/reactRouter.js
@@ -9,7 +9,7 @@ import Section2a from "./components/sections/section2a/Section2A";
 import Section2b from "./components/sections/section2b/Section2B";
 import Section3a from "./components/sections/section3a/Section3A";
 import Section3c from "./components/sections/section3c/Section3C";
-import Section3d from "./components/sections/section3d/Section3D";
+import Section3dapi from "./components/sections/section3dapi/Section3D";
 import Review from "./components/review/Review";
 import Sidebar from "./components/layout/Sidebar";
 import test from "./components/test";
@@ -38,7 +38,7 @@ const Routes = () => (
           <Route exact path="/section2/2b" component={Section2b} />
           <Route exact path="/section3/3a" component={Section3a} />
           <Route exact path="/section3/3c" component={Section3c} />
-          <Route exact path="/section3/3d" component={Section3d} />
+          <Route exact path="/section3/3d-api" component={Section3dapi} />
           <Route path="/reports/:stateAbbrev/:year" component={Review} />
           <Route exact path="/test" component={test} />
         </Switch>
