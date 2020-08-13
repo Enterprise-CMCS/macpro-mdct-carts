d14fad70d70a7cca2217f4796d584f8339b3a758
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2020-08-13 12:59:58 -0400

    pulled in Section1 JSON so I can read the answer to P3Q8 & P4Q8 to determine whether or not to show Q7

--- frontend/react/src/components/sections/section3dapi/questions/Questions3D.js
+++ frontend/react/src/components/sections/section3dapi/questions/Questions3D.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Data from "../backend-json-section-3.json";
+import Section1 from "../backend-json-section-1.json";
 import FPL from "../../../layout/FPL";
 import CMSChoice from "../../../fields/CMSChoice";
 import CMSLegend from "../../../fields/CMSLegend";
@@ -8,6 +9,9 @@ import { TextField } from "@cmsgov/design-system-core";
 
 // Get subsection of DATA
 const sectionData = Data.section.subsections[3];
+
+var S1P3Q8 = Section1.subsections.parts.questions.id["2020-01-a-03-08"].answer.entry
+var S1P4Q8 = Section1.subsections.parts.questions.id["2020-01-a-04-08"].answer.entry
 
 class Questions3d extends Component {
   constructor(props) {
@@ -62,18 +66,23 @@ class Questions3d extends Component {
 
                     {/* If textarea */}
                     {question.type === "text_multiline" ? (
-                      <div>
-                        <TextField
-                          class="ds-c-field"
-                          name={question.id}
-                          value={question.answer.entry || ""}
-                          type="text"
-                          name={question.id}
-                          rows="6"
-                          multiline
-                        />
-                      </div>
+                      S1P3Q8 === "no" && S1P4Q8 === "no" || S1P3Q8 === null && S1P4Q8 === null ? (
+                        <p>{question.context_data.skip_text}</p>
+                        ) : (
+                        <div>
+                          <TextField
+                            class="ds-c-field"
+                            name={question.id}
+                            value={question.answer.entry || ""}
+                            type="text"
+                            name={question.id}
+                            rows="6"
+                            multiline
+                          />
+                        </div>
+                        )
                     ) : null}
+
                     {/* If FPL Range */}
                     {question.type === "ranges" && (
                       <FPL label={question.label} fieldLabels={question.answer.range_categories} />
