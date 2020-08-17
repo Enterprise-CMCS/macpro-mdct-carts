3d9fbd4d504002dc27bd92cf4f698e9e1dcdb5cf
Claire Annan <claire.annan@gsa.gov>
2020-08-16 20:21:54 -0400

    Add text change requested by Erin.

--- frontend/react/src/components/sections/SharedView.js
+++ frontend/react/src/components/sections/SharedView.js
@@ -3,6 +3,10 @@ import Section3C from './section3c/Section3C'
 import { SharedViewModal } from '../layout/SharedViewModal'
 import { Alert, Button } from "@cmsgov/design-system-core"
 
+/**
+ * TODO:
+ * @param {*} section An object containing the section to display on the temporary shared view.
+ */
 export const SharedView = _ => {
   const [modal, setModal] = useState(false)
   const showModal = _ => {
@@ -17,7 +21,7 @@ export const SharedView = _ => {
     <>
       <Alert heading="Someone has shared this section with you to edit" className="ds-u-margin-top--4">
         <p className="ds-c-alert__text ds-u-measure--wide">You’ll be able to view and edit this page for 7 days, unless they revoke your access before then.
-        All finished?</p>
+        </p>
         <Button className="ds-c-button ds-c-button--primary ds-c-button--small ds-u-margin-top--1 ds-u-margin-bottom--1" onClick={_ => showModal(true)}>I’m finished with this section</Button>
       </Alert>
       {modal && <SharedViewModal hide={hideModal} />}
