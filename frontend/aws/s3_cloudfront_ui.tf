ea736b78ec657e7f25d73f9ea7b849a60a4c2582
Ishmeal Baawuah <ibaawuah@ip-10-232-39-190.ec2.internal>
2020-08-17 14:22:51 -0400

    creating cloudfront for static distro

--- frontend/aws/s3_cloudfront_ui.tf
+++ frontend/aws/s3_cloudfront_ui.tf
@@ -1,8 +1,57 @@
 
-resource "aws_s3_bucket" "ui" {
-  bucket = "cartsui-cloudfront-origin"
-  acl = "private"
-  versioning {
-    enabled = true
+#resource "aws_s3_bucket" "ui" {
+#  bucket = "cartsui-cloudfrontorigin"
+#  acl = "private"
+#  versioning {
+#    enabled = true
+#  }
+#}
+
+
+
+# s3 Bucket with Website settings
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = "cartsui-origin-${terraform.workspace}"
+  acl = "public-read"
+  website {
+    index_document = "index.html"
+#    error_document = "error.html"
+  }
+
+# cloudfront distribution
+resource "aws_cloudfront_distribution" "site_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.site_bucket.bucket_domain_name}"
+    origin_id = "cartsui-origin-${terraform.workspace}"
+  }
+  enabled = true
+  aliases = ["${terraform.workspace}"]
+  price_class = "PriceClass_100"
+  default_root_object = "index.html"
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH",
+                      "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "cartsui-origin-${terraform.workspace}"
+     forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+    viewer_protocol_policy = "https-only"
+    min_ttl                = 0
+    default_ttl            = 1000
+    max_ttl                = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn = "var.acm_certificate_domain_ui"
+    ssl_support_method  = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016" # defaults wrong, set
   }
 }
