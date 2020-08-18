72a95e287260bddbd3a2e88e42b46d94079fd675
Andrew Adcock <andrewadcock@gmail.com>
2020-08-18 15:22:55 -0400

    Add 'add new' button for additional ranges

--- frontend/react/src/components/fields/CMSRanges.js
+++ frontend/react/src/components/fields/CMSRanges.js
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import CMSLegend from "../fields/CMSLegend";
 import CMSRange from "./CMSRange";
 
 class CMSRanges extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ranges: [
+        // {
+        //   id: 0,
+        //   component: <><CMSRange item={this.props.item} mask="currency" numeric index="0" /><CMSRange item={this.props.item} mask="currency" numeric index="1" /></>
+        // }
+      ],
+    };
+
+    this.newRanges = this.newRanges.bind(this);
+  }
+
+  componentDidMount() {
+    let newRanges = [];
+
+    this.props.item.answer.range_categories.map((range, index) => {
+      let header = index === 0 ? <h3>{this.props.item.answer.header}</h3> : null
+      newRanges.push({
+        component: <>{header}<CMSRange item={this.props.item} mask="currency" numeric index={index} /></>,
+      });
+
+    })
+
+
+
+    this.setState({
+      [`ranges`]: this.state.ranges.concat(newRanges),
+    });
+  }
+
+  /**
+   * Add new Range component to list (state) and update state
+   *
+   */
+  newRanges() {
+    let newRanges = [];
+    this.props.item.answer.range_categories.map((range, index) => {
+      let header = index === 0 ? <h3>{this.props.item.answer.header}</h3> : null
+      newRanges.push({
+        component: <>{header}<CMSRange item={this.props.item} mask="currency" numeric index={index} /></>,
+      });
+
+    })
+    this.setState({
+      [`ranges`]: this.state.ranges.concat(newRanges),
+    });
+  }
 
   render() {
     return (
@@ -13,9 +66,19 @@ class CMSRanges extends Component {
           id={this.props.item.id}
         />
 
-        {this.props.item.answer.range_categories.map((range, index) => (
-          <CMSRange item={this.props.item} mask="currency" numeric index={index} />
-        ))}
+        {this.state.ranges.map(
+          (input) => {
+            return input.component;
+          })}
+
+
+        <button
+          onClick={(e) => this.newRanges()}
+          type="button"
+          className="ds-c-button ds-c-button--primary"
+        >
+          Add another? <FontAwesomeIcon icon={faPlus} />
+        </button>
       </div>
     );
   }
