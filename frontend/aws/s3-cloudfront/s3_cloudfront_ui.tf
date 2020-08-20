6e0359e2ba0cb6f6e7ee5fd25fb219e4bba6cb8d
Ishmeal Baawuah <ibaawuah@ip-10-232-42-196.ec2.internal>
2020-08-20 13:43:27 -0400

    using default cert for cloudfront

--- frontend/aws/s3-cloudfront/s3_cloudfront_ui.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_s3_bucket" "ui" {
-  bucket = "cartsui-cloudfront-origin"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-}
--- frontend/aws/s3_cloudfront_ui.tf
+++ frontend/aws/s3_cloudfront_ui.tf
@@ -16,21 +16,6 @@ resource "aws_s3_bucket" "www" {
   bucket = "${var.www_domain_name}"
   acl    = "public-read"
 
-  policy = <<POLICY
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::${var.www_domain_name}/*"]
-    }
-  ]
-}
-POLICY
-
   website {
     index_document = "index.html"
   //  error_document = "404.html"
