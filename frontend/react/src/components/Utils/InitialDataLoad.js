b27839eb16d509cdb039898a0bdb4d9fcac5bf1b
Greg Walker <michael.walker@gsa.gov>
2020-08-20 16:09:35 -0500

    lint fix

--- frontend/react/src/components/Utils/InitialDataLoad.js
+++ frontend/react/src/components/Utils/InitialDataLoad.js
@@ -7,7 +7,9 @@ const InitialDataLoad = () => {
 
   useEffect(() => {
     dispatch(loadSections());
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  // :point-up: We don't need that lint check because the empty dependencies
+  // is what we want: only load the data once, period.
 
   return null;
 };
