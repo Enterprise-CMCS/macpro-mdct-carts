7452373d0aa03c3951d4a5f3d3033fae919c9915
Greg Walker <michael.walker@gsa.gov>
2020-08-21 12:01:44 -0500

    switch to v1 api

--- frontend/react/src/actions/initial.js
+++ frontend/react/src/actions/initial.js
@@ -4,7 +4,7 @@ export const LOAD_SECTIONS = "LOAD SECTIONS";
 
 export const loadSections = () => {
   return async dispatch => {
-    const { data } = await axios.get("//localhost:8000/sections/");
+    const { data } = await axios.get("//localhost:8000/v1/sections/2020/AK");
     dispatch({ type: LOAD_SECTIONS, data });
   };
 };
