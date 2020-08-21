5b98b6e834d30e9c4cc89c290f4b4f4af11ebff9
Greg Walker <michael.walker@gsa.gov>
2020-08-21 17:32:24 -0500

    soft updates instead of hard reloads

--- frontend/react/src/components/layout/TOC.test.js
+++ frontend/react/src/components/layout/TOC.test.js
@@ -3,7 +3,7 @@ import { shallow, mount } from "enzyme";
 import { storeFactory, findByTestAttribute } from "../../testUtils";
 import { VerticalNav } from "@cmsgov/design-system-core";
 
-import TOC from "./TOC";
+import { TOC } from "./TOC";
 
 /**
  * Factory functon to create a ShallowWrapper for the Footer component.
@@ -18,6 +18,7 @@ const setup = (props = {}, state = null) => {
 
 describe("TOC Component", () => {
   const wrapper = setup();
+  console.log(wrapper.debug())
   let verticalNavWrapper = wrapper.find(VerticalNav).shallow();
   let itemProps = verticalNavWrapper.props().children;
 
