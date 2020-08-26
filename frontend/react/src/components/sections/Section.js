330e2a8c58404e4aa61486c50d9e5f7b97f09527
Tadhg O'Higgins <tadhg@tadhg.com>
2020-08-26 10:59:45 -0700

    Fix error in routing where all subsections would be shown instead of just the matching one.

--- frontend/react/src/components/sections/Section.js
+++ frontend/react/src/components/sections/Section.js
@@ -4,29 +4,29 @@ import PageInfo from "../layout/PageInfo";
 import { extractSectionOrdinalFromId, selectSectionByOrdinal, winnowProperties } from "../../store/formData";
 import Subsection from "./basicinfoapi/Subsection";
 
-const Section = ({Data}) => {
+const Section = ({Data, subsectionConstraint}) => {
+  const constrained = Data && Data.subsections.filter( (subsection) => subsection.id === subsectionConstraint);
   const title = Data && Data.title ? (<h2>{Data.title}</h2>) : "";
-  
-  return Data ? (
+  return constrained ? (
     <div className="section-basic-info ds-l-col--9 content">
       <div className="main">
         <PageInfo />
-        <div className="section-content">
          {title}
-         {Data.subsections.map((subsection) => (
+         {constrained.map((subsection) => (
              <Subsection key={subsection.id} subsectionId={subsection.id}/>
          ))}
-        </div>
       </div>
     </div>
   ) : null;
 } 
+  
 
 
 const mapStateToProps = (state, ownProps) => {
   return {
     abbr: state.stateUser.currentUser.state.id,
     Data: winnowProperties(selectSectionByOrdinal(state, extractSectionOrdinalFromId(ownProps.fragmentId))),
+    subsectionConstraint: ownProps.fragmentId,
     year: state.global.formYear,
     programType: state.stateUser.programType,
     programName: state.stateUser.programName,
