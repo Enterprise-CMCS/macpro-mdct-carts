016f196eeadb7767faec82d2aa828cdb7c9e41b6
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2020-09-10 15:46:57 -0400

    Make sure that all private subnet ids we find have a parallel public subnet (#552)
    Co-authored-by: Mike Dial <mdial@collabralink.com>

--- frontend/aws/vpc.tf
+++ frontend/aws/vpc.tf
@@ -10,8 +10,11 @@ data "aws_subnet_ids" "private" {
     Type = "private"
   }
   filter {
-    name   = "tag:vpc-conf-layer"
-    values = ["app"]
+    name = "tag:aws:cloudformation:logical-id"
+    # The tags are named slightly differently between environments.
+    # Dev/Val1 - DmzSubnetAz2/3
+    # Prod/Test/CM - DmzSubnet2/3
+    values = ["AppSubnetAz2", "AppSubnetAz3", "AppSubnet2", "AppSubnet3"]
   }
 }
 
@@ -21,7 +24,7 @@ data "aws_subnet_ids" "public" {
     Type = "public"
   }
   filter {
-    name   = "tag:aws:cloudformation:logical-id"
+    name = "tag:aws:cloudformation:logical-id"
     # The tags are named slightly differently between environments.
     # Dev/Val1 - DmzSubnetAz2/3
     # Prod/Test/CM - DmzSubnet2/3
