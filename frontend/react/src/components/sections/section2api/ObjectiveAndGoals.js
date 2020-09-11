35f08060955f3242d2dbdfdd57e251c06ec2c4a4
TGriesemer <Tgriesemer@collabralink.com>
2020-09-11 09:22:35 -0400

    pull from master

--- frontend/react/src/components/sections/section2api/ObjectiveAndGoals.js
+++ frontend/react/src/components/sections/section2api/ObjectiveAndGoals.js
@@ -1,5 +1,5 @@
 import { selectQuestion } from "../../../store/selectors";
-import { setAnswer, setId } from "../../../actions/initial";
+import { setAnswer } from "../../../actions/initial";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
@@ -28,29 +28,7 @@ class ObjectiveAndGoals extends Component {
     newObjective.questions[1].questions = [
       this.addNewGoal(newGoalId, objectiveIdString, year),
     ];
-    return {
-      type: "objective",
-      id: `2020-02-b-01-01-${objectiveIdString}`,
-      questions: [
-        {
-          type: "text_multiline",
-          label: "What is your objective as listed in your state plan?",
-          hint:
-            "For example: Our objective is to increase enrollment in our CHIP program.",
-          id: `2020-02-b-01-01-${objectiveIdString}-01`,
-          answer: {
-            readonly: false,
-            default_entry: "",
-            entry: null,
-          },
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveIdString}-02`,
-          type: "repeatables",
-          questions: [this.addNewGoal(newGoalId, objectiveIdString)],
-        },
-      ],
-    };
+    return { newObjective };
   };
 
   addNewGoal = (newGoalId, objectiveId, year) => {
@@ -74,9 +52,73 @@ class ObjectiveAndGoals extends Component {
 
       if (goal.type === "fieldset") {
         subCount = 0;
+        if (goal.fieldset_info.targets) {
+          let tempTarget = goal.fieldset_info.targets[0].split("-");
+          goal.fieldset_info.targets[0] =
+            tempTarget[0] +
+            "-" +
+            tempTarget[1] +
+            "-" +
+            tempTarget[2] +
+            "-" +
+            tempTarget[3] +
+            "-" +
+            tempTarget[4] +
+            "-" +
+            objectiveId +
+            "-" +
+            tempTarget[6] +
+            "-" +
+            goalIdString +
+            "-" +
+            tempTarget[8];
+          tempTarget = goal.fieldset_info.targets[1].split("-");
+          goal.fieldset_info.targets[1] =
+            tempTarget[0] +
+            "-" +
+            tempTarget[1] +
+            "-" +
+            tempTarget[2] +
+            "-" +
+            tempTarget[3] +
+            "-" +
+            tempTarget[4] +
+            "-" +
+            objectiveId +
+            "-" +
+            tempTarget[6] +
+            "-" +
+            goalIdString +
+            "-" +
+            tempTarget[8];
+        }
         goal.questions.forEach((subquestion) => {
-          subquestion.id = `${year}-02-b-01-01-${objectiveId}-02-${goalIdString}-${tempGoalId}-${subQuestionLetter[subCount]}`;
+          let tempSubquestion = subquestion.id.split("-");
+          subquestion.id = `${year}-02-b-01-01-${objectiveId}-02-${goalIdString}-${tempSubquestion[8]}`;
           subquestion.answer.entry = null;
+          if (subquestion.context_data.hide_if.target) {
+            let tempTarget = subquestion.context_data.hide_if.target
+              .split("-")[8]
+              .subString(0, 2);
+            subquestion.context_data.hide_if.target =
+              tempTarget[0] +
+              "-" +
+              tempTarget[1] +
+              "-" +
+              tempTarget[2] +
+              "-" +
+              tempTarget[3] +
+              "-" +
+              tempTarget[4] +
+              "-" +
+              objectiveId +
+              "-" +
+              tempTarget[6] +
+              "-" +
+              goalIdString +
+              "-" +
+              tempTarget[8];
+          }
           subCount = subCount + 1;
         });
       } else {
@@ -87,184 +129,43 @@ class ObjectiveAndGoals extends Component {
           goal.questions.forEach((subquestion) => {
             subquestion.id = `${year}-02-b-01-01-${objectiveId}-02-${goalIdString}-${tempGoalId}-${subQuestionLetter[subCount]}`;
             subquestion.answer.entry = null;
+            if (subquestion.context_data.conditional_display.hide_if.target) {
+              let tempTarget = subquestion.context_data.conditional_display.hide_if.target.split(
+                "-"
+              );
+              subquestion.context_data.hide_if.target =
+                tempTarget[0] +
+                "-" +
+                tempTarget[1] +
+                "-" +
+                tempTarget[2] +
+                "-" +
+                tempTarget[3] +
+                "-" +
+                tempTarget[4] +
+                "-" +
+                objectiveId +
+                "-" +
+                tempTarget[6] +
+                "-" +
+                goalIdString +
+                "-" +
+                tempTarget[8];
+            }
+
             subCount = subCount + 1;
           });
         }
       }
     });
     //Returns all 12 questions for a new goal
-    return {
-      id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}`,
-      type: "repeatable",
-      questions: [
-        
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-02`,
-          label: "What type of goal is it?",
-          type: "radio",
-          answer: {
-            options: [
-              { label: "New goal", value: "goal_new" },
-              { label: "Continuing goal", value: "goal_continuing" },
-              { label: "Discontinued goal", value: "goal_discontinued" },
-            ],
-            default_entry: "goal_new",
-            entry: null,
-          },
-          questions: [
-            {
-              id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-02-a`,
-              label: "Why was this goal discontinued?",
-              type: "text_multiline",
-              answer: {
-                entry: null,
-              },
-              context_data: {
-                conditional_display: {
-                  type: "conditional_display",
-                  comment: `Interactive: Hide if 2020-02-b-01-01-${objectiveId}-02-${goalIdString}-02 is null, continuing goal, or new goal; noninteractive: hide if that's continuing goal or new goal.`,
-                  hide_if: {
-                    target: `$..*[?(@.id=="2020-02-b-01-01-${objectiveId}-02-${goalIdString}-02")].answer.entry`,
-                    values: {
-                      interactive: [null, "goal_continuing", "goal_new"],
-                      noninteractive: ["goal_continuing", "goal_new"],
-                    },
-                  },
-                },
-              },
-            },
-          ],
-        },
-        {
-          type: "fieldset",
-          label: "Define the numerator you're measuring",
-          questions: [
-            {
-              id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-03`,
-              label: "Which population are you measuring in the numerator?",
-              hint:
-                "For example: The number of children enrolled in CHIP in the last federal fiscal year.",
-              type: "integer",
-              answer: {
-                entry: null,
-              },
-            },
-            {
-              id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-04`,
-              label: "Numerator (total number)",
-              type: "integer",
-              answer: {
-                entry: null,
-              },
-            },
-          ],
-        },
-        {
-          type: "fieldset",
-          label: "Define the denominator you're measuring",
-          questions: [
-            {
-              id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-05`,
-              label: "Which population are you measuring in the denominator?",
-              hint:
-                "For example: The total number of eligible children in the last federal fiscal year.",
-              type: "integer",
-              answer: {
-                entry: null,
-              },
-            },
-            {
-              id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-06`,
-              label: "Denominator (total number)",
-              type: "integer",
-              answer: {
-                entry: null,
-              },
-            },
-          ],
-        },
-        {
-          type: "fieldset",
-          fieldset_type: "synthesized_value",
-          fieldset_info: {
-            targets: [
-              `$..*[?(@.id=='2020-02-b-01-01-${objectiveId}-02-${goalIdString}-04')].answer.entry`,
-              `$..*[?(@.id=='2020-02-b-01-01-${objectiveId}-02-${goalIdString}-06')].answer.entry`,
-            ],
-            actions: ["percentage"],
-          },
-          questions: [],
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-07`,
-          label: "What is the date range of your data?",
-          type: "daterange",
-          answer: {
-            labels: ["Start", "End"],
-            entry: null,
-          },
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-08`,
-          label: "Which data source did you use?",
-          type: "radio",
-          answer: {
-            options: [
-              {
-                label: "Eligibility or enrollment data",
-                value: "goal_enrollment_data",
-              },
-              { label: "Survey data", value: "goal_survey_data" },
-              { label: "Another data source", value: "goal_other_data" },
-            ],
-            entry: null,
-          },
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-09`,
-          label:
-            "How did your progress towards your goal last year compare to your previous year\u2019s progress?",
-          type: "text_multiline",
-          answer: {
-            entry: null,
-          },
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-10`,
-          label:
-            "What are you doing to continually make progress towards your goal?",
-          type: "text_multiline",
-          answer: {
-            entry: null,
-          },
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-11`,
-          label: "Anything else you'd like to tell us about this goal?",
-          type: "text_multiline",
-          answer: {
-            entry: null,
-          },
-        },
-        {
-          id: `2020-02-b-01-01-${objectiveId}-02-${goalIdString}-12`,
-          label: "Do you have any supporting documentation?",
-          hint: "Optional",
-          type: "file_upload",
-          answer: {
-            entry: null,
-          },
-        },
-      ],
-    };
+    return { newGoal };
   };
 }
 const mapStateToProps = (state) => ({
   state: state,
 });
 
-const mapDispatchToProps = {
-  setTheId: setId,
-};
+const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ObjectiveAndGoals);
