2956df8800f2ee7eabed0ad5e21a4d1f54a7d3f4
Andrew Adcock <andrewadcock@gmail.com>
2020-09-14 10:42:49 -0400

    Add comment for sussing out part number

--- frontend/react/src/components/layout/Part.js
+++ frontend/react/src/components/layout/Part.js
@@ -6,9 +6,9 @@ import QuestionComponent from "../fields/QuestionComponent";
 
 const showPart = (context_data, programType) => {
   if (context_data &&
-      programType &&
-      _.has(context_data, "show_if_state_program_type_in") &&
-      !context_data.show_if_state_program_type_in.includes(programType)) {
+    programType &&
+    _.has(context_data, "show_if_state_program_type_in") &&
+    !context_data.show_if_state_program_type_in.includes(programType)) {
     return false;
   }
   return true;
@@ -16,6 +16,8 @@ const showPart = (context_data, programType) => {
 
 const Part = ({ partId, text, title, context_data, programType }) => {
   if (showPart(context_data, programType)) {
+
+    // Determine Part Number from partId
     let partNum = Number(partId.split("-").pop());
     return (
       <div id={partId}>
@@ -31,7 +33,7 @@ const Part = ({ partId, text, title, context_data, programType }) => {
       <div id={partId}>
         {title ? <h2>{title}</h2> : <></>}
         {context_data.skip_text ? <p>{context_data.skip_text}</p> : <></>}
-  
+
       </div>
     );
 
