a9a5faefebfaf28ecccb82c3d0895b44e0c0ae1b
Andrew Adcock <andrewadcock@gmail.com>
2020-09-14 08:30:06 -0400

    Remove commented items

--- frontend/react/src/components/sections/homepage/Homepage.js
+++ frontend/react/src/components/sections/homepage/Homepage.js
@@ -3,13 +3,7 @@ import ReportItem from "./ReportItem";
 import { DownloadDrawer } from "./DownloadDrawer";
 
 class Homepage extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      placeholder: "",
-    };
-  }
   render() {
     return (
       <div className="homepage">
@@ -26,7 +20,6 @@ class Homepage extends Component {
                 <div className="report-header ds-l-row">
                   <div className="name ds-l-col--1">Year</div>
                   <div className="status ds-l-col--2">Status</div>
-                  {/* <div className="last-edited ds-l-col--6">Last Edited</div> */}
                   <div className="actions ds-l-col--3">Actions</div>
                 </div>
 
@@ -43,17 +36,6 @@ class Homepage extends Component {
                 />
               </div>
             </div>
-            {/* <div className="ds-l-row reports-footer">
-              <div className="displaying ds-l-col--6">
-                Showing <span className="count">1</span> to{" "}
-                <span className="count">5</span> of{" "}
-                <strong>10 documents</strong>
-              </div>
-              <div className="pager ds-l-col--6">
-                Page <span className="number-primary">1</span> ...{" "}
-                <span className="number-outline">2</span>
-              </div>
-            </div> */}
           </div>
           <div className="ds-l-row">
             <div className="omb-info ds-l-col--12">
