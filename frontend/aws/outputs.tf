ddefdc1dab945bdd9cd8b2e8c27b5dcfa9877cd9
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2020-09-14 13:33:10 -0400

    invalidating cloudfront cache

--- frontend/aws/outputs.tf
+++ frontend/aws/outputs.tf
@@ -14,3 +14,7 @@ output "api_sqlserver_endpoint" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.www.id
 }
+
+output "cloudfront_distribution_id" {
+  value = "${aws_cloudfront_distribution.www_distribution.id}"
+}
