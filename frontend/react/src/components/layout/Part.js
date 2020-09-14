7cfdfe15eb65b8b1342dfcbc2df1a0b4a6473320
Andrew Adcock <andrewadcock@gmail.com>
2020-09-14 11:49:16 -0400

    Tidy up whitespace

--- frontend/react/src/components/layout/Part.js
+++ frontend/react/src/components/layout/Part.js
@@ -16,15 +16,12 @@ const showPart = (context_data, programType) => {
 
 const Part = ({ partId, text, title, context_data, programType }) => {
   if (showPart(context_data, programType)) {
-
     // Determine Part Number from partId
     let partNum = Number(partId.split("-").pop());
     return (
       <div id={partId}>
-
         <h2>Part {partNum}{title ? ": " + title : <></>}</h2>
         {text ? <p>{text}</p> : <></>}
-
         <QuestionComponent partId={partId} />
       </div>
     );
