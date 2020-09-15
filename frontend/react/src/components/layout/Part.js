10d73e9e365fa72bbe498231d4e32ce5b061e029
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2020-09-15 14:25:10 -0400

    add Andrew's part skip code. all of 3i is functioning. repeatables still need help.

--- frontend/react/src/components/layout/Part.js
+++ frontend/react/src/components/layout/Part.js
@@ -3,21 +3,27 @@ import { connect } from "react-redux";
 import { selectFragment } from "../../store/formData";
 import { _ } from "underscore";
 import QuestionComponent from "../fields/QuestionComponent";
+import { shouldDisplay } from "../../util/shouldDisplay";
 
-const showPart = (context_data, programType) => {
+const showPart = (context_data, programType, state) => {
   if (context_data &&
     programType &&
     _.has(context_data, "show_if_state_program_type_in") &&
     !context_data.show_if_state_program_type_in.includes(programType)) {
     return false;
   }
+
+  if (context_data && _.has(context_data, "conditional_display")) {
+    return shouldDisplay(state, context_data);
+  }
   return true;
 }
 
-const Part = ({ partId, text, title, context_data, programType }) => {
-  if (showPart(context_data, programType)) {
-    // Determine Part Number from partId
-    let partNum = Number(partId.split("-").pop());
+const Part = ({ partId, text, title, context_data, programType, state }) => {
+  // Determine Part Number from partId
+  let partNum = Number(partId.split("-").pop());
+
+  if (showPart(context_data, programType, state)) {
     return (
       <div id={partId}>
         <h2>Part {partNum}{title ? ": " + title : <></>}</h2>
@@ -28,9 +34,9 @@ const Part = ({ partId, text, title, context_data, programType }) => {
   } else {
     return (
       <div id={partId}>
+        <h2>Part {partNum}{title ? ": " + title : <></>}</h2>
         {title ? <h2>{title}</h2> : <></>}
         {context_data.skip_text ? <p>{context_data.skip_text}</p> : <></>}
-
       </div>
     );
 
@@ -44,6 +50,7 @@ const mapStateToProps = (state, { partId }) => {
     title: part ? part.title : null,
     context_data: _.has(part, "context_data") ? part.context_data : null,
     programType: state.stateUser.programType,
+    state: state,
   };
 };
 
