e6e86519f32bb20ffccd79697a1e9c644eb90943
Andrew Adcock <andrewadcock@gmail.com>
2020-09-15 08:28:26 -0400

    Add conditional logic for parts that rely on questions

--- frontend/react/src/components/layout/Part.js
+++ frontend/react/src/components/layout/Part.js
@@ -3,24 +3,29 @@ import { connect } from "react-redux";
 import { selectFragment } from "../../store/formData";
 import { _ } from "underscore";
 import QuestionComponent from "../fields/QuestionComponent";
+import { shouldDisplay } from "../../util/shouldDisplay";
 
-const showPart = (context_data, programType) => {
+const showPart = (context_data, programType, state) => {
   if (context_data &&
-      programType &&
-      _.has(context_data, "show_if_state_program_type_in") &&
-      !context_data.show_if_state_program_type_in.includes(programType)) {
+    programType &&
+    _.has(context_data, "show_if_state_program_type_in") &&
+    !context_data.show_if_state_program_type_in.includes(programType)) {
     return false;
+  }
+
+  if (context_data && _.has(context_data, "conditional_display")) {
+    return shouldDisplay(state, context_data);
   }
   return true;
 }
 
-const Part = ({ partId, text, title, context_data, programType }) => {
-  if (showPart(context_data, programType)) {
+const Part = ({ partId, text, title, context_data, programType, state }) => {
+  if (showPart(context_data, programType, state)) {
     return (
       <div id={partId}>
         {title ? <h2>{title}</h2> : <></>}
         {text ? <p>{text}</p> : <></>}
-  
+
         <QuestionComponent partId={partId} />
       </div>
     );
@@ -29,7 +34,6 @@ const Part = ({ partId, text, title, context_data, programType }) => {
       <div id={partId}>
         {title ? <h2>{title}</h2> : <></>}
         {context_data.skip_text ? <p>{context_data.skip_text}</p> : <></>}
-  
       </div>
     );
 
@@ -43,6 +47,7 @@ const mapStateToProps = (state, { partId }) => {
     title: part ? part.title : null,
     context_data: _.has(part, "context_data") ? part.context_data : null,
     programType: state.stateUser.programType,
+    state: state,
   };
 };
 
