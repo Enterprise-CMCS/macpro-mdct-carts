bb6eb1bc250f76127dabb49a87b06b0e9389ee10
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2020-09-16 13:17:22 -0400

    add second layer/paragraph of text for subsection component

--- frontend/react/src/components/layout/Subsection.js
+++ frontend/react/src/components/layout/Subsection.js
@@ -3,11 +3,12 @@ import { connect } from "react-redux";
 import { selectSubsectionTitleAndPartIDs } from "../../store/selectors";
 import Part from "./Part";
 
-const Subsection = ({ partIds, subsectionId, title, text }) => {
+const Subsection = ({ partIds, subsectionId, title, text, text_2 }) => {
   return (
     <div id={subsectionId}>
       <h2>{title}</h2>
       {text ? <div className="helper-text">{text}</div> : null}
+      {text_2 ? <div className="helper-text"><p>{text_2}</p></div> : null}
       {partIds.map((partId) => (
         <Part key={partId} partId={partId} />
       ))}
@@ -24,6 +25,7 @@ const mapStateToProps = (state, ownProps) => {
     partIds: subsection ? subsection.parts : [],
     title: subsection ? subsection.title : null,
     text: subsection ? subsection.text : null,
+    text_2: subsection ? subsection.text_2 : null,
   };
 };
 
