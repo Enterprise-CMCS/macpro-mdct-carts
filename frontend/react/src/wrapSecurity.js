d8b159b256222b6cb3a7bbfa281e73cfda541385
TonyDavydets <tdavydets@collabralink.com>
2020-09-22 14:56:56 -0400

    ability to override okta authentication for localhost / cartsdemo

--- frontend/react/src/wrapSecurity.js
+++ frontend/react/src/wrapSecurity.js
@@ -10,21 +10,33 @@ import InitialDataLoad from "./components/Utils/InitialDataLoad";
 import Home from "./Home";
 import config from './auth-config';
 import * as qs from "query-string";
-
 const WrappedSecurity = () => {
   let VisibleHeader =
     window.location.pathname.split("/")[1] === "reports" ||
-      window.location.pathname.split("/")[1] === "coming-soon" ? null : <Header />;
-
+    window.location.pathname.split("/")[1] === "coming-soon" ? null : <Header />;
   let VisibleFooter =
     window.location.pathname.split("/")[1] === "reports" ||
-      window.location.pathname.split("/")[1] === "coming-soon" ? null : <Footer />;
-
+    window.location.pathname.split("/")[1] === "coming-soon" ? null : <Footer />;
   const loc = qs.parse(useLocation().search);
   const devKeys = { "dev-ak": "AK", "dev-az": "AZ", "dev-ma": "MA" }
-  if (loc.dev && Object.keys(devKeys).includes(loc.dev)) {
-    const userData = {userToken: loc.dev}
-
+  // *** !!!! THIS PART WAS ADDED / SLIGHTLY AUGMENTED
+  // *** pull apart the url using '/' as a separating delimiter
+  const urlParts = window.location.href.trim().toLowerCase().split('/');
+  const domain = urlParts[2];
+  // *** if override dev parameters are present OR if current domain is localhost
+  if ((loc.dev && Object.keys(devKeys).includes(loc.dev)) ||
+      domain.indexOf('localhost') > -1                    ||
+      window.location.href.trim().toLowerCase().indexOf('cartsdemo') > -1
+  ) {
+    // *** default state is currently Alaska change to choose a different state
+    const defaultUserState = 'AK';
+    // *** start of with user token being based on our default state
+    let userData = {userToken: `dev-${defaultUserState.toLowerCase()}`};
+    // *** if a state other than default is specified, override default
+    if (loc.dev && loc.dev.length > 0) {
+      userData = {userToken: loc.dev}
+    }
+    // *** !!! end of ADDED / SLIGHTLY ALTERED PART
     return (
       <div className="App" data-test="component-app">
         <InitialDataLoad userData={userData} />
@@ -33,7 +45,8 @@ const WrappedSecurity = () => {
         {VisibleFooter}
       </div>
     )
-  } else {
+  }
+  else {
     return (
       <Router>
         <Security {...config.oidc}>
@@ -44,6 +57,4 @@ const WrappedSecurity = () => {
     )
   }
 }
-
 export default WrappedSecurity;
-
