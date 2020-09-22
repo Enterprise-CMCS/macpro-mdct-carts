1e6d1cab3d6176314c4cd1b5e3501c0635a66cdc
dwhitecl <dwhite@collabralink.com>
2020-09-22 15:03:18 -0400

    fixing more syntax

--- frontend/aws/s3_cloudfront_ui.tf
+++ frontend/aws/s3_cloudfront_ui.tf
@@ -204,7 +204,7 @@ resource "aws_wafv2_web_acl" "uiwaf" {
 
     statement {
       managed_rule_group_statement{
-        vendor_name = "AWS
+        vendor_name = "AWS"
         "Name": "${terraform.workspace}-AWSManagedRulesKnownBadInputsRuleSet"
       }
     }
