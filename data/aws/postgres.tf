638c0664d31b765db0880960a753c2361fae869b
dwhitecl <dwhite@collabralink.com>
2020-09-24 17:21:08 -0400

    changing param groups to apply on reboot

--- data/aws/postgres.tf
+++ data/aws/postgres.tf
@@ -75,26 +75,31 @@ resource "aws_db_parameter_group" "db_param_group" {
   parameter {
     name  = "pgaudit.role"
     value = "rds_pgaudit"
+    apply_method = "reboot"
   }
 
   parameter {
     name  = "pgaudit.log"
     value = "ALL"
+    apply_method = "reboot"
   }
 
   parameter {
     name  = "shared_preload_libraries"
     value = "pg_stat_statements"
+    apply_method = "reboot"
   }
 
   parameter {
     name  = "shared_preload_libraries"
     value = "pgaudit"
+    apply_method = "reboot"
   }
 
   parameter {
     name  = "log_min_duration_statement"
     value = "10000"
+    apply_method = "reboot"
   }
 }
 
