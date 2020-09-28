3201202300dfac2a44fd54debf554ff50e1961af
Tadhg O'Higgins <tadhg@tadhg.com>
2020-09-28 12:18:22 -0700

    Ensure that we load state data before making migration that involves forign keys relying on state data to be present.

--- frontend/api_postgres/carts/management/commands/idempotent_fixtures.py
+++ frontend/api_postgres/carts/management/commands/idempotent_fixtures.py
@@ -1,21 +1,33 @@
 from django.core import management  # type: ignore
 from django.core.management.base import BaseCommand  # type: ignore
-from carts.carts_api.models import Section, SectionBase, SectionSchema, FMAP, ACS
+from carts.carts_api.models import (
+    ACS,
+    FMAP,
+    Section,
+    SectionBase,
+    SectionSchema,
+    State,
+)
+
 from json import loads
 import jsonschema  # type: ignore
 from pathlib import Path
 
 
 class Command(BaseCommand):
-    help = "".join(["Imports base data, but doesn't overwrite existing ",
-                    "data unless --overwrite is present as a flag."])
+    help = "".join(
+        [
+            "Imports base data, but doesn't overwrite existing ",
+            "data unless --overwrite is present as a flag.",
+        ]
+    )
 
     def add_arguments(self, parser):
         # Named (optional) arguments
         parser.add_argument(
-            '--overwrite',
-            action='store_true',
-            help='Overwrite existing instead of leaving as is',
+            "--overwrite",
+            action="store_true",
+            help="Overwrite existing instead of leaving as is",
         )
 
     def handle(self, *args, **options):
@@ -53,11 +65,13 @@ class Command(BaseCommand):
             elif fixture["model"] == "carts_api.sectionbase":
                 year = fixture["fields"]["contents"]["section"].get("year")
                 ordinal = fixture["fields"]["contents"]["section"].get(
-                    "ordinal")
+                    "ordinal"
+                )
                 try:
                     existing = SectionBase.objects.filter(
                         contents__section__year=year,
-                        contents__section__ordinal=ordinal)
+                        contents__section__ordinal=ordinal,
+                    )
                     if overwrite:
                         existing.delete()
                         is_new = True
@@ -71,13 +85,15 @@ class Command(BaseCommand):
             elif fixture["model"] == "carts_api.section":
                 year = fixture["fields"]["contents"]["section"].get("year")
                 ordinal = fixture["fields"]["contents"]["section"].get(
-                    "ordinal")
+                    "ordinal"
+                )
                 state = fixture["fields"]["contents"]["section"].get("state")
                 try:
                     existing = Section.objects.filter(
                         contents__section__year=year,
                         contents__section__ordinal=ordinal,
-                        contents__section__state=state)
+                        contents__section__state=state,
+                    )
                     if overwrite:
                         existing.delete()
                         is_new = True
@@ -89,7 +105,11 @@ class Command(BaseCommand):
                 except jsonschema.exceptions.ValidationError:
                     print(path, "failed validation")
             elif fixture["model"] == "carts_api.State":
-                paths_to_load.append(path)
+                State.objects.all().delete()
+                # We have to put the state data at the start of the list
+                # because the FMAP and ACS models have foreign keys that
+                # require the state data to already be present.
+                paths_to_load.insert(0, path)
                 continue
             elif fixture["model"] == "carts_api.FMAP":
                 # these are reference objects so we should be safe dumping all
