dde230fb774b505827e780f6392f4878774c5420
Tadhg O'Higgins <tadhg@tadhg.com>
2020-09-27 20:45:32 -0700

    Make compare_fixtures.py work properly with generated state JSON.

--- frontend/api_postgres/utils/section-schemas/compare_fixtures.py
+++ frontend/api_postgres/utils/section-schemas/compare_fixtures.py
@@ -3,6 +3,7 @@ The contents of the JSON in the docs/section-schemas files should match the
 contents of the fields.contents property in the fixture files.
 """
 import json
+from operator import attrgetter
 from pathlib import Path
 from typing import Any
 
@@ -11,15 +12,22 @@ def main() -> None:
     here = Path(__file__).parent.resolve()
     django_root = here.parent.parent.resolve()
     there = (django_root / "fixtures").resolve()
+    states = (here / "state-files").resolve()
 
     # Break early if anything has moved unexpectedly:
     assert here.name == "section-schemas"
     assert here == django_root / "utils" / "section-schemas"
 
-    docs = here.glob("*-section-*.json")
-    fixtures = there.glob("*-section-*.json")
+    generic = here.glob("*-section-*.json")
+    specific = states.glob("*-section-*.json")
+    docs = sorted([*generic] + [*specific], key=attrgetter("name"))
+    fixtures = sorted(
+        [*there.glob("*-section-*.json")], key=attrgetter("name")
+    )
+    print(len(docs), len(fixtures), flush=True)
 
     for doc, fixture in zip(docs, fixtures):
+        assert doc.name == fixture.name
         doc_json, fixture_json = load_json(doc), load_json(fixture)
         contents = fixture_json[0]["fields"]["contents"]
         assert doc_json == contents
