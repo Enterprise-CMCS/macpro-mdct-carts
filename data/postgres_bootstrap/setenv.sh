c7cf9b792752f6166e6472f6985994fea9f35358
Berry Davenport <bdavenport@collabralink.com>
2020-09-28 17:01:56 -0400

    Add this temporarily

--- data/postgres_bootstrap/setenv.sh
+++ data/postgres_bootstrap/setenv.sh
@@ -1,7 +1,6 @@
-export PG_DATABASES=postgres
-export PG_SCHEMAS=public,schip,schipannualreports
+export PG_DATABASE=postgres
+export PG_TABLESPACE=schipdata
+export PG_TABLESPACE_LOCATION=/app/pgdata
 export PG_RO_ROLE=readonly
 export PG_RW_ROLE=readwrite
-export PG_USERS=mpdct_app/@p9P@5s,mpdct_ro/R09P@5s
-export PG_RO_USERS=mpdct_ro
-export PG_RW_USERS=mpdct_app
+export PG_USERS='[{"username":"mdct_app","password":"@p9P@5s","roles":["readwrite","readonly"]},{"username":"mdct_ro","password":"R09P@5s","roles":["readonly"]},{"username":"schip","password":"schip","roles":["readonly"]},{"username":"schipannualreports","password":"schipannualreports","roles":["readonly"]}]'
