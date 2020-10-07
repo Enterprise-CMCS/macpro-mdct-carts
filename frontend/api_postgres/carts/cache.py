1c2681d83373cefe6f05a2ed8795fc3fbfc965d8
Tadhg O'Higgins <tadhg@tadhg.com>
2020-10-07 12:55:44 -0700

    Formatting/linting changes.

--- frontend/api_postgres/carts/cache.py
+++ frontend/api_postgres/carts/cache.py
@@ -1,29 +1,31 @@
-from django.core.cache import caches
-from django.utils.encoding import force_text, force_bytes
 import hashlib
+from django.core.cache import caches  # type: ignore
+from django.utils.encoding import force_text, force_bytes  # type: ignore
+
 
 def cache_key(f, *args, **kwargs):
-  cache_key = ':'.join(
-    [force_text(x) for x in args] +
-    [force_text(f'{k}={v}') for k, v in kwargs.items()]
-  )
-  return hashlib.md5(force_bytes(
-    f.__name__ + cache_key
-  )).hexdigest()
+    cache_k = ":".join(
+        [force_text(x) for x in args]
+        + [force_text(f"{k}={v}") for k, v in kwargs.items()]
+    )
+    return hashlib.md5(force_bytes(f.__name__ + cache_k)).hexdigest()
+
 
 def cached(location, timeout):
-  def inner(func):
-    def wrapper(*args, **kwargs):
-      cache = caches[location]
-      key = cache_key(func, *args, **kwargs)
-      
-      if rv := cache.get(key):
-        print(f'cache hit for {key}')
-        return rv
+    def inner(func):
+        def wrapper(*args, **kwargs):
+            cache = caches[location]
+            key = cache_key(func, *args, **kwargs)
 
-      print(f'cache miss for {key}')
-      rv = func(*args, **kwargs)
-      cache.set(key, rv, timeout)
-      return rv
-    return wrapper
-  return inner
+            if rv := cache.get(key):
+                print(f"cache hit for {key}")
+                return rv
+
+            print(f"cache miss for {key}")
+            rv = func(*args, **kwargs)
+            cache.set(key, rv, timeout)
+            return rv
+
+        return wrapper
+
+    return inner
