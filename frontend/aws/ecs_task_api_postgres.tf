eb1d97e6f40aacbb99f968d20a0eb282a2e7be92
Mike Dial <mdial@collabralink.com>
2020-10-08 16:03:27 -0400

    Give the api task permissions on the upload bucket

--- frontend/aws/ecs_task_api_postgres.tf
+++ frontend/aws/ecs_task_api_postgres.tf
@@ -37,6 +37,17 @@ data "aws_ecr_repository" "postgres_django" {
   name = "postgres_django"
 }
 
+resource "aws_iam_policy" "uploads_bucket_policy" {
+  policy = templatefile("templates/api_policy_for_uploads_bucket.json.tpl", {
+    uploads_bucket_arn = aws_s3_bucket.uploads.arn
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "uploads_bucket_policy_attachment" {
+  role       = aws_iam_role.ecs_task.id
+  policy_arn = aws_iam_policy.uploads_bucket_policy.arn
+}
+
 resource "aws_ecs_task_definition" "api_postgres" {
   family                   = "api_postgres-${terraform.workspace}"
   network_mode             = "awsvpc"
