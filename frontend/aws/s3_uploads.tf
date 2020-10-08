21398fc915d32962617e667e5e319d3877ce97c3
Mike Dial <mdial@collabralink.com>
2020-10-08 10:04:02 -0400

    Make the bucket private

--- frontend/aws/s3_uploads.tf
+++ frontend/aws/s3_uploads.tf
@@ -1,7 +1,6 @@
 
 resource "aws_s3_bucket" "uploads" {
-
   bucket        = "cartscms-uploads-${terraform.workspace}"
-  acl           = "public-read"
+  acl           = "private"
   force_destroy = terraform.workspace == "prod" ? false : true
 }
