1d3083ca0e4bf3639a573ef33edc07699d6b27c0
Mike Dial <mdial@collabralink.com>
2020-10-08 07:00:19 -0400

    Chagne ACL to public-read instead of public

--- frontend/aws/s3_uploads.tf
+++ frontend/aws/s3_uploads.tf
@@ -2,6 +2,6 @@
 resource "aws_s3_bucket" "uploads" {
 
   bucket        = "cartscms-uploads-${terraform.workspace}"
-  acl           = "public"
+  acl           = "public-read"
   force_destroy = terraform.workspace == "prod" ? false : true
 }
