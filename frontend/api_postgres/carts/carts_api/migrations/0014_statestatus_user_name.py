8b2378351196bb476a4b9d7084bfaedbde90fb77
Greg Walker <michael.walker@gsa.gov>
2020-10-14 16:40:08 -0500

    fix migration name

--- frontend/api_postgres/carts/carts_api/migrations/0014_statestatus_user_name.py
+++ frontend/api_postgres/carts/carts_api/migrations/0014_statestatus_user_name.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('carts_api', '0010_statestatus'),
+        ('carts_api', '0013_auto_20201013_2227'),
     ]
 
     operations = [
