7637dc25d50c47204a00567adc2e28a256ffd3d9
TGriesemer <Tgriesemer@collabralink.com>
2020-11-03 16:54:09 -0500

    add jquery and update sidebar to stay in view except over header and footer. Need to test for other roles

--- frontend/react/src/components/layout/Sidebar.js
+++ frontend/react/src/components/layout/Sidebar.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
 import StateHeader from "./StateHeader";
 import TOC from "./TOC";
 
-const Sidebar = () => (
-  <div className="sidebar ds-l-col--3">
-    <StateHeader />
-    <TOC />
-  </div>
-);
+const Sidebar = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = $(window).scrollTop();
+      const elementOffsetTop = $(".sidebar").offset().top;
+      const distance = elementOffsetTop - scrollTop;
+      const untilTheBottom =
+        parseInt($(document).height().toString().replace("px", ""), 10) -
+        parseInt(scrollTop.toString().replace("px", ""), 10);
+      const calculatedTop = // eslint-disable-next-line
+        // eslint-disable-next-line
+        (parseInt($(".sidebar").css("width").replace("px", ""), 10) - 25) // eslint-disable-next-line
+          .toString() + "px";
+
+      if (distance <= 20 && scrollTop > 75 && untilTheBottom > 775) {
+        $(".sidebarInner").css("width", calculatedTop);
+        $(".sidebarInner").css("position", "absolute");
+        $(".sidebarInner").css("top", scrollTop);
+      } else if (distance > 20 && scrollTop > 75) {
+        $(".sidebarInner").css("width", calculatedTop);
+        $(".sidebarInner").css("position", "absolute");
+        $(".sidebarInner").css("top", scrollTop);
+      }
+      // eslint-disable-next-line
+      else if (untilTheBottom < 775) {
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+  return (
+    <div className="sidebar ds-l-col--3">
+      <div className="sidebarInner">
+        <StateHeader />
+        <TOC />
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
