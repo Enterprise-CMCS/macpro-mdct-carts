2b4b25d0f82512669dfdcac66bcc377a79d0e86a
Mike Dial <mdial@collabralink.com>
2020-11-04 08:24:36 -0500

    Remove scaling logic, as it should never be more than 1.  If/when that changes, the logic should be moved to a map

--- frontend/aws/ecs_task_api_postgres.tf
+++ frontend/aws/ecs_task_api_postgres.tf
@@ -2,17 +2,6 @@ locals {
   endpoint_api_postgres = var.acm_certificate_domain_api_postgres == "" ? "http://${aws_alb.api_postgres.dns_name}:8000" : "https://${var.acm_certificate_domain_api_postgres}"
 }
 
-# Number of container instances to spawn per resource. Default is 1.
-locals {
-  dev_postgres     = substr(terraform.workspace, 0, 4) == "dev-" ? 1 : 0
-  master_postgres  = terraform.workspace == "master" ? 1 : 0
-  staging_postgres = terraform.workspace == "staging" ? 1 : 0
-  prod_postgres    = terraform.workspace == "prod" ? 1 : 0
-
-  count_postgres         = local.dev_postgres + local.master_postgres + local.staging_postgres + local.prod_postgres
-  desired_count_postgres = local.count_postgres > 0 ? local.count_postgres : 1
-}
-
 data "aws_ssm_parameter" "postgres_user" {
   name = "/${terraform.workspace}/postgres_user"
 }
@@ -106,7 +95,7 @@ resource "aws_ecs_service" "api_postgres" {
     capacity_provider = "FARGATE"
     weight            = "100"
   }
-  desired_count = local.desired_count_postgres
+  desired_count = 1
   network_configuration {
     subnets         = data.aws_subnet_ids.private.ids
     security_groups = [aws_security_group.api_postgres.id]
