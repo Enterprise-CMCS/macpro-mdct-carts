691027e6fe3b794aad933c3b270fa7eecd361f6b
Mike Dial <mdial@collabralink.com>
2020-11-04 10:27:48 -0500

    Set allowed hosts for settings_prod

--- frontend/api_postgres/carts/settings_prod.py
+++ frontend/api_postgres/carts/settings_prod.py
@@ -26,6 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_random_string(50)
 
+
+ALLOWED_HOSTS = [
+    'localhost' '127.0.0.1',
+    '[::1]',
+    '*',
+    os.environ.get('POSTGRES_API_URL'),
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
