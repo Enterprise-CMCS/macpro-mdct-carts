bb46cfb8b45319eb9a43bb7ddfe4ee520690904e
WAQF <awoodbury@collabralink.com>
2020-11-05 13:22:29 -0500

    move validation into Range Component

--- frontend/react/src/components/fields/Ranges.js
+++ frontend/react/src/components/fields/Ranges.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Button } from "@cmsgov/design-system-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,115 +15,44 @@ const inputs = new Map([
 ]);
 
 const Range = ({ category, id, index, onChange, row, type, values }) => {
-  const [rangeError, setRangeError] = useState("");
-
-  // This choses the appropriate mask for the <Input/>, Month, Percentage or Text
   let Input = Text;
   if (inputs.has(type)) {
     Input = inputs.get(type);
   }
 
-  // Extract start value from redux's nested array, passed down from <Ranges/>
-  // This is an object because of the way it is eventually read by the Textfield component (in Integer.js or Text.js)
-  const startQuestionObject = {
+  const startQuestion = {
     id: `${id}-row-${row}-${index}-start`,
     answer: {
       entry: values ? values[0] : "",
     },
   };
 
-  // Extract start value from redux's nested array, passed down from <Ranges/>
-  // This is an object because of the way it is eventually read by the Textfield component (in Integer.js or Text.js)
-  const endQuestionObject = {
+  const endQuestion = {
     id: `${id}-row-${row}-${index}-end`,
     answer: {
       entry: values ? values[1] : "",
     },
   };
 
-  // Set both the start value object and the end value objects to STATE
-  const [startQuestion, setStartQuestion] = useState(startQuestionObject);
-  const [endQuestion, setEndQuestion] = useState(endQuestionObject);
-
-  // This will update both the start and end objects in STATE when the value from redux is changed
-  useEffect(() => {
-    setStartQuestion(startQuestionObject);
-    setEndQuestion(endQuestionObject);
-  }, [values]);
-
-  // Create input references for both the start and end input fields
-  let rangeStart = useRef();
-  let rangeEnd = useRef();
-
-  // This helper function checks that both values are present from REDUX and validates them
-  // This helper function sets a validation error on the UI but currently does nothing to prevent...
-  // ... invalid data from reaching REDUX
-  const validateInequality = () => {
-    if (values.length === 2) {
-      const start = parseInt(values[0], 10);
-      const end = parseInt(values[1], 10);
-
-      if (start > end) {
-        setRangeError("Start value must be less than end value");
-      } else {
-        setRangeError("");
-      }
-    }
-  };
-
-  // This function returns a boolean after confirming chronology
-  // This function is truggered before either input field sends data to REDUX
-  const chronologyValidation = () => {
-    console.log("Range start??", rangeStart);
-    console.log("Range end??", rangeEnd);
-
-    // PSEUODOCODE:
-    // -- Are both values present ?
-    // ---- Are they in the right order?
-    // ------ return true
-
-    // -- return false
-  };
-
-  // This function is triggered by the first input onChange
   const changeStart = ({ target: { value } }) => {
-    chronologyValidation(); // call some validation function
-
-    onChange(row, index, 0, value); // Sends data to redux
+    onChange(row, index, 0, value);
   };
 
-  // This function is triggered by the second input onChange
   const changeEnd = ({ target: { value } }) => {
-    chronologyValidation(); // call some validation function
-
-    onChange(row, index, 1, value); // Sends data to redux
+    onChange(row, index, 1, value);
   };
-
-  // Any more ideas??
-  // Make it stateful?? Issue: state needs to be as updated as possible,
-  // make sure it never gets stale
-  // Use effect
-
-  // Plan Z:
-  // If theres an error, go into redux and change all values to null
 
   return (
     <div className="cmsrange">
       <div className="cmsrange-outer ds-l-container">
-        {rangeError ? <div className="errors">{rangeError}</div> : null}
         <div className="ds-l-row">
           <div className="cmsrange-container range-start">
             <Input
-              // ref={rangeStart}
-              ref={(input) => {
-                rangeStart = input;
-              }}
               id={`${id}-${row}-${index}-0`}
               label={category[0]}
               className="cmsrange-input"
               question={startQuestion}
               onChange={changeStart}
-              onBlur={validateInequality}
             />
           </div>
           <div className="cmsrange-arrow">
@@ -131,16 +60,11 @@ const Range = ({ category, id, index, onChange, row, type, values }) => {
           </div>
           <div className="cmsrange-container cmsrange-end">
             <Input
-              // ref={rangeEnd}
-              ref={(input) => {
-                rangeEnd = input;
-              }}
               id={`${id}-${row}-${index}-1`}
               label={category[1]}
               className="cmsrange-input"
               question={endQuestion}
               onChange={changeEnd}
-              onBlur={validateInequality}
             />
           </div>
         </div>
@@ -170,28 +94,16 @@ const Ranges = ({ onChange, question }) => {
     },
   } = question;
 
-  // DELETE:
-  // Example entry
-  //     "entry": [
-  //     [["21", "40"], ["30", "50"]],
-  //     [["41", "60"], ["60", "80"]],
-  // ]
-
-  // If there is a saved "entry" in redux, save as the variable "values"
   const [values, setValues] = useState(() => {
     if (Array.isArray(entry)) {
       return entry;
     }
 
-    // TODO: Document what min and max mean and do
     const numberToCreate = min > 0 ? min : 1;
     return [...Array(numberToCreate)].map(() =>
       categories.map(() => [null, null])
     );
   });
-
-  // This function is triggered in <Range/> onChange
-  // This function takes in input and places it where it should be in the nested entry array
 
   const rowChange = (row, category, index, value) => {
     values[row][category][index] = value;
