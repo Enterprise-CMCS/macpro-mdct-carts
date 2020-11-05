90435fd0988ca8e0ec7e0aabc6bf3dd97fa947a8
Andrew Adcock <andrewadcock@gmail.com>
2020-11-05 09:41:40 -0500

    Convert to function from class

--- frontend/react/src/components/layout/FormNavigation.js
+++ frontend/react/src/components/layout/FormNavigation.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { Button } from "@cmsgov/design-system-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
@@ -8,70 +9,80 @@ import { selectSectionsForNav } from "../../store/selectors";
 
 const idToUrl = (id) => `/sections/${id.replace(/-/g, "/")}`;
 
-class FormNavigation extends Component {
-  render() {
-    const { history, location, sections } = this.props;
+const FormNavigation = (props) => {
+  const { history, location, sections } = props;
 
-    const items = [];
-    sections.forEach((section) => {
-      if (section.subsections.length < 2) {
-        items.push(idToUrl(section.id));
-      } else {
-        section.subsections.forEach((subsection) => {
-          items.push(idToUrl(subsection.id));
-        });
-      }
-    });
+  const items = [];
+  sections.forEach((section) => {
+    if (section.subsections.length < 2) {
+      items.push(idToUrl(section.id));
+    } else {
+      section.subsections.forEach((subsection) => {
+        items.push(idToUrl(subsection.id));
+      });
+    }
+  });
 
-    // Get current url
-    let currentUrl = window.location.pathname;
+  // Pull year from current location
+  const year = location.pathname.split("/")[2];
 
-    // Get index of url in items array
-    let currentIndex = items.indexOf(currentUrl);
+  // Add certify and submit page to items array
+  items.push(`/sections/${year}/certify-and-submit`);
 
-    // Determine previous index
-    const previousUrl = items[currentIndex - 1];
+  // Get current url
+  const currentUrl = window.location.pathname;
 
-    // Determine next index
-    const nextUrl = items[currentIndex + 1];
+  // Get index of url in items array
+  const currentIndex = items.indexOf(currentUrl);
 
-    return (
-      <section className="nav-buttons">
-        <div className="ds-l-row">
-          <div className="ds-l-col form-buttons">
-            {previousUrl ? (
-              <div className="form-button previous">
-                <Button
-                  type="submit"
-                  className="ds-c-button"
-                  onClick={() => {
-                    history.push(previousUrl);
-                  }}
-                >
-                  <FontAwesomeIcon icon={faAngleLeft} /> Previous
-                </Button>
-              </div>
-            ) : null}
+  // Determine previous index
+  const previousUrl = items[currentIndex - 1];
 
-            {nextUrl ? (
-              <div className="form-button next">
-                <Button
-                  type="submit"
-                  className="ds-c-button ds-c-button--primary"
-                  onClick={() => {
-                    history.push(nextUrl);
-                  }}
-                >
-                  Next <FontAwesomeIcon icon={faAngleRight} />
-                </Button>
-              </div>
-            ) : null}
-          </div>
+  // Determine next index
+  const nextUrl = items[currentIndex + 1];
+
+  return (
+    <section className="nav-buttons">
+      <div className="ds-l-row">
+        <div className="ds-l-col form-buttons">
+          {previousUrl ? (
+            <div className="form-button previous">
+              <Button
+                type="submit"
+                className="ds-c-button"
+                onClick={() => {
+                  history.push(previousUrl);
+                }}
+              >
+                <FontAwesomeIcon icon={faAngleLeft} /> Previous
+              </Button>
+            </div>
+          ) : null}
+
+          {nextUrl ? (
+            <div className="form-button next">
+              <Button
+                type="submit"
+                className="ds-c-button ds-c-button--primary"
+                onClick={() => {
+                  history.push(nextUrl);
+                }}
+              >
+                Next <FontAwesomeIcon icon={faAngleRight} />
+              </Button>
+            </div>
+          ) : null}
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+FormNavigation.propTypes = {
+  history: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  sections: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = (state) => ({ sections: selectSectionsForNav(state) });
 
