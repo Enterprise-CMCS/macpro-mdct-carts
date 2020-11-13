7d846188473e761f4494c56b32cdf2c014bf7be4
Andrew Adcock <aadcock@collabralink.com>
2020-11-13 11:01:18 -0500

    Run prettier

--- frontend/react/src/components/layout/BrowserIssue.js
+++ frontend/react/src/components/layout/BrowserIssue.js
@@ -4,9 +4,10 @@ const BrowserIssue = () => (
   <div className="browser-issue">
     <h1>Browser Problem</h1>
     <p>
-      We are sorry but <a href="https://mdctcarts.cms.gov/">https://mdctcarts.cms.gov</a> cannot be viewed by Internet Explorer or
-      Edge browsers. Please use a more secure browser such as Google Chrome,
-      Mozilla Firefox, Opera, etc.
+      We are sorry but{" "}
+      <a href="https://mdctcarts.cms.gov/">https://mdctcarts.cms.gov</a> cannot
+      be viewed by Internet Explorer or Edge browsers. Please use a more secure
+      browser such as Google Chrome, Mozilla Firefox, Opera, etc.
     </p>
     <p>
       If you feel this is an error, please contact the helpdesk{" "}
