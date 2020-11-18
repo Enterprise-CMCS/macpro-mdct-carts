fafb4eaf5aaadc531afda9583caa7f3275da2df2
Andrew Adcock <andrewadcock@gmail.com>
2020-11-18 10:58:49 -0500

    Add user info to component and style

--- frontend/react/src/components/layout/users/Users.js
+++ frontend/react/src/components/layout/users/Users.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "../../../authenticatedAxios";
-
+import { LOAD_SECTIONS, loadSections } from "../../../actions/initial";
+import { useDispatch } from "react-redux";
+import moment from "moment";
 /**
  * Display all users with options
  *
@@ -9,27 +11,79 @@ import axios from "../../../authenticatedAxios";
  */
 
 const Users = () => {
-  let data;
-  useEffect(() => async(dispatch) => {
-  dispatch({ type: "CONTENT_FETCHING_STARTED" });
+  const [users, setUsers] = useState();
+  const dispatch = useDispatch();
 
-  try {
-    data = await axios.post(`/api/v1/userprofile/`);
+  useEffect(async () => {
+    dispatch({ type: "CONTENT_FETCHING_STARTED" });
 
-  } catch (e) {
-    console.log("Error pulling users data: ", e);
+    try {
+      console.log("xxxxxxxxxxxxxxxxx");
+      let { data } = await axios.post(`/api/v1/userprofiles`);
+      console.log("zzjson", data);
+      setUsers(data);
+    } catch (e) {
+      console.log("Error pulling users data: ", e);
+      console.log("zzjson-failure");
+    }
+    dispatch({ type: "CONTENT_FETCHING_FINISHED" });
+  }, []);
+
+  let output = [];
+
+  if (users) {
+    for (const user of users) {
+      // Split array into comma separated string
+      const stateCodes = user.state_codes.join(", ");
+
+      // Trim email for display
+      // const email = user.email.substring(0, 18) + "..."
+      const email = user.email;
+
+      // Correct time
+      const dateJoined = moment(user.date_joined).format("MM/DD/YYYY");
+
+      // Set active/inactive
+      const active = user.is_active ? "active" : "inactive";
+
+      output.push(
+        <li key={user.id}>
+          <div class="username">{user.username}</div>
+          <div class="name">
+            {user.first_name} {user.last_name}
+          </div>
+          <div class="email">
+            <a href={`mailto:${user.email}`}>{email}</a>
+          </div>
+          <div class="date_joined">{dateJoined}</div>
+          <div class="user_role">{user.user_role}</div>
+          <div class="state_codes">{stateCodes}</div>
+          <div class="active">{active}</div>
+        </li>
+      );
+    }
   }
-  dispatch({type: "CONTENT_FETCHING_STARTED"})
-  })
 
-  console.log("zzjson", data);
-let a =0;
+  console.log("zzoutput", output);
+
+  let a = 0;
   return (
-    <div>
-      Data: {data}
+    <div className="user-profiles">
+      <h1>Users</h1>
+      <ul>
+        <li>
+          <div className="label username">Username</div>
+          <div className="label name">Name</div>
+          <div className="label email">Email</div>
+          <div className="label date_joined">Joined</div>
+          <div className="label user_role">Role</div>
+          <div className="label state_codes">State(s)</div>
+          <div className="label active">Active</div>
+        </li>
+        {output}
+      </ul>
     </div>
-  )
-
+  );
 };
 
 export default Users;
