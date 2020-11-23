2226b4fde2deda08441bbfbf1677c817e505451c
Andrew Adcock <andrewadcock@gmail.com>
2020-11-23 13:11:32 -0500

    Run black on migrations

--- frontend/api_postgres/carts/carts_api/migrations/0018_auto_20201123_1702.py
+++ frontend/api_postgres/carts/carts_api/migrations/0018_auto_20201123_1702.py
@@ -8,13 +8,18 @@ import django_db_views.operations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('carts_api', '0017_userprofiles'),
+        ("carts_api", "0017_userprofiles"),
     ]
 
     operations = [
         django_db_views.operations.ViewRunPython(
-            code=django_db_views.migration_functions.ForwardViewMigration('SELECT a.id,\n            a.password,\n            a.last_login,\n            a.is_superuser,\n            a.username,\n            a.first_name,\n            a.last_name,\n            a.email,\n            a.is_staff,\n            a.is_active,\n            a.date_joined,\n            r.user_role,\n            s.state_codes\n           FROM ((auth_user a\n             LEFT JOIN carts_api_rolefromusername r ON (((r.username)::text = (a.username)::text)))\n             LEFT JOIN carts_api_statesfromusername s ON (((s.username)::text = (a.username)::text)))', 'vw_userprofile'),
-            reverse_code=django_db_views.migration_functions.BackwardViewMigration('', 'vw_userprofile'),
+            code=django_db_views.migration_functions.ForwardViewMigration(
+                "SELECT a.id,\n            a.password,\n            a.last_login,\n            a.is_superuser,\n            a.username,\n            a.first_name,\n            a.last_name,\n            a.email,\n            a.is_staff,\n            a.is_active,\n            a.date_joined,\n            r.user_role,\n            s.state_codes\n           FROM ((auth_user a\n             LEFT JOIN carts_api_rolefromusername r ON (((r.username)::text = (a.username)::text)))\n             LEFT JOIN carts_api_statesfromusername s ON (((s.username)::text = (a.username)::text)))",
+                "vw_userprofile",
+            ),
+            reverse_code=django_db_views.migration_functions.BackwardViewMigration(
+                "", "vw_userprofile"
+            ),
             atomic=False,
         ),
     ]
