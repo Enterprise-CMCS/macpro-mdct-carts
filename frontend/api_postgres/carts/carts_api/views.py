e0662669a3e89ddc9fd0271110c897c585f01c71
TonyDavydets <tdavydets@collabralink.com>
2020-11-24 15:23:16 -0500

    testing S3_UPLOADS_BUCKET_NAME environment var

--- frontend/api_postgres/carts/carts_api/views.py
+++ frontend/api_postgres/carts/carts_api/views.py
@@ -686,6 +686,9 @@ def generate_upload_psurl(request):
     s3_bucket = os.environ.get("S3_UPLOADS_BUCKET_NAME")
     file = request.data["uploadedFile"]
 
+    print(f"\n\n()()???>env variables")
+    print(os.environ)
+
     print(f"\n\n===>uploading {file} to bucket: {s3_bucket} ")
 
     # Generate the URL to get 'key-name' from 'bucket-name'
