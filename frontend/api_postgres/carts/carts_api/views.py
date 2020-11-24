b15296c415a5ca359d757ff287a86a5d41b80971
TonyDavydets <tdavydets@collabralink.com>
2020-11-24 13:10:23 -0500

    logs added

--- frontend/api_postgres/carts/carts_api/views.py
+++ frontend/api_postgres/carts/carts_api/views.py
@@ -684,15 +684,16 @@ def generate_upload_psurl(request):
     s3 = session.client("s3")
 
     s3_bucket = os.environ.get("S3_UPLOADS_BUCKET_NAME")
+    file = request.data["uploadedFile"]
 
-    print(f"\n\n===>uploading to bucket: {s3_bucket} ")
+    print(f"\n\n===>uploading {file} to bucket: {s3_bucket} ")
 
     # Generate the URL to get 'key-name' from 'bucket-name'
     url = s3.generate_presigned_url(
         ClientMethod="get_object",
         Params={
             f"Bucket": f"{s3_bucket}",
-            f"Key": request.data["uploadedFile"],
+            f"Key": f"{file}",
             f"ResponseContentType": "text/plain",
         },
         ExpiresIn=100,
