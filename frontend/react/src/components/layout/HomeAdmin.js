763a18d4f325ad349aeef132c469fa61abcd69ce
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2020-12-11 13:28:09 -0500

    Revert "Merge pull request #965 from CMSgov/feature/3401-add-user"
    This reverts commit 9942ebaac074d228e362eba2402b0aa0a9f2433c, reversing
    changes made to ee283ea81b4b91f7ce9a2e3073caa7bfb80687b7.

--- frontend/react/src/components/layout/HomeAdmin.js
+++ frontend/react/src/components/layout/HomeAdmin.js
@@ -6,7 +6,7 @@ import JobCodeRoleAssociations from "../Utils/JobCodeRoleAssociations";
 import StateAssociations from "../Utils/StateAssociations";
 import UserRoleAssociations from "../Utils/UserRoleAssociations";
 import Users from "../layout/users/Users";
-import AddUser from "../Utils/AddUser";
+import AddStateUser from "../Utils/AddStateUser";
 
 const AdminHome = ({ SecureRouteComponent: SecureRoute }) => (
   <>
@@ -24,10 +24,25 @@ const AdminHome = ({ SecureRouteComponent: SecureRoute }) => (
           <div className="ds-l-row">
             <ul>
               <li>
-                <a href="/users">List users</a>
+                <Link to="/state_assoc">
+                  Associate usernames (EUA IDs) to states
+                </Link>
               </li>
               <li>
-                <Link to="/add_user">Add user</Link>
+                <Link to="/role_user_assoc">
+                  Associate usernames (EUA IDs) to CARTS roles
+                </Link>
+              </li>
+              <li>
+                <Link to="/role_jobcode_assoc">
+                  Associate EUA job codes to CARTS roles
+                </Link>
+              </li>
+              <li>
+                <a href="/users">List and edit all users</a>
+              </li>
+              <li>
+                <Link to="/add_state_user">Add state user</Link>
               </li>
             </ul>
           </div>
@@ -46,7 +61,7 @@ const AdminHome = ({ SecureRouteComponent: SecureRoute }) => (
       component={JobCodeRoleAssociations}
     />
     <SecureRoute exact path="/users" component={Users} />
-    <SecureRoute exact path="/add_user" component={AddUser} />
+    <SecureRoute exact path="/add_state_user" component={AddStateUser} />
   </>
 );
 AdminHome.propTypes = {
