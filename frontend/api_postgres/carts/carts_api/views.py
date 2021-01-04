64aadb14b51245540dd845637822c21045ac6729
Jenna Gillis <36236589+jgillis1@users.noreply.github.com>
2021-01-04 13:43:14 -0500

    formatting
    formatting

--- frontend/api_postgres/carts/carts_api/views.py
+++ frontend/api_postgres/carts/carts_api/views.py
@@ -1039,7 +1039,7 @@ def SendEmailStatusChange(request):
     # Disallow if not in Dev or Prod environments
     if (
         os.environ.get("ENVIRONMENT") != "dev"
-    #1/4/2021: commenting out PROD for the moment since SES service has not been fully configured so emails cannot be sent
+    # 1/4/2021: commenting out PROD for the moment since SES service has not been fully configured so emails cannot be sent
     #    and os.environ.get("ENVIRONMENT") != "prod"
     ):
         return JsonResponse(
