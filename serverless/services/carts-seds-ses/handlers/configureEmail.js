a4d511b219a24b60fab653c7032d6df7e3c85f0e
mskalkar <mkalkar@collabralink.com>
2021-02-26 11:50:41 -0500

    Added IAM db connect policy to serverless yaml

--- serverless/services/carts-seds-ses/handlers/configureEmail.js
+++ serverless/services/carts-seds-ses/handlers/configureEmail.js
@@ -4,38 +4,49 @@
 const AWS = require("aws-sdk");
 const { Client } = require("pg");
 var ses = new AWS.SES({ region: "us-east-1" });
+
 const issueQuery = (
-  username,
-  password,
-  databasename,
-  hostname,
+  username = process.env.postgresUser,
+  password = process.env.postgresPassword,
+  databasename = process.env.postgresDb,
+  hostname = process.env.postgresHost,
   port = 5432
 ) => {
-  const client = new Client({
+  console.log('Made it here', username);
+  const clientConfig = {
     user: username,
     host: hostname,
     database: databasename,
     password: password,
     port: port,
-  });
+  };
+  const client = new Client(clientConfig);
+  console.log('config', clientConfig);
+  client.on('connect', err => {
+    console.error('something bad has happened!', err.stack)
+  })
   client.connect();
+  console.log('did I connect?');
+  
   client.query(
-    "select usename, valuntil from pg_user where valuntil != 'infinity' and valuntil < now() + interval '25' day",
+    "select now()",
+//    "select usename, valuntil from pg_user where valuntil != 'infinity' and valuntil < now() + interval '25' day",
     (err, resp) => {
+      console.log('RESPONSE', resp);
       if (err) {
         client.end();
         return console.error("error running query", err);
       } else {
         var numrows = resp.rows.length;
         console.log("Numrows:", numrows);
-        if (numrows > 0) emailOutput(resp.rows);
+       // if (numrows > 0) emailOutput(resp.rows);
         client.end();
       }
     }
   );
 };
 const emailOutput = (result) => {
-  console.log(result);
+  console.log('Did I make it to the email function?', result);
   var params = {
     Destination: {
       ToAddresses: ["bdavenport@collabralink.com"],
@@ -54,35 +65,7 @@ const emailOutput = (result) => {
   });
 };
 const executeTask = () => {
-  let ssm = new AWS.SSM({ region: "us-east-1" });
-  let ssmParameters = {
-    "/master/postgres_db": null,
-    "/master/postgres_host": null,
-    "/master/postgres_password": null,
-    "/master/postgres_user": null,
-  };
-  let params = {
-    Names: Object.keys(ssmParameters),
-    WithDecryption: true,
-  };
-  return ssm.getParameters(params, function (err, data) {
-    if (err) console.log(err, err.stack);
-    // an error occurred
-    else {
-      data.Parameters.forEach((item) => {
-        //console.log(item);
-        let value = item.Value;
-        let name = item.Name;
-        ssmParameters[name] = value;
-      });
-      issueQuery(
-        ssmParameters["/master/postgres_user"],
-        ssmParameters["/master/postgres_password"],
-        ssmParameters["/master/postgres_db"],
-        ssmParameters["/master/postgres_host"]
-      );
-    }
-  });
+  issueQuery();
 };
 exports.handler = (event, context, callback) => {
   executeTask();
