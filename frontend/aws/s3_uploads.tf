6dd84ecfd80eb8afa5a0bdd9db47c1ec611703e5
cl-adavyd <tdavydets@collabralink.com>
2021-04-27 09:09:43 -0400

    bucket provisioning added to correct place

--- frontend/aws/s3_uploads.tf
+++ frontend/aws/s3_uploads.tf
@@ -1,12 +1,40 @@
+data "aws_cloudformation_stack" "uploads" {
+  name = "uploads-${terraform.workspace}"
+}
+
 resource "aws_s3_bucket" "uploads" {
   bucket        = "cartscms-uploads-${terraform.workspace}"
   acl           = "private"
   force_destroy = terraform.workspace == "prod" ? false : true
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = [local.endpoint_ui]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = aws_s3_bucket.uploads.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY"
+    Statement = [
+      {
+        Sid       = "MustBeClean"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.uploads.arn}/*"
+
+        Condition = {
+          StringNotEquals = {
+            "s3:ExistingObjectTag/virusScanStatus" = [
+              "CLEAN"
+            ],
+            "aws:PrincipalArn" = data.aws_cloudformation_stack.uploads.outputs["BucketAVScanRoleArn"]
+          }
+        }
+      },
+    ]
+  })
 }
