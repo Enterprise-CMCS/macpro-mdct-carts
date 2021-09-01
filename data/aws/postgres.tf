07aa070320ec722136acda04f50d34bbbb9a3580
dwhitecl <dwhite@collabralink.com>
2021-09-01 14:47:04 -0400

    trying to fix db upgrade

--- data/aws/postgres.tf
+++ data/aws/postgres.tf
@@ -71,7 +71,7 @@ resource "aws_security_group_rule" "vpn" {
 resource "aws_db_parameter_group" "db_param_group" {
   name   = "rds-pg-${terraform.workspace}"
 
-  family = "postgres9.6"
+  family = "postgres12.7"
 
   parameter {
     name  = "pgaudit.role"
