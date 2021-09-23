75a9c6866a52541a9c01f2650b2ed0edcf787b18
zach lewis <zlewis@clarityinnovates.com>
2021-09-22 19:46:41 -0500

    2021 only

--- frontend/react/src/components/layout/FormTemplates.js
+++ frontend/react/src/components/layout/FormTemplates.js
@@ -31,9 +31,6 @@ const FormTemplates = () => {
           <option value="2021" selected>
             2021
           </option>
-          <option value="2022">2022</option>
-          <option value="2023">2023</option>
-          <option value="2024">2024</option>
         </select>
         <Button
           type="button"
