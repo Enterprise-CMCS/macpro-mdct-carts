be74d45b3ca51bd37717d10b35b48f6e2e45f081
Samuel Sierra <ssierra@collabralink.com>
2021-09-24 15:38:11 -0400

    formatting

--- frontend/aws/hsts.js
+++ frontend/aws/hsts.js
@@ -1,7 +1,6 @@
 function handler(event) {
-    var response = event.response;
-    var headers = response.headers;
-    headers['strict-transport-security'] = { value: 'max-age=63072000; includeSubdomains; preload' };
-    return response;
-  }
-  
+  var response = event.response;
+  var headers = response.headers;
+  headers['strict-transport-security'] = { value: 'max-age=63072000; includeSubdomains; preload' };
+  return response;
+}
