186781350263f3751e44d0d4bafc95e05d0db921
Hasan Farook <hasanfar@gmail.com>
2021-09-30 17:43:14 -0400

    hide account menu when logged out

--- frontend/react/src/components/layout/Header.js
+++ frontend/react/src/components/layout/Header.js
@@ -45,32 +45,7 @@ class Header extends Component {
             <div className="user-details ds-l-col--8 ds-u-padding--2">
               <div className="ds-l-row">
                 <Autosave />
-                <div className="nav-user" id="nav-user">
-                  <ul className="user-email-button">
-                    <li>
-                      <a
-                        href="#menu"
-                        className="nav--dropdown__trigger"
-                        onClick={this.toggleUserNav}
-                      >
-                        {email}
-                      </a>
-                    </li>
-                  </ul>
-                  <ul className="menu-block" id="menu-block">
-                    <li className="helpdesk">
-                      <a href="mailto:mdct_help@cms.hhs.gov">Helpdesk</a>
-                    </li>
-
-                    <li className="manage-account">
-                      <a href="/user/profile">Manage account</a>
-                    </li>
-
-                    <li className="logout">
-                      {isLoggedIn ? <Logout /> : null}
-                    </li>
-                  </ul>
-                </div>
+                {isLoggedIn ? renderMenu(this.toggleUserNav, email) : null}
               </div>
             </div>
           </div>
@@ -79,7 +54,36 @@ class Header extends Component {
     );
   }
 }
+function renderMenu(toggleUserNav, email) {
+  return(
+    <div className="nav-user" id="nav-user">
+    <ul className="user-email-button">
+      <li>
+        <a
+          href="#menu"
+          className="nav--dropdown__trigger"
+          onClick={toggleUserNav}
+        >
+          {email}
+        </a>
+      </li>
+    </ul>
+    <ul className="menu-block" id="menu-block">
+      <li className="helpdesk">
+        <a href="mailto:mdct_help@cms.hhs.gov">Helpdesk</a>
+      </li>
 
+      <li className="manage-account">
+        <a href="/user/profile">Manage account</a>
+      </li>
+
+      <li className="logout">
+        <Logout />
+      </li>
+    </ul>
+  </div>
+  );
+}
 Header.propTypes = {
   currentUser: PropTypes.object.isRequired,
   formYear: PropTypes.object.isRequired,
