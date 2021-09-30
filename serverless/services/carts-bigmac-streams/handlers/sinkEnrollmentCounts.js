61d5a0c53ffa33ee5f1c00d9fc313a0e04d35124
13bfrancis <13bfrancis@gmail.com>
2021-09-30 18:23:36 -0400

    established connection to kafka for sending data to new topic

--- serverless/services/carts-bigmac-streams/handlers/sinkEnrollmentCounts.js
+++ serverless/services/carts-bigmac-streams/handlers/sinkEnrollmentCounts.js
@@ -1,5 +1,33 @@
-function myHandler(event, context, callback) {
-  console.log(event);
+const { Kafka } = require("kafkajs");
+
+async function myHandler(event, _context, _callback) {
+  const kafka = new Kafka({
+    clientId: `sed-${process.env.STAGE}`,
+    brokers: process.env.BOOTSTRAP_BROKER_STRING_TLS.split(","),
+    retry: {
+      initialRetryTime: 300,
+      retries: 8,
+    },
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const producer = kafka.producer();
+
+  await producer.connect();
+
+  await producer.send({
+    topic: "aws.mdct.seds.cdc.enrollment-counts.v0",
+    messages: [event],
+  });
+
+  console.log(producer);
+
+  await producer.disconnect();
+  console.log(event.value);
+  console.log("brian");
+  console.log("hello");
 }
 
 exports.handler = myHandler;
