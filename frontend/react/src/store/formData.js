c981183b9349644dc17cbc4b44f9a9efc44e0044
zach lewis <zlewis@clarityinnovates.com>
2021-10-05 15:32:22 -0500

    prettier

--- frontend/react/src/store/formData.js
+++ frontend/react/src/store/formData.js
@@ -24,7 +24,7 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case LOAD_SECTIONS:
       updatedData = action.data.sort(sortByOrdinal);
-      console.log("work")
+      console.log("work");
       if (action.lastYearData) {
         let lastYearData = action.lastYearData.data.sort(sortByOrdinal);
         updatedData[0].contents.section.subsections[0].parts[0].questions[0] =
