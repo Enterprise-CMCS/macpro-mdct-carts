df9bad36bcb42deac6913f3fa4ac51bb717bd292
Hasan Farook <hasanfar@gmail.com>
2021-10-13 14:12:08 -0400

    troubleshotting

--- frontend/aws/s3_uploads.tf
+++ frontend/aws/s3_uploads.tf
@@ -2,8 +2,6 @@ data "aws_cloudformation_stack" "uploads" {
   name = "uploads-${terraform.workspace}"
 }
 
-data "aws_caller_identity" "current" {}
-
 resource "aws_s3_bucket" "uploads" {
   bucket        = "cartscms-uploads-${terraform.workspace}"
   acl           = "private"
