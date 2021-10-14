1995923306f188595ba9c206bda3cd43323d9a37
hasanfar <hfarook@collabralink.com>
2021-10-14 12:52:09 -0400

    Dev lambda (#1159)
    * added source account to s3

--- frontend/aws/s3_uploads.tf
+++ frontend/aws/s3_uploads.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_permission" "allow_bucket" {
   action        = "lambda:InvokeFunction"
   function_name = data.aws_cloudformation_stack.uploads.outputs["AvScanArn"]
   principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.uploads.arn
+  source_arn    = "arn:aws:s3::${local.account_id}:${aws_s3_bucket.uploads.bucket}"
 }
 
 resource "aws_s3_bucket_notification" "avscan" {
