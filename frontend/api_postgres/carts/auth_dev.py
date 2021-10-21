15398d789d6d250c95ab3733737033785b9f86e0
Hasan Farook <hasanfar@gmail.com>
2021-10-21 16:49:36 -0400

    cleanup

--- frontend/api_postgres/carts/auth_dev.py
+++ frontend/api_postgres/carts/auth_dev.py
@@ -6,30 +6,23 @@ from carts.carts_api.models import AppUser, State, StatesFromUsername
 
 class JwtDevAuthentication(JwtAuthentication):
     def authenticate(self, request, username=None):
-        print("Inside Auth")
-        print(username)
         dev_username = username
-
         try:
             if not dev_username:
                 if "dev" in request.query_params:
-                    print("inside dev_username")
                     try:
                         dev_username = request.query_params["dev"]
                     except Exception as e:
-                        print("exception 1")
                         raise exceptions.AuthenticationFailed(
                             "dev authentication failed"
                         ) from e
         except Exception as e:
-            print("exception 2")
             raise exceptions.AuthenticationFailed(
                 "dev authentication failed"
             ) from e
 
         if dev_username:
             _, suffix = dev_username.split("-")
-            print("dev_username")
             roles = {
                 "admin": "admin_user",
                 "bus": "bus_user",
@@ -40,9 +33,7 @@ class JwtDevAuthentication(JwtAuthentication):
             }
 
             role = roles[suffix]
-            print(role)
             if role == "state_user":
-                print("state_user")
                 state_codes = [suffix.upper()]
                 states = State.objects.filter(code__in=state_codes)
                 email = f"{dev_username}@{states[0].name.lower()}.gov"
@@ -54,7 +45,6 @@ class JwtDevAuthentication(JwtAuthentication):
                         state_codes = state_relationship.state_codes
                         states = State.objects.filter(code__in=state_codes)
                 except StatesFromUsername.DoesNotExist:
-                    print("before pass")
                     pass
             else:
                 states = []
@@ -66,26 +56,24 @@ class JwtDevAuthentication(JwtAuthentication):
             dev_user.first_name = "DevFirst"
             dev_user.last_name = f"Dev{role}"
             dev_user.email = email
-            print("after users")
-            if role == "admin_user":
-                group = Group.objects.get(name="Admin users")
-                dev_user.groups.set([group])
-            # Once we have different permissions for co_users, add here.
-            elif role in ("bus_user", "co_user"):
-                print("75")
-                try:
-                    group = Group.objects.get(name="Business owner users")
-                    print("77")
+            try:
+                if role == "admin_user":
+                    group = Group.objects.get(name="Admin users")
                     dev_user.groups.set([group])
-                    print("79")
-                except BaseException as err:
-                    print(err)
-            elif role == "state_user":
-                group = Group.objects.get(
-                    name__endswith=f"{state_codes[0]} sections"
-                )
-                dev_user.groups.set([group])
-            print("before app_user")
+                # Once we have different permissions for co_users, add here.
+                elif role in ("bus_user", "co_user"):
+                    group = Group.objects.get(name="Business owner users")
+                    dev_user.groups.set([group])
+                elif role == "state_user":
+                    group = Group.objects.get(
+                        name__endswith=f"{state_codes[0]} sections"
+                    )
+                    dev_user.groups.set([group])
+            except Exception as err:
+                raise exceptions.AuthenticationFailed(
+                    "authentication failed"
+                ) from err
+
             app_user, _ = AppUser.objects.get_or_create(user=dev_user)
             app_user.states.set(states)
             app_user.role = role
