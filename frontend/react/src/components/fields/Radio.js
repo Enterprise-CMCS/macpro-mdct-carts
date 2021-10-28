2a6b521ebad4627d57ba7a574c7db3e37c1575de
zach lewis <zlewis@clarityinnovates.com>
2021-10-28 09:48:12 -0500

    eslint

--- frontend/react/src/components/fields/Radio.js
+++ frontend/react/src/components/fields/Radio.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Choice } from "@cmsgov/design-system-core";
 
-import Question from "./Question"; // eslint-disable-line import/no-cycle
+import Question from "./Question";
 
 const Radio = ({ onChange, question, ...props }) => {
   const [checked, setChecked] = useState(question.answer.entry);
@@ -27,6 +27,7 @@ const Radio = ({ onChange, question, ...props }) => {
 
   return question.answer.options.map(({ label, value }) => (
     <Choice
+      key={value}
       checked={checked === value}
       type="radio"
       value={!checked ? value : undefined}
