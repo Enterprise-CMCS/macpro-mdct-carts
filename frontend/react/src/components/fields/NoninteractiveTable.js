8eb2cf488939692325dfdd0ccd233f4109ecfe3f
Andrew Belzer <abelzer@collabralink.com>
2021-11-08 09:41:33 -0600

    Add Brian's Suggestions

--- frontend/react/src/components/fields/NoninteractiveTable.js
+++ frontend/react/src/components/fields/NoninteractiveTable.js
@@ -20,11 +20,7 @@ const NoninteractiveTable = ({ question }) => {
                 percentLocation[count] = false;
               }
               return (
-                <th
-                  width={`${columnWidth}%`}
-                  name={`${header}`}
-                  key={`${header}`}
-                >
+                <th width={`${columnWidth}%`} name={`${header}`} key={header}>
                   {header}
                 </th>
               );
@@ -35,7 +31,7 @@ const NoninteractiveTable = ({ question }) => {
           {question.fieldset_info.rows.map((row) => {
             count = -1;
             return (
-              <tr key={`${count}`}>
+              <tr key={count}>
                 {row.map((value) => {
                   count += 1;
                   // adds % to any element that has percent in the header and adds commas via toLocaleString
