b11e41e3b2b537fa9be99895689ce6e74553a56a
Andrew Belzer <abelzer@collabralink.com>
2021-11-15 13:25:19 -0600

    use formyear instead of current year

--- frontend/react/src/components/layout/PageInfo.js
+++ frontend/react/src/components/layout/PageInfo.js
@@ -3,20 +3,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import moment from "moment";
 
-//Dynamically get the year of the form by referencing the path
-const getYear = () => {
-  let linkYear = window.location.href.toString().split("/")[6];
-  return linkYear;
-};
-
-const PageInfo = ({ lastSaved, name, status }) => (
+const PageInfo = ({ lastSaved, name, status, formYear }) => (
   <div className="page-info">
     <div className="edit-info">
       {status ?? "draft"}
       {lastSaved.isValid() && ` | Last Edit: ${lastSaved.format("M/D/YYYY")}`}
     </div>
     <h1>
-      {name} CARTS{} FY{getYear()}
+      {name} CARTS{} FY{formYear}
     </h1>
   </div>
 );
@@ -31,7 +25,7 @@ const mapStateToProps = (state) => ({
   name: state.stateUser.name,
   lastSaved: moment(state.save.lastSave),
   status: state.reportStatus.status,
-  currentYear: state.global.currentYear,
+  formYear: state.global.formYear,
 });
 
 export default connect(mapStateToProps)(PageInfo);
