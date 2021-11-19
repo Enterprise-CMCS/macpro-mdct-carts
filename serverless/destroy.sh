859254018e6b020887512ae66f33e4715ce80cc5
13bfrancis <13bfrancis@gmail.com>
2021-11-19 12:44:06 -0500

    trying something

--- serverless/destroy.sh
+++ serverless/destroy.sh
@@ -9,10 +9,6 @@ stage=$1
 
 #install jq
 yum install jq -y
-#install aws cli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
 
 # A list of protected/important branches/environments/stages.
 protected_stage_regex="(^master$|^val$|^production)"
