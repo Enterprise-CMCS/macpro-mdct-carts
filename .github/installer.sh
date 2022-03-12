afee32afebe85f8726641175e91100b510b8e5d1
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2022-03-12 08:27:46 -0500

    testing grrovy script

--- .github/installer.sh
+++ .github/installer.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
 
 curl -s get.sdkman.io | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install groovy
 groovy -version
+
+
+cd .jenkins/groovy
+groovy -version
+jenkinsUtils.buildAndPushImageToEcr("data/postgres_deployer", "postgres_deployer", env.BRANCH_NAME)
+jenkinsUtils.buildAndPushImageToEcr("frontend/api_postgres", "postgres_django", [env.BUILD_TAG, env.BRANCH_NAME])
