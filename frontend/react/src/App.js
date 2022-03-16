676a3e3510d94d67ee236118f5caf64f8d293008
13bfrancis <13bfrancis@gmail.com>
2022-03-16 12:22:00 -0400

    new comment added

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -5,11 +5,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import WrappedSecurity from "./wrapSecurity";
 
 function App() {
+  // This is where the react app gets kickstartedz
   return (
     <Router>
       <WrappedSecurity />
     </Router>
   );
 }
-
 export default App;
