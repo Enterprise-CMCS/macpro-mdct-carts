a58d74b25cb1f7667fe0463e0523871461dfb718
Ishmeal Baawuah <ibaawuah@ip-10-232-37-74.ec2.internal>
2022-03-21 15:23:54 -0400

    resolving build tag

--- frontend_deploy.sh
+++ frontend_deploy.sh
@@ -13,12 +13,15 @@ workspace=${2}
 action=${3}
 varString1=${4}
 varString2=${5}
+varString3=${6}
 
 
-echo "$varString1"
-echo "$varString2"
+#echo "$varString1"
+#echo "$varString2"
+#echo "$varString3"
 
 cd frontend/aws
+
 
 terraform init -backend-config="bucket=${stateBucket}" -input=false -reconfigure
 
@@ -39,16 +42,14 @@ api_endpoint=$(terraform output prince_api_endpoint)
 
 
 
-export CLOUDFRONT_DISTRIBUTION_ID=${distribution_id}
-export S3_BUCKET_NAME=${bucket_name}
-export API_POSTGRES_UR=${postgres_endpoint}
-export PRINCE_API_ENDPOINT=${api_endpoint}
-
-echo $CLOUDFRONT_DISTRIBUTION_ID
+CLOUDFRONT_DISTRIBUTION_ID=${distribution_id}
+S3_BUCKET_NAME=${bucket_name}
+API_POSTGRES_UR=${postgres_endpoint}
+PRINCE_API_ENDPOINT=${api_endpoint}
 
 
 
-aws s3 cp s3://${stateBucket}/artifacts/${varString1}/cartsbuild.tar.gz cartsbuild.tar.gz
+aws s3 cp s3://${stateBucket}/artifacts/${varString3}/cartsbuild.tar.gz cartsbuild.tar.gz
 tar -xvzf cartsbuild.tar.gz
 #Populate the static archive with the API_POSTGRES_URL before you sync it to the host bucket
 cd build  && ./env.sh  && cd ..
