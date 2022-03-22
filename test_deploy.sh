9dde9601d469295864f75c5617b6ebc8dc2c797a
Ishmeal Baawuah <ibaawuah@ip-10-232-62-194.ec2.internal>
2022-03-22 16:00:51 -0400

    adding file to deploy test stage

---
+++ test_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##Test Workflow Deployment##
+
+set -e
+
+##Initializing terraform
+pushd frontend/aws
+
+#set application_endpoint env variables from terraform output Files
+APPLICATION_ENDPOINT=$(terraform output application_endpoint)
+
+popd
+
+cd tests
+
+set -e
+docker run --rm -e APPLICATION_ENDPOINT=$APPLICATION_ENDPOINT -v $(pwd)/testcafe:/tests testcafe/testcafe chromium /tests/**/*.js
