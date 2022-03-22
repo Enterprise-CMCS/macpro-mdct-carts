5d8321f7b55b1613e3d869eb297f1b7fecacab32
Ishmeal Baawuah <ibaawuah@ip-10-232-62-194.ec2.internal>
2022-03-22 16:00:10 -0400

    running test stage

--- frontend_deploy.sh
+++ frontend_deploy.sh
@@ -2,12 +2,9 @@
 
 ##FRONT-END DEPLOYMENT##
 
-
-
 set -e
 
-
-
+#setting variables
 stateBucket=${1}
 workspace=${2}
 action=${3}
@@ -16,12 +13,8 @@ varString2=${5}
 varString3=${6}
 
 
-#echo "$varString1"
-#echo "$varString2"
-#echo "$varString3"
-
+##Initializing terraform
 cd frontend/aws
-
 
 terraform init -backend-config="bucket=${stateBucket}" -input=false -reconfigure
 
@@ -34,25 +27,15 @@ if ! terraform workspace list | grep -q " ${workspace}\$" ; then
 terraform ${action} -var "${varString1}" -var "${varString2}" -input=false -auto-approve
 
 
-set frontend env variables from terraform output Files
+#set frontend env variables from terraform output Files
 
 CLOUDFRONT_DISTRIBUTION_ID=$(terraform output cloudfront_distribution_id)
 S3_BUCKET_NAME=$(terraform output s3_bucket_name)
 API_POSTGRES_UR=$(terraform output api_postgres_endpoint)
 PRINCE_API_ENDPOINT=$(terraform output prince_api_endpoint)
 
-echo $CLOUDFRONT_DISTRIBUTION_ID
-echo $S3_BUCKET_NAME
-echo $API_POSTGRES_UR
-echo $PRINCE_API_ENDPOINT
 
-#CLOUDFRONT_DISTRIBUTION_ID=${distribution_id}
-#S3_BUCKET_NAME=${bucket_name}
-#API_POSTGRES_UR=${postgres_endpoint}
-#PRINCE_API_ENDPOINT=${api_endpoint}
-
-
-
+#download and unzip artifacts from s3
 aws s3 cp s3://${stateBucket}/artifacts/${varString3}/cartsbuild.tar.gz cartsbuild.tar.gz
 tar -xvzf cartsbuild.tar.gz
 #Populate the static archive with the API_POSTGRES_URL before you sync it to the host bucket
@@ -60,12 +43,3 @@ cd build  && cat env.sh && ./env.sh  && cd ..
 aws s3 sync build s3://${S3_BUCKET_NAME}
 # Cloudfront cache invalidation
 aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
-
-
-
-#outputVar=(
-#'cloudfront_distribution_id'
-#'s3_bucket_name'
-#'api_postgres_endpoint'
-#'prince_api_endpoint'
-#)
