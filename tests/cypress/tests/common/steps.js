32aa220b36587f9246c1701b98fc3566e617fa62
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-04-07 17:23:20 -0400

    update support pages for carts

---
+++ tests/cypress/tests/common/steps.js
@@ -0,0 +1,14 @@
+import Homepage from "../../../support/pages/Homepage";
+import LoginPage from "../../../support/pages/LoginPage";
+
+const homePage = new Homepage();
+const loginPage = new LoginPage();
+
+Given("user visits homepage", () => {
+  homePage.launch();
+});
+
+When("user enters Cognito email and password for login", () => {
+  loginPage.enterEmailwithCognitoLogin();
+  loginPage.enterPasswordwithCognitoLogin();
+});
