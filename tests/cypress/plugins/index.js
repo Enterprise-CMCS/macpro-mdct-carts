9558264e5f04d214a47598e20191abce2d2d5244
karla-vm <karla@coforma.io>
2022-04-11 10:16:41 -0400

    Added a11y testing using Cypress based on MCR code changes

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,4 +1,6 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const { pa11y, prepareAudit } = require("@cypress-audit/pa11y");
+const { lighthouse } = require("@cypress-audit/lighthouse");
 
 module.exports = (on, config) => {
   on("file:preprocessor", cucumber());
@@ -13,5 +15,14 @@ module.exports = (on, config) => {
 
       return null;
     },
+  });
+
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    pa11y: pa11y(),
+    lighthouse: lighthouse(),
   });
 };
