7edc6b753b22b5b91d8a0208fbece3984842fb42
Braxton Excell <braxton.excell@gmail.com>
2022-04-22 10:31:20 -0500

    update semantic html

--- services/ui-src/src/components/sections/UserProfile.js
+++ services/ui-src/src/components/sections/UserProfile.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 
 const UserProfile = ({ currentUser }) => {
   return (
-    <main className="page-info">
+    <div className="page-info">
       <div className="ds-l-col--12 content ds-u-padding-left--4 ">
-        <h1>User Profile</h1>
-        <div className="main">
+        <header>
+          <h1>User Profile</h1>
+        </header>
+        <main className="main">
           If any information is incorrect, please contact the{" "}
           <a href="mailto:mdct_help@cms.hhs.gov">mdct_help@cms.hhs.gov</a>.
           <div className="profile-information">
@@ -30,9 +32,9 @@ const UserProfile = ({ currentUser }) => {
               <div>{currentUser.role}</div>
             </div>
           </div>
-        </div>
+        </main>
       </div>
-    </main>
+    </div>
   );
 };
 
