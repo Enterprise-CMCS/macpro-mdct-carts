e2906e56c410413d46be72ce3e6765494cfd0a6f
Ishmeal Baawuah <ibaawuah@ip-10-232-50-138.ec2.internal>
2022-04-25 08:35:00 -0400

    setting master workflow secrets

--- .github/build_vars.sh
+++ .github/build_vars.sh
@@ -4,9 +4,6 @@ var_list=(
   'AWS_ACCESS_KEY_ID'
   'AWS_SECRET_ACCESS_KEY'
   'AWS_DEFAULT_REGION'
-  'STAGE_PREFIX'
-#  'SLACK_WEBHOOK_URL'
-#  'CODE_CLIMATE_ID'
   'BUILD_TAG'
   'VPC_NAME'
   'OIDC_CLIENT_ID'
@@ -20,9 +17,6 @@ var_list=(
   'TF_VAR_skip_data_deployment'
   'TF_VAR_postgres_custom_password'
   'TF_VAR_use_custom_db_password_info'
-  'SLACK_CREDENTIAL_NAME'
-  'SLACK_CHANNEL_NAME'
-  'SLACK_TEAM_DOMAIN_NAME'
 )
 
 set_value() {
