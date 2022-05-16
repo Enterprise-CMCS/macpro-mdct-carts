cff56d67caf771964139fb06f92096c180de815a
Brandon Bodnar <121035+bodnarbm@users.noreply.github.com>
2022-05-16 11:44:05 -0400

    Fix precommit

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -13,13 +13,13 @@ const passwordForCognito = "//input[@name='password']";
 
 const stateUser = {
   email: Cypress.env("STATE_USER_EMAIL"),
-  password: Cypress.env("STATE_USER_PASSWORD")
+  password: Cypress.env("STATE_USER_PASSWORD"),
 };
 
 const adminUser = {
   email: Cypress.env("ADMIN_USER_EMAIL"),
-  password: Cypress.env("ADMIN_USER_PASSWORD")
-}
+  password: Cypress.env("ADMIN_USER_PASSWORD"),
+};
 
 Cypress.Commands.add("authenticate", (userType, userCredentials) => {
   let credentials = {};
@@ -42,7 +42,7 @@ Cypress.Commands.add("authenticate", (userType, userCredentials) => {
     }
   } else {
     throw new Error("Must specify either userType or userCredentials.");
-  }  
+  }
   cy.xpath(emailForCognito).type(credentials.email);
   cy.xpath(passwordForCognito).type(credentials.password);
   cy.get('[data-cy="login-with-cognito-button"]').click();
