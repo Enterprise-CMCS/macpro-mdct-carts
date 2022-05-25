b7018a1b1256777014b5f9d46b23e84d2cc64027
juliuscaesar <caesarnuzzolo@Caesars-MBP.lan>
2022-05-25 14:59:36 -0400

    lint the backend

--- services/app-api/handlers/state/get.ts
+++ services/app-api/handlers/state/get.ts
@@ -9,7 +9,7 @@ export const getStates = handler(async (_event, _context) => {
   const queryValue = await dynamoDb.scan(params);
   // TODO collect map fmap & acs values
   const states = queryValue.Items?.map((state) => ({
-    ...state as State,
+    ...(state as State),
     ...{ fmap_set: [], acs_set: [] },
   })).sort((a, b) => a.name.localeCompare(b.name));
   return states;
