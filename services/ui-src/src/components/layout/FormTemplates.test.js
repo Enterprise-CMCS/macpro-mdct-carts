ba47938ad6b54754521dcfcb2f3f4923d9d80d4c
brendan <brendan@coforma.io>
2022-06-06 12:56:14 -0400

    layout folder tests

---
+++ services/ui-src/src/components/layout/FormTemplates.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { shallow } from "enzyme";
+import FormTemplates from "./FormTemplates";
+import { act, render, fireEvent } from "@testing-library/react";
+
+const mockPost = jest.fn();
+jest.mock("aws-amplify", () => ({
+  __esModule: true,
+  API: {
+    post: () => mockPost(),
+  },
+}));
+jest.mock("../../hooks/authHooks/requestOptions");
+window.alert = jest.fn();
+
+const mockHistoryPush = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+const formTemplate = <FormTemplates />;
+describe("FormTemplates Component", () => {
+  beforeEach(() => {
+    window.alert.mockClear();
+  });
+
+  it("should render correctly", () => {
+    expect(shallow(formTemplate).exists()).toBe(true);
+  });
+
+  it("fires the generate forms event on button click, then navigates", async () => {
+    const { getByTestId } = render(formTemplate);
+    const generateButton = getByTestId("generate-forms-button");
+    await act(async () => {
+      fireEvent.click(generateButton);
+    });
+    expect(mockPost).toHaveBeenCalled();
+    expect(mockHistoryPush).toHaveBeenCalledWith("/");
+  });
+});
