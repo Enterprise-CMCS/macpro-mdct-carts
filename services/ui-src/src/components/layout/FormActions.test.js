a8cf54c29bb885c4a92765056462380733332891
brendan <brendan@coforma.io>
2022-06-09 09:08:49 -0400

    don't commit broken tests

--- services/ui-src/src/components/layout/FormActions.test.js
+++ services/ui-src/src/components/layout/FormActions.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { shallow } from "enzyme";
 import FormActions from "./FormActions";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
 
-const form = <FormActions />;
+const mockStore = configureMockStore();
+const store = mockStore({});
+const formActions = (
+  <Provider store={store}>
+    <FormActions />
+  </Provider>
+);
 
 describe("Fill Form Component", () => {
   it("should render correctly", () => {
-    expect(shallow(form).exists()).toBe(true);
+    expect(shallow(formActions).exists()).toBe(true);
   });
 });
