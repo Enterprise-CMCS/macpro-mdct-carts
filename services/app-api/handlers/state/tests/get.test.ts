da911f3aab12d8dc1389504fa43661345e5ddc2a
karla-vm <karla@coforma.io>
2022-06-17 14:33:25 -0400

    Added unit tests for /states endpoint

---
+++ services/app-api/handlers/state/tests/get.test.ts
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+import { getStates } from "../get";
+import dbLib from "../../../libs/dynamodb-lib";
+import { APIGatewayProxyEvent } from "aws-lambda"; // eslint-disable-line no-unused-vars
+import { testEvent } from "../../../test-util/testEvents";
+
+jest.mock("../../../libs/dynamodb-lib", () => ({
+  __esModule: true,
+  default: {
+    get: jest.fn(),
+    scan: jest.fn().mockReturnValue({
+      Items: [
+        {
+          programType: "combo",
+          code: "AL",
+          name: "Alabama",
+          programNames: {},
+          fmapSet: [{}],
+          acsSet: [{}],
+        },
+      ],
+    }),
+  },
+}));
+
+jest.mock("../../../libs/authorization", () => ({
+  __esModule: true,
+  isAuthorized: jest.fn().mockReturnValue(true),
+}));
+
+jest.mock("../../dynamoUtils/convertToDynamoExpressionVars", () => ({
+  __esModule: true,
+  convertToDynamoExpression: jest.fn(),
+}));
+
+describe("Test Get States Handlers", () => {
+  test("fetching states should return all state data", async () => {
+    const event: APIGatewayProxyEvent = {
+      ...testEvent,
+    };
+
+    const res = await getStates(event, null);
+
+    expect(res.statusCode).toBe(200);
+    expect(dbLib.scan).toHaveBeenCalledWith({
+      TableName: undefined,
+    });
+  });
+});
