51a50c1b98d5d5020db064d5482144047db51bbf
karla-vm <karla@coforma.io>
2022-06-21 15:41:53 -0400

    Removing seed files

--- services/app-api/handlers/state/get.ts
+++ services/app-api/handlers/state/get.ts
@@ -9,22 +9,8 @@ export const getStates = handler(async (_event, _context) => {
 
   const queryValue = await dynamoDb.scan(params);
 
-  // eslint-disable-next-line
-  // let acsParams = {
-  //   TableName: process.env.acsTableName!,
-  // };
-  // let fmapParams = {
-  //   TableName: process.env.fmapTableName!,
-  // };
-  // todo grab from other tables
-  // let acsQueryValue = await dynamoDb.scan(acsParams);
-  // console.log("acs", acsQueryValue);
-  // let fmapQueryValue = await dynamoDb.scan(fmapParams);
-  // console.log("fmap", fmapQueryValue);
-
   const states = queryValue.Items?.map((state) => ({
     ...(state as State),
-    //...{ fmapSet: [], acsSet: [] },
   })).sort((a, b) => a.name.localeCompare(b.name));
   return states;
 });
