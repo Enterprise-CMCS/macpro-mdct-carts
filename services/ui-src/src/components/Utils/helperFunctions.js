6fa8ad0a1ffd44274ef1232ae3e0fa27d1ebb803
James Dinh <jdinh@collabralink.com>
2022-07-01 07:18:08 -0700

    508 - Update PDF Code into v3 of CARTS (#2375) (#3166)
    * 508 pdf (#2375)
    
    * Fix empty tables
    
    * Adjust section headers
    
    * Add back in styles-components 🙄
    
    * fix label 508 issue (#2320)
    
    Co-authored-by: 13bfrancis <13bfrancis@gmail.com>
    
    * moved label and hint
    
    * Remove Choice comp
    
    * major progress on logical reading order
    
    * Update Checkbox.js
    
    * minimize margin for radio button
    
    * Modify radio and checkbox components
    
    * fix key issue (thanks ben)
    
    * remove console.log statements
    
    * add table summaries
    
    * fix prop type errors
    
    * code cleanup
    
    * Update frontend/react/src/components/fields/Checkbox.js
    
    Co-authored-by: Benjamin Paige <benjamin.paige@gmail.com>
    
    * remove log statements
    
    Co-authored-by: Benjamin Paige <benjamin.paige@gmail.com>
    
    * remove extra title
    
    * pull in 401 changes
    
    * cleaned logic to match brians 503 work
    
    * changed logic to match Brians accessibility work
    
    * removed spacing
    
    * fix issues found by peer review
    
    * changed spacing
    
    Co-authored-by: 13bfrancis <40218571+13bfrancis@users.noreply.github.com>
    Co-authored-by: Benjamin Paige <benjamin.paige@gmail.com>

--- services/ui-src/src/components/Utils/helperFunctions.js
+++ services/ui-src/src/components/Utils/helperFunctions.js
@@ -12,4 +12,23 @@ const showQuestionByPath = (path) => {
   return pageDisable;
 };
 
-export { showQuestionByPath };
+const generateQuestionNumber = (id) => {
+  let labelBits = "";
+
+  if (id) {
+    const lastHunk = Number.parseInt(id.substring(id.length - 2), 10);
+    if (Number.isNaN(lastHunk)) {
+      const numberBit = Number.parseInt(
+        id.substring(id.length - 4, id.length - 2),
+        10
+      );
+      labelBits = `${numberBit}${id.substring(id.length - 1)}. `;
+    } else {
+      labelBits = `${lastHunk}. `;
+    }
+  }
+
+  return labelBits;
+};
+
+export { showQuestionByPath, generateQuestionNumber };
