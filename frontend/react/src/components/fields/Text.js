cfe4c9faee2421f54340ff7f3095be6890d2d9b3
James Dinh <jdinh@collabralink.com>
2022-07-11 09:11:47 -0700

    Added fix for PDFs to show all text in a Textarea (#3344)
    * added to print all of a textbox text into a p tag so all the text can get rendered
    
    * remove unusued code

--- frontend/react/src/components/fields/Text.js
+++ frontend/react/src/components/fields/Text.js
@@ -78,15 +78,20 @@ const Text = ({ question, state, ...props }) => {
         question.label
       }`}</label>
       {question.hint && <p className="ds-c-field__hint">{question.hint}</p>}
-      <TextField
-        id={question.id}
-        value={
-          prevYearValue || (question.answer && question.answer.entry) || ""
-        }
-        type="text"
-        {...props}
-        disabled={prevYearDisabled || !!props.disabled}
-      />
+      <div className="non-print-textarea">
+        <TextField
+          id={question.id}
+          value={
+            prevYearValue || (question.answer && question.answer.entry) || ""
+          }
+          type="text"
+          {...props}
+          disabled={prevYearDisabled || !!props.disabled}
+        />
+      </div>
+      <p className="print-text-area">
+        {prevYearValue || (question.answer && question.answer.entry) || ""}
+      </p>
     </>
   );
 };
