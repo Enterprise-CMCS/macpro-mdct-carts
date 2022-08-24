5b676746809dad9549da0a23f32befa886f911c9
brendan <brendan@coforma.io>
2022-08-24 17:11:19 -0400

    topic name

--- services/carts-bigmac-streams/libs/kafka-source-lib.js
+++ services/carts-bigmac-streams/libs/kafka-source-lib.js
@@ -81,9 +81,9 @@ class KafkaSourceLib {
 
   topic(t) {
     if (this.version) {
-      return `${this.topicPrefix}.${t}.${this.version}`;
+      return `${this.topicPrefix}${t}.${this.version}`;
     } else {
-      return `${this.topicPrefix}.${t}`;
+      return `${this.topicPrefix}${t}`;
     }
   }
 
