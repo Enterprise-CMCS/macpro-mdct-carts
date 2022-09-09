c260751e09e3aa9707af59821ffed2a989a51593
karla-vm <karla@coforma.io>
2022-09-09 11:37:11 -0400

    And this?

--- tests/cypress/tests/integration/submitAndUncertify.spec.js
+++ tests/cypress/tests/integration/submitAndUncertify.spec.js
@@ -15,12 +15,9 @@ describe("CARTS Submit and Uncertify Integration Tests", () => {
     cy.authenticate("stateUser");
 
     // certify and submit report
-    cy.get(actionButton, { timeout: 30000 })
-      .should("be.visible")
-      .contains("Edit")
-      .click();
+    cy.get(actionButton, { timeout: 30000 }).contains("Edit").click();
     cy.contains("Certify and Submit").click();
-    cy.get(certifySubmitButton).should("be.visible").click();
+    cy.get(certifySubmitButton).click();
     cy.get("button").contains("Confirm Certify and Submit").click();
     cy.get("button").contains("Return Home").click();
 
@@ -32,11 +29,7 @@ describe("CARTS Submit and Uncertify Integration Tests", () => {
     cy.authenticate("reviewer");
 
     // uncertify report
-    cy.get(uncertifyButton)
-      .first()
-      .should("be.visible")
-      .contains("Uncertify")
-      .click();
+    cy.get(uncertifyButton).first().contains("Uncertify").click();
     cy.get("button").contains("Yes, Uncertify").click();
 
     // log back out
