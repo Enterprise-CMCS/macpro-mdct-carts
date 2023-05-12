c899faa25a8ece562e366a7980e1960fab721ae7
brittbujacich <britt.bujacich@gmail.com>
2023-05-12 09:44:47 -0600

    wip

--- services/database/handlers/dataCorrections/sectionFiveCorrections.js
+++ services/database/handlers/dataCorrections/sectionFiveCorrections.js
@@ -22,31 +22,37 @@ async function myHandler(event, context, callback) {
   dynamoClient = new aws.DynamoDB.DocumentClient(dynamoConfig);
 
   const results = await scan(dynamoClient);
-  console.log(results, 'results');
+  console.log(results, "results");
 
   const transformed = transform(results);
-  console.log(transformed, 'transformed');
+  console.log(transformed, "transformed");
 
   // const keys = ["pk", "sectionId"];
   // updateItems(`${dynamoPrefix}-section`, items, keys)
   return results;
   // eslint-disable-next-line no-console
-//   console.log("Completed data fix");
+  //   console.log("Completed data fix");
 }
 
 async function transform(items) {
-    const transformed = items.map((item) => {
-      item.contents.section.subsections.parts.questions[2].questions[7].fieldset_info.headers.contents[0] = "";
-      item.contents.section.subsections.parts.questions[0].questions[7].fieldset_info.headers.contents[1] = "FFY 2022";
-      item.contents.section.subsections.parts.questions[0].questions[7].fieldset_info.headers.contents[2] = "FFY 2023";
-      item.contents.section.subsections.parts.questions[0].questions[7].fieldset_info.headers.contents[3] = "FFY 2024";
-      // item.contents.section.subsections.parts.questions.questions.fieldset_info.rows.targets.lookupFmapFy[0] = "2022";
-      // item.contents.section.subsections.parts.questions.questions.fieldset_info.rows.targets.lookupFmapFy[1] = "2023";
-      // item.contents.section.subsections.parts.questions.questions.fieldset_info.rows.targets.lookupFmapFy[2] = "2024";
+  const transformed = items.map((item) => {
+    item.contents.section.subsections[0].parts[0].questions[5].fieldset_info.headers[0].contents[0] =
+      "";
+    item.contents.section.subsections[0].parts[0].questions[5].fieldset_info.headers[0].contents[1] =
+      "FFY 2022";
+    item.contents.section.subsections[0].parts[0].questions[5].fieldset_info.headers[0].contents[2] =
+      "FFY 2023";
+    item.contents.section.subsections[0].parts[0].questions[5].fieldset_info.headers[0].contents[3] =
+      "FFY 2024";
+    item.contents.section.subsections[0].parts[0].questions[8].fieldset_info.rows[0].targets[0].lookupFmapFy =
+      "2022";
+    item.contents.section.subsections[0].parts[0].questions[8].fieldset_info.rows[0].targets[1].lookupFmapFy =
+      "2023";
+    item.contents.section.subsections[0].parts[0].questions[8].fieldset_info.rows[0].targets[2].lookupFmapFy =
+      "2024";
+  });
 
-    });
-    
-    return transformed;
+  return transformed;
 }
 
 async function scan(dynamoClient) {
@@ -57,14 +63,14 @@ async function scan(dynamoClient) {
   const queryParams = {
     TableName: `${dynamoPrefix}-section`,
     ExpressionAttributeNames: {
-        "#year": "year",
-        "#sectionId": "sectionId",
-      },
-      ExpressionAttributeValues: {
-        ":year": 2022,
-        ":sectionId": 5,
-      },
-      FilterExpression: "#year = :year AND #sectionId = :sectionId",
+      "#year": "year",
+      "#sectionId": "sectionId",
+    },
+    ExpressionAttributeValues: {
+      ":year": 2022,
+      ":sectionId": 5,
+    },
+    FilterExpression: "#year = :year AND #sectionId = :sectionId",
   };
 
   const queryTable = async (startingKey) => {
@@ -89,45 +95,45 @@ async function scan(dynamoClient) {
 }
 
 const updateItems = async (tableName, items, keys) => {
-    try {
-      for (const item of items) {
-        let key = {};
-        for (const k of keys) {
-          key[k] = item[k];
-          delete item[k];
-        }
-  
-        const params = {
-          TableName: tableName,
-          Key: key,
-          ...convertToDynamoExpression(item),
-        };
-        await dynamoClient.update(params).promise();
+  try {
+    for (const item of items) {
+      let key = {};
+      for (const k of keys) {
+        key[k] = item[k];
+        delete item[k];
       }
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log(` -- ERROR UPLOADING ${tableName}\n`, e);
+
+      const params = {
+        TableName: tableName,
+        Key: key,
+        ...convertToDynamoExpression(item),
+      };
+      await dynamoClient.update(params).promise();
     }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(` -- ERROR UPLOADING ${tableName}\n`, e);
+  }
+};
+
+const convertToDynamoExpression = (listOfVars) => {
+  let expressionAttributeNames = {};
+  let expressionAttributeValues = {};
+  let updateExpression = "";
+  Object.keys(listOfVars).forEach((key, index) => {
+    expressionAttributeNames[`#${key}`] = key;
+    expressionAttributeValues[`:${key}`] = listOfVars[key];
+
+    updateExpression =
+      index === 0
+        ? `set #${key}=:${key}`
+        : `${updateExpression}, #${key}=:${key}`;
+  });
+  return {
+    UpdateExpression: updateExpression,
+    ExpressionAttributeNames: expressionAttributeNames,
+    ExpressionAttributeValues: expressionAttributeValues,
   };
-  
-  const convertToDynamoExpression = (listOfVars) => {
-    let expressionAttributeNames = {};
-    let expressionAttributeValues = {};
-    let updateExpression = "";
-    Object.keys(listOfVars).forEach((key, index) => {
-      expressionAttributeNames[`#${key}`] = key;
-      expressionAttributeValues[`:${key}`] = listOfVars[key];
-  
-      updateExpression =
-        index === 0
-          ? `set #${key}=:${key}`
-          : `${updateExpression}, #${key}=:${key}`;
-    });
-    return {
-      UpdateExpression: updateExpression,
-      ExpressionAttributeNames: expressionAttributeNames,
-      ExpressionAttributeValues: expressionAttributeValues,
-    };
-  };
+};
 
 exports.handler = myHandler;
