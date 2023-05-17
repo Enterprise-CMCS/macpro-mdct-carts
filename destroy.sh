6f105147dac0149280d52e6329b123a3f91485c5
Oren Weingrod <124941331+oweingrod-coforma@users.noreply.github.com>
2023-05-17 14:05:02 -0400

    Error with variable substitution (#139360)

--- destroy.sh
+++ destroy.sh
@@ -107,7 +107,9 @@ done
 
 echo "Removing certificate from stage"
 
-restApiId=$(aws apigateway get-rest-apis | jq -r '.[] | .[] |  select(.name=="fix-destroy-issues-app-api") | .id|tostring')
+restApiName=$stage-app-api
+
+restApiId=$(aws apigateway get-rest-apis | jq -r ".[] | .[] |  select(.name==\"$restApiName\") | .id|tostring")
 
 aws apigateway update-stage \
   --rest-api-id $restApiId \
