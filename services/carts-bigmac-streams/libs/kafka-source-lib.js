61c322c55a858e2131d3ebe344acce17d52a0b1e
Ishmeal Baawuah <ibaawuah@ip-10-232-50-232.ec2.internal>
2023-06-30 11:19:20 -0400

    modifying kafka-source-lib.js

--- services/carts-bigmac-streams/libs/kafka-source-lib.js
+++ services/carts-bigmac-streams/libs/kafka-source-lib.js
@@ -8,6 +8,100 @@ const { STSClient, AssumeRoleCommand } = require("@aws-sdk/client-sts");
 const STAGE = process.env.STAGE;
 let kafka; // Declare kafka variable
 
+class KafkaSourceLib {
+  unmarshallOptions = {
+    convertEmptyValues: true,
+    wrapNumbers: true,
+  };
+
+  stringify(e, prettyPrint) {
+    if (prettyPrint === true) return JSON.stringify(e, null, 2);
+    return JSON.stringify(e);
+  }
+
+  determineTopicName(streamARN) {
+    for (const table of this.tables) {
+      if (streamARN.includes(`/${STAGE}-${table}/`)) return this.topic(table);
+    }
+  }
+
+  unmarshall(r) {
+    return AWS.DynamoDB.Converter.unmarshall(r, this.unmarshallOptions);
+  }
+
+  createPayload(record) {
+    return this.createDynamoPayload(record);
+  }
+
+  createDynamoPayload(record) {
+    const dynamodb = record.dynamodb;
+    const { eventID, eventName } = record;
+    const dynamoRecord = {
+      NewImage: this.unmarshall(dynamodb.NewImage),
+      OldImage: this.unmarshall(dynamodb.OldImage),
+      Keys: this.unmarshall(dynamodb.Keys),
+    };
+    return {
+      key: Object.values(dynamoRecord.Keys).join("#"),
+      value: this.stringify(dynamoRecord),
+      partition: 0,
+      headers: { eventID: eventID, eventName: eventName },
+    };
+  }
+
+  topic(t) {
+    if (this.version) {
+      return `${this.topicPrefix}${t}.${this.version}`;
+    } else {
+      return `${this.topicPrefix}${t}`;
+    }
+  }
+
+  createOutboundEvents(records) {
+    let outboundEvents = {};
+    for (const record of records) {
+      const topicName = this.determineTopicName(
+        String(record.eventSourceARN.toString())
+      );
+
+      const dynamoPayload = this.createPayload(record);
+
+      //initialize configuration object keyed to topic for quick lookup
+      if (!(outboundEvents[topicName] instanceof Object))
+        outboundEvents[topicName] = {
+          topic: topicName,
+          messages: [],
+        };
+
+      //add messages to messages array for corresponding topic
+      outboundEvents[topicName].messages.push(dynamoPayload);
+    }
+    return outboundEvents;
+  }
+
+  async handler(event) {
+    if (!connected) {
+      console.log("Attempting connection...");
+      await producer.connect();
+      connected = true;
+    }
+    console.log("Raw event", this.stringify(event, true));
+
+    if (event.Records) {
+      const outboundEvents = this.createOutboundEvents(event.Records);
+
+      const topicMessages = Object.values(outboundEvents);
+      console.log(
+        `Batch configuration: ${this.stringify(topicMessages, true)}`
+      );
+
+      await producer.sendBatch({ messages: topicMessages });
+    }
+
+    console.log(`Successfully processed ${event.Records.length} records.`);
+  }
+}
+
 async function initializeKafka() {
   const sasl = await getMechanism("us-east-1", process.env.bigmacRoleArn);
 
@@ -33,100 +127,6 @@ async function initializeKafka() {
   signalTraps.map((type) => {
     process.once(type, producer.disconnect);
   });
-
-  class KafkaSourceLib {
-    unmarshallOptions = {
-      convertEmptyValues: true,
-      wrapNumbers: true,
-    };
-
-    stringify(e, prettyPrint) {
-      if (prettyPrint === true) return JSON.stringify(e, null, 2);
-      return JSON.stringify(e);
-    }
-
-    determineTopicName(streamARN) {
-      for (const table of this.tables) {
-        if (streamARN.includes(`/${STAGE}-${table}/`)) return this.topic(table);
-      }
-    }
-
-    unmarshall(r) {
-      return AWS.DynamoDB.Converter.unmarshall(r, this.unmarshallOptions);
-    }
-
-    createPayload(record) {
-      return this.createDynamoPayload(record);
-    }
-
-    createDynamoPayload(record) {
-      const dynamodb = record.dynamodb;
-      const { eventID, eventName } = record;
-      const dynamoRecord = {
-        NewImage: this.unmarshall(dynamodb.NewImage),
-        OldImage: this.unmarshall(dynamodb.OldImage),
-        Keys: this.unmarshall(dynamodb.Keys),
-      };
-      return {
-        key: Object.values(dynamoRecord.Keys).join("#"),
-        value: this.stringify(dynamoRecord),
-        partition: 0,
-        headers: { eventID: eventID, eventName: eventName },
-      };
-    }
-
-    topic(t) {
-      if (this.version) {
-        return `${this.topicPrefix}${t}.${this.version}`;
-      } else {
-        return `${this.topicPrefix}${t}`;
-      }
-    }
-
-    createOutboundEvents(records) {
-      let outboundEvents = {};
-      for (const record of records) {
-        const topicName = this.determineTopicName(
-          String(record.eventSourceARN.toString())
-        );
-
-        const dynamoPayload = this.createPayload(record);
-
-        //initialize configuration object keyed to topic for quick lookup
-        if (!(outboundEvents[topicName] instanceof Object))
-          outboundEvents[topicName] = {
-            topic: topicName,
-            messages: [],
-          };
-
-        //add messages to messages array for corresponding topic
-        outboundEvents[topicName].messages.push(dynamoPayload);
-      }
-      return outboundEvents;
-    }
-
-    async handler(event) {
-      if (!connected) {
-        console.log("Attempting connection...");
-        await producer.connect();
-        connected = true;
-      }
-      console.log("Raw event", this.stringify(event, true));
-
-      if (event.Records) {
-        const outboundEvents = this.createOutboundEvents(event.Records);
-
-        const topicMessages = Object.values(outboundEvents);
-        console.log(
-          `Batch configuration: ${this.stringify(topicMessages, true)}`
-        );
-
-        await producer.sendBatch({ messages: topicMessages });
-      }
-
-      console.log(`Successfully processed ${event.Records.length} records.`);
-    }
-  }
 
   return new KafkaSourceLib();
 }
