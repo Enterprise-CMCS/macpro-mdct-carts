3a65e2ad0c4aca6b63a060b4b115aa7a54d234b2
Ishmeal Baawuah <ibaawuah@ip-10-232-50-232.ec2.internal>
2023-06-30 11:27:14 -0400

    modifying kafka-source-lib.js

--- services/carts-bigmac-streams/libs/kafka-source-lib.js
+++ services/carts-bigmac-streams/libs/kafka-source-lib.js
@@ -4,35 +4,58 @@ const {
   createMechanism,
 } = require("@jm18457/kafkajs-msk-iam-authentication-mechanism");
 const { STSClient, AssumeRoleCommand } = require("@aws-sdk/client-sts");
-
 const STAGE = process.env.STAGE;
-let kafka; // Declare kafka variable
-
+const kafka = new Kafka({
+  clientId: `carts-${STAGE}`,
+  brokers: process.env.BOOTSTRAP_BROKER_STRING_TLS.split(","),
+  retry: {
+    initialRetryTime: 300,
+    retries: 8,
+  },
+  ssl: true,
+  sasl: await getMechanism("us-east-1", process.env.bigmacRoleArn),
+  requestTimeout: 295000
+});
+const producer = kafka.producer();
+let connected = false;
+const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2", "beforeExit"];
+signalTraps.map((type) => {
+  process.removeListener(type, producer.disconnect);
+});
+signalTraps.map((type) => {
+  process.once(type, producer.disconnect);
+});
 class KafkaSourceLib {
+  /*
+   *Event types:
+   *cmd – command; restful publish
+   *cdc – change data capture; record upsert/delete in data store
+   *sys – system event; send email, archive logs
+   *fct – fact; user activity, notifications, logs
+   *
+   *topicPrefix = "[data_center].[system_of_record].[business_domain].[event_type]";
+   *version = "some version";
+   *tables = [list of tables];
+   */
   unmarshallOptions = {
     convertEmptyValues: true,
     wrapNumbers: true,
   };
-
   stringify(e, prettyPrint) {
     if (prettyPrint === true) return JSON.stringify(e, null, 2);
     return JSON.stringify(e);
   }
-
   determineTopicName(streamARN) {
     for (const table of this.tables) {
       if (streamARN.includes(`/${STAGE}-${table}/`)) return this.topic(table);
     }
   }
-
   unmarshall(r) {
     return AWS.DynamoDB.Converter.unmarshall(r, this.unmarshallOptions);
   }
-
   createPayload(record) {
     return this.createDynamoPayload(record);
   }
-
   createDynamoPayload(record) {
     const dynamodb = record.dynamodb;
     const { eventID, eventName } = record;
@@ -48,7 +71,6 @@ class KafkaSourceLib {
       headers: { eventID: eventID, eventName: eventName },
     };
   }
-
   topic(t) {
     if (this.version) {
       return `${this.topicPrefix}${t}.${this.version}`;
@@ -56,79 +78,45 @@ class KafkaSourceLib {
       return `${this.topicPrefix}${t}`;
     }
   }
-
   createOutboundEvents(records) {
     let outboundEvents = {};
     for (const record of records) {
       const topicName = this.determineTopicName(
         String(record.eventSourceARN.toString())
       );
-
       const dynamoPayload = this.createPayload(record);
-
       //initialize configuration object keyed to topic for quick lookup
       if (!(outboundEvents[topicName] instanceof Object))
         outboundEvents[topicName] = {
           topic: topicName,
           messages: [],
         };
-
       //add messages to messages array for corresponding topic
       outboundEvents[topicName].messages.push(dynamoPayload);
     }
     return outboundEvents;
   }
-
   async handler(event) {
     if (!connected) {
+      // eslint-disable-next-line no-console
       console.log("Attempting connection...");
       await producer.connect();
       connected = true;
     }
+    // eslint-disable-next-line no-console
     console.log("Raw event", this.stringify(event, true));
-
     if (event.Records) {
       const outboundEvents = this.createOutboundEvents(event.Records);
-
       const topicMessages = Object.values(outboundEvents);
+      // eslint-disable-next-line no-console
       console.log(
         `Batch configuration: ${this.stringify(topicMessages, true)}`
       );
-
-      await producer.sendBatch({ messages: topicMessages });
+      await producer.sendBatch({ topicMessages });
     }
-
+    // eslint-disable-next-line no-console
     console.log(`Successfully processed ${event.Records.length} records.`);
   }
-}
-
-async function initializeKafka() {
-  const sasl = await getMechanism("us-east-1", process.env.bigmacRoleArn);
-
-  kafka = new Kafka({
-    clientId: `carts-${STAGE}`,
-    brokers: process.env.BOOTSTRAP_BROKER_STRING_TLS.split(","),
-    retry: {
-      initialRetryTime: 300,
-      retries: 8,
-    },
-    ssl: true,
-    sasl: sasl,
-  });
-
-  const producer = kafka.producer();
-  let connected = false;
-  const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2", "beforeExit"];
-
-  signalTraps.map((type) => {
-    process.removeListener(type, producer.disconnect);
-  });
-
-  signalTraps.map((type) => {
-    process.once(type, producer.disconnect);
-  });
-
-  return new KafkaSourceLib();
 }
 
 async function getMechanism(region, role) {
@@ -145,20 +133,11 @@ async function getMechanism(region, role) {
   return createMechanism({
     region,
     credentials: {
-      authorizationIdentity: crossAccountRoleData.AssumedRoleUser.AssumedRoleId,
+      authorizationIdentity: crossAccountRoleData.AssumedRoleUser.AssumeRoleId,
       accessKeyId: crossAccountRoleData.Credentials.AccessKeyId,
       secretAccessKey: crossAccountRoleData.Credentials.SecretAccessKey,
       sessionToken: crossAccountRoleData.Credentials.SessionToken,
     },
   });
 }
-
-// Call the async function to initialize Kafka
-initializeKafka()
-  .then(() => {})
-  .catch((error) => {
-    // Handle any errors that occurred during initialization
-    console.error("Failed to initialize Kafka:", error);
-  });
-
-export default KafkaSourceLib; // Export the KafkaSourceLib class
+export default KafkaSourceLib;
