2012b4d6b81c0f696037328350694d4caf0d9c13
ailZhou <aileen.zhou@coforma.io>
2023-08-04 13:59:00 -0400

    Update submitAndUncertify.spec.js

--- tests/cypress/tests/integration/submitAndUncertify.spec.js
+++ tests/cypress/tests/integration/submitAndUncertify.spec.js
@@ -1,43 +1,47 @@
-// element selectors
+/* element selectors
 const actionButton = "[data-testid='report-action-button']";
 const certifySubmitButton = "[data-testid='certifySubmit']";
 const uncertifyButton = "[data-testid='uncertifyButton']";
+*/
+
 describe("CARTS Submit and Uncertify Integration Tests", () => {
   it("Should submit form as a State User and uncertify as a Reviewer", () => {
     cy.ensureAvailableReport(); // Needs to happen each iteration of the test
 
-    // // log in as State User
-    // cy.authenticate("stateUser");
+    /*
+    // log in as State User
+    cy.authenticate("stateUser");
 
-    // // certify and submit report
-    // cy.get(actionButton, { timeout: 30000 }).contains("Edit").click();
-    // cy.wait(3000);
-    // cy.contains("Certify and Submit").click();
-    // cy.wait(3000);
+    // certify and submit report
+    cy.get(actionButton, { timeout: 30000 }).contains("Edit").click();
+    cy.wait(3000);
+    cy.contains("Certify and Submit").click();
+    cy.wait(3000);
 
-    // cy.get(certifySubmitButton).click();
-    // cy.get("button").contains("Confirm Certify and Submit").click();
-    // cy.get("button").contains("Return Home").click();
+    cy.get(certifySubmitButton).click();
+    cy.get("button").contains("Confirm Certify and Submit").click();
+    cy.get("button").contains("Return Home").click();
 
-    // cy.logout();
+    cy.logout();
 
-    // // log in as CMS Admin (user who can uncertify)
-    // cy.authenticate("adminUser");
+    // log in as CMS Admin (user who can uncertify)
+    cy.authenticate("adminUser");
 
-    // // uncertify report - Scope to test user's state
-    // cy.get(".dropdown-heading").first().click();
-    // cy.contains("Alabama").click();
-    // cy.get("body").click(0, 0);
-    // cy.get(".filter-button").contains("Filter").click();
-    // cy.wait(3000);
+    // uncertify report - Scope to test user's state
+    cy.get(".dropdown-heading").first().click();
+    cy.contains("Alabama").click();
+    cy.get("body").click(0, 0);
+    cy.get(".filter-button").contains("Filter").click();
+    cy.wait(3000);
 
-    // cy.get(uncertifyButton).first().contains("Uncertify").click();
-    // cy.get("button").contains("Yes, Uncertify").click();
+    cy.get(uncertifyButton).first().contains("Uncertify").click();
+    cy.get("button").contains("Yes, Uncertify").click();
 
-    // cy.logout();
+    cy.logout();
 
-    // // log back in as State User - the report should be "In Progress" again
-    // cy.authenticate("stateUser");
-    // cy.get(actionButton).contains("Edit").should("be.visible");
+    // log back in as State User - the report should be "In Progress" again
+    cy.authenticate("stateUser");
+    cy.get(actionButton).contains("Edit").should("be.visible");
+    */
   });
 });
