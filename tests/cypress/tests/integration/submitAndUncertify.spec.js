7713925b80fa69a0e07028ed877273fc5cfa3eac
ailZhou <aileen.zhou@coforma.io>
2023-08-04 14:19:03 -0400

    Update submitAndUncertify.spec.js

--- tests/cypress/tests/integration/submitAndUncertify.spec.js
+++ tests/cypress/tests/integration/submitAndUncertify.spec.js
@@ -1,5 +1,24 @@
+//element selectors
+const actionButton = "[data-testid='report-action-button']";
+const certifySubmitButton = "[data-testid='certifySubmit']";
+
 describe("CARTS Submit and Uncertify Integration Tests", () => {
   it("Should submit form as a State User and uncertify as a Reviewer", () => {
     cy.ensureAvailableReport(); // Needs to happen each iteration of the test
+
+    // log in as State User
+    cy.authenticate("stateUser");
+
+    // certify and submit report
+    cy.get(actionButton, { timeout: 30000 }).contains("Edit").click();
+    cy.wait(3000);
+    cy.contains("Certify and Submit").click();
+    cy.wait(3000);
+
+    cy.get(certifySubmitButton).click();
+    cy.get("button").contains("Confirm Certify and Submit").click();
+    cy.get("button").contains("Return Home").click();
+
+    cy.logout();
   });
 });
