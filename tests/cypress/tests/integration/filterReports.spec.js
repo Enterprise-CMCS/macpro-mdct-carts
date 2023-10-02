a73a443688d60cd5e78a2913ccac85cce01d8f00
Garrett Rabian <garrett@coforma.io>
2023-10-02 13:01:55 -0400

    cleanup filter report spec

--- tests/cypress/tests/integration/filterReports.spec.js
+++ tests/cypress/tests/integration/filterReports.spec.js
@@ -1,111 +1,102 @@
+const stateDropdownSelector = '[data-cy="cms-homepage-state-dropdown"]';
+const yearDropdownSelector = '[data-cy="cms-homepage-year-dropdown"]';
+const statusDropdownSelector = '[data-cy="cms-homepage-status-dropdown"]';
+
+const reportsSelector = '[data-cy="cms-homepage-reports"]';
+const filterSubmitSelector = '[data-cy="cms-homepage-filter-submit"]';
+const clearFilterSelector = '[data-cy="cms-homepage-filter-clear"]';
+
 describe("Check Report Filtering as a CMS Admin", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.authenticate("adminUser");
-    cy.get('[data-cy="cms-homepage-filter-clear"]').click();
+    cy.get(clearFilterSelector).click();
   });
 
   it("Should display all provided reports when no filters activated", () => {
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
+    cy.get(reportsSelector).as("reports");
   });
 
   it("Should display the correct reports when filtered by state", () => {
-    cy.get('[data-cy="cms-homepage-state-dropdown"] > div > div > div').as(
-      "state-dropdown"
-    );
-    cy.get("@state-dropdown").click();
+    cy.get(stateDropdownSelector).click();
 
-    cy.get('[data-cy="cms-homepage-state-dropdown"] > div > div')
+    cy.get(stateDropdownSelector)
       .find(".dropdown-content")
-      .as("listOfStates");
-    cy.get("@listOfStates").children().contains("Alabama").click();
+      .children()
+      .contains("Alabama")
+      .click();
 
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
-    cy.get("@reports").click();
+    cy.get(reportsSelector).click();
 
-    cy.get('[data-cy="cms-homepage-filter-submit"]').click();
+    cy.get(filterSubmitSelector).click();
   });
 
   it("Should display the correct reports when filtered by year", () => {
-    cy.get('[data-cy="cms-homepage-year-dropdown"] > div > div > div').as(
-      "year-dropdown"
-    );
-    cy.get("@year-dropdown").click();
+    cy.get(yearDropdownSelector).click();
 
-    cy.get('[data-cy="cms-homepage-year-dropdown"] > div > div')
+    cy.get(yearDropdownSelector)
       .find(".dropdown-content")
-      .as("listOfYears");
-    cy.get("@listOfYears").children().contains("2021").click();
+      .children()
+      .contains("2021")
+      .click();
 
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
-    cy.get("@reports").click();
+    cy.get(reportsSelector).click();
 
-    cy.get('[data-cy="cms-homepage-filter-submit"]').click();
+    cy.get(filterSubmitSelector).click();
   });
 
   it("Should display the correct reports when filtered by status", () => {
-    cy.get('[data-cy="cms-homepage-status-dropdown"] > div > div > div').as(
-      "status-dropdown"
-    );
-    cy.get("@status-dropdown").click();
+    cy.get(statusDropdownSelector).click();
 
-    cy.get('[data-cy="cms-homepage-status-dropdown"] > div > div')
+    cy.get(statusDropdownSelector)
       .find(".dropdown-content")
-      .as("listOfStatuses");
-    cy.get("@listOfStatuses").children().contains("In Progress").click();
+      .children()
+      .contains("In Progress")
+      .click();
 
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
-    cy.get("@reports").click();
+    cy.get(reportsSelector).click();
 
-    cy.get('[data-cy="cms-homepage-filter-submit"]').click();
+    cy.get(filterSubmitSelector).click();
   });
 
   it("Should display the correct reports with multiple filters active", () => {
-    cy.get('[data-cy="cms-homepage-state-dropdown"] > div > div > div').as(
-      "state-dropdown"
-    );
-    cy.get("@state-dropdown").click();
+    cy.get(stateDropdownSelector).click();
 
-    cy.get('[data-cy="cms-homepage-state-dropdown"] > div > div')
+    cy.get(stateDropdownSelector)
       .find(".dropdown-content")
-      .as("listOfStates");
-    cy.get("@listOfStates").children().contains("Alabama").click();
+      .children()
+      .contains("Alabama")
+      .click();
 
-    cy.get('[data-cy="cms-homepage-status-dropdown"] > div > div > div').as(
-      "status-dropdown"
-    );
-    cy.get("@status-dropdown").click();
+    cy.get(statusDropdownSelector).click();
 
-    cy.get('[data-cy="cms-homepage-status-dropdown"] > div > div')
+    cy.get(statusDropdownSelector)
       .find(".dropdown-content")
-      .as("listOfStatuses");
-    cy.get("@listOfStatuses").children().contains("In Progress").click();
+      .children()
+      .contains("In Progress")
+      .click();
 
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
-    cy.get("@reports").click();
+    cy.get(reportsSelector).click();
 
-    cy.get('[data-cy="cms-homepage-filter-submit"]').click();
+    cy.get(filterSubmitSelector).click();
   });
 
   it("Should display no results when filters do not match reports", () => {
-    cy.get('[data-cy="cms-homepage-status-dropdown"] > div > div > div').as(
-      "status-dropdown"
-    );
-    cy.get("@status-dropdown").click();
+    cy.get(statusDropdownSelector).click();
 
-    cy.get('[data-cy="cms-homepage-status-dropdown"] > div > div')
+    cy.get(statusDropdownSelector)
       .find(".dropdown-content")
-      .as("listOfStatuses");
-    cy.get("@listOfStatuses").children().contains("Not Started").click();
+      .children()
+      .contains("Not Started")
+      .click();
 
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
-    cy.get("@reports").click();
+    cy.get(reportsSelector).click();
 
-    cy.get('[data-cy="cms-homepage-filter-submit"]').click();
+    cy.get(filterSubmitSelector).click();
   });
 
   it("Should display all reports after clearing filters", () => {
-    cy.get('[data-cy="cms-homepage-filter-clear"]').click();
-    cy.get('[data-cy="cms-homepage-reports"]').as("reports");
+    cy.get(clearFilterSelector).click();
+    cy.get(reportsSelector).as("reports");
   });
 });
