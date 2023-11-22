d19fc62aa0fbb2ed3f552bebb8be91509ba90803
benmartin-coforma <Ben.Martin@coforma.io>
2023-11-21 18:02:06 -0700

    Upgrade Cognito to SDKv3 in ui-auth

--- services/ui-auth/libs/cognito-lib.js
+++ services/ui-auth/libs/cognito-lib.js
@@ -1,53 +1,23 @@
-/* eslint-disable no-unused-vars */
-var aws = require("aws-sdk");
-const COGNITO_CLIENT = new aws.CognitoIdentityServiceProvider({
+const {
+  CognitoIdentityProviderClient,
+  AdminCreateUserCommand,
+  AdminSetUserPasswordCommand,
+  AdminUpdateUserAttributesCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
+
+const COGNITO_CLIENT = new CognitoIdentityProviderClient({
   apiVersion: "2016-04-19",
   region: "us-east-1",
 });
 
 export async function createUser(params) {
-  await new Promise((resolve, _reject) => {
-    COGNITO_CLIENT.adminCreateUser(params, function (err, data) {
-      var response;
-      if (err) {
-        response = { statusCode: 500, body: { message: "FAILED", error: err } };
-        resolve(response); //if user already exists, we still continue and ignore
-      } else {
-        response = { statusCode: 200, body: { message: "SUCCESS" } };
-        resolve(response);
-      }
-    });
-  });
+  await COGNITO_CLIENT.send(new AdminCreateUserCommand(params));
 }
 
 export async function setPassword(params) {
-  await new Promise((resolve, reject) => {
-    COGNITO_CLIENT.adminSetUserPassword(params, function (err, data) {
-      if (err) {
-        var response = {
-          statusCode: 500,
-          body: { message: "FAILED", error: err },
-        };
-        reject(response);
-      } else {
-        resolve();
-      }
-    });
-  });
+  await COGNITO_CLIENT.send(new AdminSetUserPasswordCommand(params));
 }
 
 export async function updateUserAttributes(params) {
-  await new Promise((resolve, reject) => {
-    COGNITO_CLIENT.adminUpdateUserAttributes(params, function (err, data) {
-      if (err) {
-        var response = {
-          statusCode: 500,
-          body: { message: "FAILED", error: err },
-        };
-        reject(response);
-      } else {
-        resolve();
-      }
-    });
-  });
+  await COGNITO_CLIENT.send(new AdminUpdateUserAttributesCommand(params));
 }
