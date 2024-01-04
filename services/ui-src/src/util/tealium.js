fd1bdda7e3203016bd711831b49fd68a4119bd24
Garrett Rabian <garrett@coforma.io>
2024-01-04 15:28:02 -0500

    setup tealium in carts

---
+++ services/ui-src/src/util/tealium.js
@@ -0,0 +1,18 @@
+export const fireTealiumPageView = (user, url, pathname) => {
+  const isReportPage = pathname.includes("sections");
+  const contentType = isReportPage ? "form" : "app";
+  const sectionName = isReportPage ? pathname.split("/")[1] : "main app";
+  const { host: siteDomain } = url ? new URL(url) : null;
+  if (window.utag) {
+    window.utag.view({
+      content_language: "en",
+      content_type: contentType,
+      page_name: sectionName + ":" + pathname,
+      page_path: pathname,
+      site_domain: siteDomain,
+      site_environment: process.env.NODE_ENV,
+      site_section: sectionName,
+      logged_in: !!user,
+    });
+  }
+};
