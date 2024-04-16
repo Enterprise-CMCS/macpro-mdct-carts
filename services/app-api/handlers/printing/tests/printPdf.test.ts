3b51e719bee98808333a77ba7011dfafa70d5ceb
Garrett Rabian <garrett.rabian@coforma.io>
2024-04-16 10:25:35 -0400

    prettier

--- services/app-api/handlers/printing/tests/printPdf.test.ts
+++ services/app-api/handlers/printing/tests/printPdf.test.ts
@@ -6,17 +6,17 @@ jest.spyOn(console, "error").mockImplementation();
 jest.spyOn(console, "warn").mockImplementation();
 global.fetch = jest.fn().mockImplementationOnce(() => {
   return new Promise((resolve) => {
-   resolve({
-    status: 200,
-    headers: {
-      get: jest.fn().mockResolvedValue("3"),
-    },
-    arrayBuffer: jest.fn().mockResolvedValue(
-      // An ArrayBuffer containing `%PDF-1.7`
-      new Uint8Array([37, 80, 68, 70, 45, 49, 46, 55]).buffer
-    ),
+    resolve({
+      status: 200,
+      headers: {
+        get: jest.fn().mockResolvedValue("3"),
+      },
+      arrayBuffer: jest.fn().mockResolvedValue(
+        // An ArrayBuffer containing `%PDF-1.7`
+        new Uint8Array([37, 80, 68, 70, 45, 49, 46, 55]).buffer
+      ),
+    });
   });
- });
 });
 
 jest.mock("../../../libs/authorization", () => ({
