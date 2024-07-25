394ea39a9dccb42b2f210bce18ebf1c92b93bc28
Nicholas Summers <ntsummers1@proton.me>
2024-07-25 10:36:22 -0400

    swap let to const

--- services/ui-src/src/components/utils/ScrollToTop.js
+++ services/ui-src/src/components/utils/ScrollToTop.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 function ScrollToTop() {
-  let history = useHistory();
+  const history = useHistory();
   useEffect(() => {
     const unlisten = history.listen(() => {
       window.scrollTo(0, 0);
