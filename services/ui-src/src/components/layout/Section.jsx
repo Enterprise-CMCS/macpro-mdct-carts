87f64d2bca60a44649838ec8f8452d017dbe51bd
Nick Summers <ntsummers1@proton.me>
2024-07-31 13:24:19 -0400

    Migrate Section Component (#139709)

--- services/ui-src/src/components/layout/Section.jsx
+++ services/ui-src/src/components/layout/Section.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
 import PageInfo from "./PageInfo";
 import { selectSectionTitle } from "../../store/selectors";
 import Subsection from "./Subsection";
@@ -8,12 +8,10 @@ import FormNavigation from "./FormNavigation";
 import FormActions from "./FormActions";
 import Autosave from "../fields/Autosave";
 
-// Get section number only from sectionId
-const selectSectionNumber = (sectionId) => {
-  return Number(sectionId.split("-")[1]);
-};
+const Section = ({ subsectionId, sectionId }) => {
+  const formData = useSelector((state) => state.formData);
+  const title = selectSectionTitle(formData, sectionId);
 
-const Section = ({ subsectionId, title }) => {
   return (
     <div className="section-basic-info ds-l-col--9 content">
       <main id="main-content" className="main">
@@ -31,16 +29,7 @@ const Section = ({ subsectionId, title }) => {
 };
 Section.propTypes = {
   subsectionId: PropTypes.string.isRequired,
-  title: PropTypes.string,
   sectionId: PropTypes.number.isRequired,
 };
 
-const mapStateToProps = (state, { sectionId, subsectionId }) => {
-  return {
-    subsectionId,
-    title: selectSectionTitle(state, sectionId),
-    sectionId: selectSectionNumber(sectionId),
-  };
-};
-
-export default connect(mapStateToProps)(Section);
+export default Section;
