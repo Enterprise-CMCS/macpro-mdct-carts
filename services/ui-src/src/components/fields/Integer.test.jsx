8526a9e6f379159dba7e70f6a42b971f8e40fbe7
Garrett Rabian <garrett.rabian@coforma.io>
2024-09-09 11:52:13 -0400

    add tests

--- services/ui-src/src/components/fields/Integer.test.jsx
+++ services/ui-src/src/components/fields/Integer.test.jsx
@@ -6,7 +6,40 @@ import Integer from "./Integer";
 import { screen, render, fireEvent } from "@testing-library/react";
 
 const mockStore = configureMockStore();
-const store = mockStore({ lastYearTotals: { 2022: [] } });
+const lastYearFormData = [
+  {},
+  {},
+  {},
+  {
+    contents: {
+      section: {
+        subsections: [
+          {},
+          {},
+          {
+            parts: [
+              {},
+              {},
+              {},
+              {},
+              {
+                questions: [
+                  {
+                    fieldset_info: {
+                      id: "2022-03-c-05-03",
+                    },
+                    questions: [{ answer: { entry: 3000 } }],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
+];
+const store = mockStore({ lastYearTotals: { 2022: [] }, lastYearFormData });
 const buildInteger = (intProps) => {
   return (
     <Provider store={store}>
@@ -72,5 +105,19 @@ describe("<Integer />", () => {
     fireEvent.change(input, { target: { value: "raw text" } });
     expect(screen.queryByDisplayValue("raw text")).not.toBeInTheDocument();
     expect(screen.getByRole("alert")).toBeInTheDocument();
+  });
+
+  it("should render previous year value for appropriate 3c part 5 or 6 questions", () => {
+    const props = {
+      question: {
+        id: "2023-03-c-05-03-a",
+        label: "How much?",
+        answer: { entry: null },
+      },
+    };
+
+    render(buildInteger(props));
+
+    expect(screen.getByDisplayValue("3000")).toBeInTheDocument();
   });
 });
