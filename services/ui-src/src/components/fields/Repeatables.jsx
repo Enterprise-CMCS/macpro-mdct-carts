0dd1f6bef379aa366a0aeded8bb5f49fdc5886ac
Nick Summers <ntsummers1@proton.me>
2024-09-24 15:25:56 -0400

    Revert "Rollback to Arch Diagram (#139777)" (#139779)

--- services/ui-src/src/components/fields/Repeatables.jsx
+++ services/ui-src/src/components/fields/Repeatables.jsx
@@ -17,6 +17,7 @@ const Repeatables = ({
   question,
   removeRepeatableFrom,
   type,
+  printView,
 }) => {
   const ref = useRef();
 
@@ -62,6 +63,7 @@ const Repeatables = ({
               number={i + 1}
               question={q}
               type={question.typeLabel ? question.typeLabel : type}
+              printView={printView}
             />
           </AccordionItem>
         ))}
@@ -107,6 +109,7 @@ Repeatables.propTypes = {
   question: PropTypes.object.isRequired,
   removeRepeatableFrom: PropTypes.func.isRequired,
   type: PropTypes.oneOf([PropTypes.string, null]),
+  printView: PropTypes.bool,
 };
 Repeatables.defaultProps = {
   type: null,
