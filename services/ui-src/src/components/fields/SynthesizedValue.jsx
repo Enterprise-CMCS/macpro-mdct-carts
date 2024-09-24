b1e1bb9a7ffbab2cf160dcd7e42b4f6f29e57a0c
Nicholas Summers <ntsummers1@proton.me>
2024-09-24 12:37:06 -0400

    Make it so synthesized values follow lessthaneleven mask

--- services/ui-src/src/components/fields/SynthesizedValue.jsx
+++ services/ui-src/src/components/fields/SynthesizedValue.jsx
@@ -7,7 +7,7 @@ import synthesizeValue from "../../util/synthesize";
 //types
 import PropTypes from "prop-types";
 
-const SynthesizedValue = ({ question, ...props }) => {
+const SynthesizedValue = ({ question, printView, ...props }) => {
   const [allStatesData, stateName, stateUserAbbr, chipEnrollments, formData] =
     useSelector(
       (state) => [
@@ -20,23 +20,33 @@ const SynthesizedValue = ({ question, ...props }) => {
       shallowEqual
     );
 
-  const value = synthesizeValue(
-    question.fieldset_info,
-    allStatesData,
-    stateName,
-    stateUserAbbr,
-    chipEnrollments,
-    formData
-  ).contents;
+  const showValue = !(
+    printView && question.fieldset_info.mask === "lessThanEleven"
+  );
+
+  const renderValue = () => {
+    if (showValue) {
+      return synthesizeValue(
+        question.fieldset_info,
+        allStatesData,
+        stateName,
+        stateUserAbbr,
+        chipEnrollments,
+        formData
+      ).contents;
+    }
+  };
 
   return (
-    <div>
-      <strong>Computed:</strong> {value}
-      {question.questions &&
-        question.questions.map((q) => (
-          <Question key={q.id} question={q} {...props} />
-        ))}
-    </div>
+    showValue && (
+      <div>
+        <strong>Computed:</strong> {renderValue}
+        {question.questions &&
+          question.questions.map((q) => (
+            <Question key={q.id} question={q} {...props} />
+          ))}
+      </div>
+    )
   );
 };
 SynthesizedValue.propTypes = {
