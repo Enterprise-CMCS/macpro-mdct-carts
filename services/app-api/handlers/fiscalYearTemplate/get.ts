8156958fb62f16cd00f5a484dfcfa77c2115dc5a
Bangbay Siboliban <bangbay@bluetiger.digital>
2024-10-09 12:43:05 -0400

    Add year to getTemplate call

--- services/app-api/handlers/fiscalYearTemplate/get.ts
+++ services/app-api/handlers/fiscalYearTemplate/get.ts
@@ -2,8 +2,15 @@ import handler from "../../libs/handler-lib";
 import s3 from "../../libs/s3-lib";
 import { ReportPdfs } from "../../types";
 
-export const getFiscalYearTemplateLink = handler(async (_context) => {
-  const filename = ReportPdfs[2024];
+export const getFiscalYearTemplateLink = handler(async (event, _context) => {
+  if (!event.pathParameters) throw new Error("No Path Parameters Object");
+  if (!event.pathParameters.year) {
+    throw new Error("Be sure to include year in the path");
+  }
+
+  const { year } = event.pathParameters;
+
+  const filename = ReportPdfs[year];
   const url = await s3.getSignedDownloadUrl(
     {
       Bucket:
