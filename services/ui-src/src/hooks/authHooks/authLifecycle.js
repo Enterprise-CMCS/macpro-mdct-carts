5011085799f0a71477823afe189bc6bc333d51f6
Nick Summers <ntsummers1@proton.me>
2024-11-19 11:14:04 -0500

    Revert "Ticket CS2371782: Add global param to signout (#139825)" (#139827)

--- services/ui-src/src/hooks/authHooks/authLifecycle.js
+++ services/ui-src/src/hooks/authHooks/authLifecycle.js
@@ -28,7 +28,7 @@ class AuthManager {
     const isExpired = expiration && new Date(expiration).valueOf() < Date.now();
     if (isExpired) {
       localStorage.removeItem("mdctcarts_session_exp");
-      signOut({ global: true }).then(() => {
+      signOut().then(() => {
         window.location.href = "/";
       });
     }
@@ -76,7 +76,7 @@ class AuthManager {
         this.promptTimeout(exp);
         this.timeoutForceId = setTimeout(() => {
           localStorage.removeItem("mdctcarts_session_exp");
-          signOut({ global: true });
+          signOut();
         }, IDLE_WINDOW - PROMPT_AT);
       },
       PROMPT_AT,
