0623f1c6589a369d36d6f2be5a1d119f6e0b3839
Nicholas Summers <ntsummers1@proton.me>
2024-11-25 11:34:39 -0500

    Amplify Fix tpye is string array

--- services/ui-src/src/index.jsx
+++ services/ui-src/src/index.jsx
@@ -37,12 +37,19 @@ Amplify.configure({
       userPoolId: config.cognito.USER_POOL_ID,
       identityPoolId: config.cognito.IDENTITY_POOL_ID,
       userPoolClientId: config.cognito.APP_CLIENT_ID,
-      oauth: {
-        domain: config.cognito.APP_CLIENT_DOMAIN,
-        redirectSignIn: config.cognito.REDIRECT_SIGNIN,
-        redirectSignOut: config.cognito.REDIRECT_SIGNOUT,
-        scopes: ["email", "openid", "profile", "aws.cognito.signin.user.admin"],
-        responseType: "code",
+      loginWith: {
+        oauth: {
+          domain: config.cognito.APP_CLIENT_DOMAIN,
+          redirectSignIn: [config.cognito.REDIRECT_SIGNIN],
+          redirectSignOut: [config.cognito.REDIRECT_SIGNOUT],
+          scopes: [
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin",
+          ],
+          responseType: "code",
+        },
       },
     },
   },
