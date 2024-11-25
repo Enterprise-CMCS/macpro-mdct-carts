0baa66596b71f6a399992b4fb27ee603f98766a0
Nick Summers <ntsummers1@proton.me>
2024-11-25 12:32:30 -0500

    Bring back amp to v6 now that we know where to look (#139839)

--- services/ui-src/src/util/apiLib.js
+++ services/ui-src/src/util/apiLib.js
@@ -1,4 +1,6 @@
-import { API } from "aws-amplify";
+/* eslint-disable no-console */
+
+import { get, put, post, del } from "aws-amplify/api";
 import { updateTimeout } from "../hooks/authHooks";
 
 /**
@@ -6,26 +8,26 @@ import { updateTimeout } from "../hooks/authHooks";
  * Below we just key off of these API calls as our source of user activity to make sure
  * credentials don't expire.
  */
-const post = (api, uri, opts) => {
-  updateTimeout();
-  return API.post(api, uri, opts);
-};
-const put = (api, uri, opts) => {
-  updateTimeout();
-  return API.put(api, uri, opts);
-};
-const get = (api, uri, opts) => {
-  updateTimeout();
-  return API.get(api, uri, opts);
-};
-const del = (api, uri, opts) => {
-  updateTimeout();
-  return API.del(api, uri, opts);
+const apiRequest = async (request, path, options) => {
+  try {
+    // swallow request error codes for aws lib, they'll kill the lib
+    await updateTimeout();
+    const { body } = await request({ apiName: "carts-api", path, options })
+      .response;
+    const res = await body.text(); // body.json() dies on an empty response, spectacularly
+    return res && res.length > 0 ? JSON.parse(res) : null;
+  } catch (e) {
+    // Return our own error for handling in the app
+    const info = `Request Failed - ${path} - ${e.response?.body}`;
+    console.log(e);
+    console.log(info);
+    throw new Error(info);
+  }
 };
 
 export const apiLib = {
-  post,
-  put,
-  get,
-  del,
+  post: async (path, options) => await apiRequest(post, path, options),
+  put: async (path, options) => await apiRequest(put, path, options),
+  get: async (path, options) => await apiRequest(get, path, options),
+  del: async (path, options) => await apiRequest(del, path, options),
 };
