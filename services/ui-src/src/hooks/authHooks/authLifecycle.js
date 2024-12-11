06dd1aaf7f14693a8ca8b1f93cac6230883347f2
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2024-12-11 10:24:28 -0500

    Amplify debugging: Remove force refresh flag (#139853)

--- services/ui-src/src/hooks/authHooks/authLifecycle.js
+++ services/ui-src/src/hooks/authHooks/authLifecycle.js
@@ -57,7 +57,7 @@ class AuthManager {
    * Manual refresh of credentials paired with an instant timer clear
    */
   async refreshCredentials() {
-    await fetchAuthSession({ forceRefresh: true }); // Force a token refresh
+    await fetchAuthSession(); // Look for an active session (indirectly refreshes?)
     this.setTimer();
   }
 
