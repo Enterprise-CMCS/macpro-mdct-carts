ea6c34961113aa1b1dbf2898f38e928c4929583a
Pete Dunlap <pete.dunlap@stratiform.digital>
2025-02-10 09:49:49 -0500

    CMDCT-4324 - gets run local not needing credentials

--- src/run.ts
+++ src/run.ts
@@ -200,9 +200,7 @@ async function destroy_stage(options: {
  * All valid arguments to dev should be enumerated here, this is the entrypoint to the script
  */
 yargs(process.argv.slice(2))
-  .command("local", "run system locally", {}, () => {
-    run_all_locally();
-  })
+  .command("local", "run system locally", {}, run_all_locally)
   .command("test", "run all tests", () => {})
   .command(
     "deploy",
