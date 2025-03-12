5b76aab2607956761185117a931820ff9f442caf
Nicholas Summers <ntsummers1@proton.me>
2025-03-12 16:47:13 -0400

    Remove routes that go nowhere on CMS homepage

--- services/ui-src/src/components/layout/HomeCMS.jsx
+++ services/ui-src/src/components/layout/HomeCMS.jsx
@@ -5,7 +5,6 @@ import InvokeSection from "../utils/InvokeSection";
 import SaveError from "./SaveError";
 import ScrollToTop from "../utils/ScrollToTop";
 import Sidebar from "./Sidebar";
-import Unauthorized from "./Unauthorized";
 
 const Section = () => (
   <>
@@ -28,11 +27,6 @@ const CMSHome = () => (
         path="/views/sections/:state/:year/:sectionOrdinal"
         element={<Section />}
       />
-      {/* Add routes from admin that should be unauthorized for cms users */}
-      <Route path={"/role_user_assoc"} element={Unauthorized} />
-      <Route path={"/state_assoc"} element={Unauthorized} />
-      <Route path={"/role_jobcode_assoc"} element={Unauthorized} />
-      <Route path={"/users"} element={Unauthorized} />
     </Routes>
   </>
 );
