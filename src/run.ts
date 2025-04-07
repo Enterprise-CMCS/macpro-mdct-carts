de22d572669b474ad473b6ea74c71328da7582a3
Garrett Rabian <garrett.rabian@coforma.io>
2025-04-07 11:00:48 -0400

    deploy kafka in branch

--- src/run.ts
+++ src/run.ts
@@ -163,9 +163,7 @@ async function deploy(options: { stage: string }) {
   await runner.run_command_and_output("SLS Deploy", deployCmd, ".");
   await addSlsBucketPolicies();
   // Only deploy resources for kafka ingestion in real envs
-  if (stage === "main" || stage === "val" || stage === "production") {
-    await deploy_kafka_service(runner, stage);
-  }
+  await deploy_kafka_service(runner, stage);
 }
 
 async function destroy_stage(options: {
