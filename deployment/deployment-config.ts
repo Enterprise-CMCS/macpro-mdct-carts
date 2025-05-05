3df8ef8010de090569540355884b4144b6c0e59c
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-05-05 16:12:59 -0400

    Update deployment-config.ts

--- deployment/deployment-config.ts
+++ deployment/deployment-config.ts
@@ -23,8 +23,7 @@ export interface DeploymentConfigProperties {
 
 export const determineDeploymentConfig = async (stage: string) => {
   const project = process.env.PROJECT!;
-  const isDev =
-    isLocalStack || !["main", "val", "production", "jon-cdk"].includes(stage); // TODO: remove jon-cdk after main is deployed
+  const isDev = isLocalStack || !["main", "val", "production"].includes(stage);
   const secretConfigOptions = {
     ...(await loadDefaultSecret(project, stage)),
     ...(await loadStageSecret(project, stage)),
