d67f0755e0b7b0ac2fc19fb8c44054d7475be0ff
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-05-06 14:01:47 -0400

    Update ui.ts

--- deployment/stacks/imports_included/ui.ts
+++ deployment/stacks/imports_included/ui.ts
@@ -10,18 +10,20 @@ import {
 interface CreateUiComponentsProps {
   scope: Construct;
   stage: string;
+  isDev: boolean;
 }
 
 export function createUiComponents(props: CreateUiComponentsProps) {
-  const { scope, stage } = props;
+  const { scope, stage, isDev } = props;
 
   const logBucket = new s3.Bucket(scope, "CloudfrontLogBucket", {
     bucketName: `ui-${stage}-cloudfront-logs-${Aws.ACCOUNT_ID}`,
+    autoDeleteObjects: isDev,
     encryption: s3.BucketEncryption.S3_MANAGED,
     publicReadAccess: false,
     blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_PREFERRED,
-    removalPolicy: RemovalPolicy.RETAIN,
+    removalPolicy: isDev ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
     versioned: true,
   });
 
