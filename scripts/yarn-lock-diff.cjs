fb08301b2187bd9b54c080d840da14505bf69989
Bangbay Siboliban <bangbay@bluetiger.digital>
2025-06-09 10:28:51 -0700

    Install deps

--- scripts/yarn-lock-diff.cjs
+++ scripts/yarn-lock-diff.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 /*
  * Usage:
- *   node yarn-lock-diff.cjs \
+ *   node ./scripts/yarn-lock-diff.cjs \
  *     yarn.lock.new \
  *     yarn.lock.old \
  *     package.json.new \
