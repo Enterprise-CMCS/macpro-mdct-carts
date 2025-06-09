5070a0509b9e4d2be85b06ec2142ce64e99de61f
Bangbay Siboliban <bangbay@bluetiger.digital>
2025-06-09 09:46:40 -0700

    Add node path

--- .github/commit-log.sh
+++ .github/commit-log.sh
@@ -90,7 +90,7 @@ for FILE in $YARN_LOCK_FILES; do
 
   # Run diff script
   DIFF_OUTPUT=$(
-    node ./tests/scripts/yarn-lock-diff.cjs \
+    NODE_PATH=$(pwd)/node_modules node ./tests/scripts/yarn-lock-diff.cjs \
       "$TEMP_DIR/yarn.lock.new" \
       "$TEMP_DIR/yarn.lock.old" \
       "$TEMP_DIR/package.json.new" \
