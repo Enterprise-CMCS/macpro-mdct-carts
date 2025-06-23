de8712224677ce8a642cc95c644a484b470ab9a1
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-06-23 16:14:13 -0400

    Cleaning up these consts that are now not used and the other only used in 1 place

--- services/ui-src/src/store/fiscalYearTemplate.js
+++ services/ui-src/src/store/fiscalYearTemplate.js
@@ -1,10 +1,8 @@
-import { GET_TEMPLATE_SUCCESS } from "../actions/download";
-
 const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case GET_TEMPLATE_SUCCESS:
+    case "GET_TEMPLATE_SUCCESS":
       return action.data;
     default:
       return state;
