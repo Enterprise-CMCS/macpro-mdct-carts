8a309bb9ef10b739ab11270fa9129c68d0450a9a
brittbujacich <britt.bujacich@gmail.com>
2025-07-02 08:17:40 -0600

    add uuid

--- services/ui-src/src/components/layout/Part.jsx
+++ services/ui-src/src/components/layout/Part.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import { shallowEqual, useSelector } from "react-redux";
 import { Alert } from "@cmsgov/design-system";
 // components
@@ -61,9 +62,9 @@ const Part = ({ partId, partNumber, nestedSubsectionTitle, printView }) => {
       return (
         <>
           {text && <Text data-testid="part-text">{text}</Text>}
-          {questions.map((question, index) => (
+          {questions.map((question) => (
             <Question
-              key={question.id || index}
+              key={question.id || uuidv4()}
               question={question}
               tableTitle={title}
               data-testid="part-question"
