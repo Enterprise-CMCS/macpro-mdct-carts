b5d79c591f94d57aa03a21f7ac93540367579a4f
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2025-07-30 14:31:17 -0400

    CMDCT-4908, CMDCT-4909, CMDCT-5010: Add test coverage for repeatable, repeatables, and subsection (#140023)

---
+++ services/ui-src/src/components/layout/Subsection.test.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { mockInitialState } from "../../util/testing/testUtils";
+import Subsection from "./Subsection";
+
+const mockStore = configureMockStore();
+const store = mockStore(mockInitialState);
+
+jest.mock("../../store/selectors", () => ({
+  selectSubsectionTitleAndPartIDs: jest.fn().mockReturnValue({
+    parts: ["mock-partId"],
+    title: "Mock part title",
+    text: "Mock part hint",
+  }),
+}));
+
+const mockPart = jest.fn(() => <div data-testid="part" />);
+jest.mock("./Part", () => (props) => mockPart(props));
+
+describe("Subsection component", () => {
+  test("renders", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Subsection subsectionId={"subsection-1"} />
+      </Provider>
+    );
+    expect(
+      screen.getByRole("heading", { name: "Mock part title" })
+    ).toBeVisible();
+    expect(screen.getByText("Mock part hint")).toBeVisible();
+    expect(mockPart).toHaveBeenCalledWith(
+      expect.objectContaining({
+        nestedSubsectionTitle: true,
+        partId: "mock-partId",
+        partNumber: null,
+        printView: undefined,
+      })
+    );
+    const subSectionDivId = container.querySelector("#subsection-1");
+    expect(subSectionDivId).toBeVisible();
+  });
+});
