9c4f7d65fa7d9c17f9daeaa377c82046e17bc88b
Nicholas Summers <ntsummers1@proton.me>
2025-08-06 13:42:54 -0400

    Add tests for Print

--- services/ui-src/src/components/sections/Print.jsx
+++ services/ui-src/src/components/sections/Print.jsx
@@ -13,6 +13,62 @@ import statesArray from "../utils/statesArray";
 import { loadEnrollmentCounts, loadSections } from "../../actions/initial";
 import { apiLib } from "../../util/apiLib";
 import { v4 as uuidv4 } from "uuid";
+import { Buffer } from "buffer";
+
+const openPdf = (basePdf) => {
+  const byteCharacters = Buffer.from(basePdf, "base64").toString("utf8");
+  // const byteCharacters = atob(basePdf);
+  let byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const file = new Blob([byteArray], { type: "application/pdf;base64" });
+  const fileURL = URL.createObjectURL(file);
+  window.open(fileURL);
+};
+
+export const getPdfFriendlyDocument = async () => {
+  // get html element and remove noscript tag
+  const html = document.querySelector("html");
+  html.querySelector("noscript")?.remove();
+
+  document.querySelectorAll("input").forEach((element) => {
+    if (element.type === "text") {
+      element.style.height = "50px";
+    }
+  });
+  document.querySelectorAll("button").forEach((element) => {
+    if (element.title !== "Print") {
+      element.remove();
+    }
+  });
+
+  if (!document.querySelector("base")) {
+    const base = document.createElement("base");
+    base.href = `https://${window.location.host}`;
+    document.querySelector("head").prepend(base);
+  }
+
+  const htmlString = document
+    .querySelector("html")
+    .outerHTML.replaceAll(`’`, `'`)
+    .replaceAll(`‘`, `'`)
+    .replaceAll(`”`, `"`)
+    .replaceAll(`“`, `"`)
+    .replaceAll("\u2013", "-")
+    .replaceAll("\u2014", "-");
+
+  const base64String = Buffer.from(htmlString, "base64").toString("utf8");
+  const opts = {
+    body: {
+      encodedHtml: base64String,
+    },
+  };
+
+  const res = await apiLib.post("/print_pdf", opts);
+  openPdf(res.data);
+};
 
 /**
  * Generate data and load entire form based on user information
@@ -20,7 +76,7 @@ import { v4 as uuidv4 } from "uuid";
  * @returns {JSX.Element}
  * @constructor
  */
-const Print = () => {
+export const Print = () => {
   const dispatch = useDispatch();
   const [formData, currentUser, name] = useSelector(
     (state) => [
@@ -39,59 +95,6 @@ const Print = () => {
   const sectionId = searchParams.get("sectionId");
   const subsectionId = searchParams.get("subsectionId");
 
-  const openPdf = (basePdf) => {
-    const byteCharacters = atob(basePdf);
-    let byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
-      byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
-    const byteArray = new Uint8Array(byteNumbers);
-    const file = new Blob([byteArray], { type: "application/pdf;base64" });
-    const fileURL = URL.createObjectURL(file);
-    window.open(fileURL);
-  };
-
-  const getPdfFriendlyDocument = async () => {
-    // get html element and remove noscript tag
-    const html = document.querySelector("html");
-    html.querySelector("noscript")?.remove();
-
-    document.querySelectorAll("input").forEach((element) => {
-      if (element.type === "text") {
-        element.style.height = "50px";
-      }
-    });
-    document.querySelectorAll("button").forEach((element) => {
-      if (element.title !== "Print") {
-        element.remove();
-      }
-    });
-
-    if (!document.querySelector("base")) {
-      const base = document.createElement("base");
-      base.href = `https://${window.location.host}`;
-      document.querySelector("head").prepend(base);
-    }
-
-    const htmlString = document
-      .querySelector("html")
-      .outerHTML.replaceAll(`’`, `'`)
-      .replaceAll(`‘`, `'`)
-      .replaceAll(`”`, `"`)
-      .replaceAll(`“`, `"`)
-      .replaceAll("\u2013", "-")
-      .replaceAll("\u2014", "-");
-
-    const base64String = btoa(unescape(encodeURIComponent(htmlString)));
-    const opts = {
-      body: {
-        encodedHtml: base64String,
-      },
-    };
-
-    const res = await apiLib.post("/print_pdf", opts);
-    openPdf(res.data);
-  };
   // Load formData via side effect
   useEffect(() => {
     // Create function to call data to prevent return data from useEffect
@@ -205,5 +208,3 @@ const Print = () => {
     </div>
   );
 };
-
-export default Print;
