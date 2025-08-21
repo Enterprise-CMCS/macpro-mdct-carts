188dedc553155bce8e5c69d43cd7ed2c9e66ce21
Garrett Rabian <garrett.rabian@coforma.io>
2025-08-21 14:43:51 -0400

    lint

--- deployment/stacks/uploads.ts
+++ deployment/stacks/uploads.ts
@@ -121,7 +121,7 @@ export function createUploadsComponents(props: CreateUploadsComponentsProps) {
 
     new triggers.Trigger(scope, "AvDownloadDefinitionsTrigger", {
       handler: avDownloadDefinitionsLambda,
-      invocationType: triggers.InvocationType.EVENT
+      invocationType: triggers.InvocationType.EVENT,
     });
 
     new events.Rule(scope, `schedule-av-download-definitions`, {
