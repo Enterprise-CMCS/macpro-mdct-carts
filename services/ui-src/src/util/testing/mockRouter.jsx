3325c0a05b69c879be7954c2d260ad4f842efda0
Garrett Rabian <garrett.rabian@coforma.io>
2025-09-23 12:55:46 -0400

    more cleanup

--- services/ui-src/src/util/testing/mockRouter.jsx
+++ services/ui-src/src/util/testing/mockRouter.jsx
@@ -2,10 +2,5 @@ import React from "react";
 import { MemoryRouter as MemoryRouterOriginal } from "react-router";
 
 export const MemoryRouter = ({ children, ...props }) => (
-  <MemoryRouterOriginal
-    future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-    {...props}
-  >
-    {children}
-  </MemoryRouterOriginal>
+  <MemoryRouterOriginal {...props}>{children}</MemoryRouterOriginal>
 );
