d29fd3048fea26b609e2ad07b5fe5164ea665705
Garrett Rabian <garrett.rabian@coforma.io>
2025-10-17 13:47:44 -0400

    playwright: further simplify home page load

--- tests/playwright/utils/home.ts
+++ tests/playwright/utils/home.ts
@@ -1,10 +1,6 @@
-import { expect, Page } from "@playwright/test";
+import { Page } from "@playwright/test";
 
 export async function stateUserHomePageLoad(page: Page) {
   await page.goto("/");
-  await page.waitForResponse(
-    (response) =>
-      response.url().includes("state_status") && response.status() === 200
-  );
-  expect(page.getByRole("table")).toBeVisible();
+  await page.getByRole("table").waitFor();
 }
