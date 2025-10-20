7863a6e1e42fc790656947d063006e20a93e6343
Garrett Rabian <garrett.rabian@coforma.io>
2025-10-20 13:37:02 -0400

    remove comments

--- tests/playwright/utils/reports.ts
+++ tests/playwright/utils/reports.ts
@@ -2,14 +2,12 @@ import { Page } from "@playwright/test";
 import { LOADING_IMG_ALT_TEXT } from "./constants";
 
 export async function enterFirstReport(page: Page) {
-  // Any concerns this data-testid could be modified or removed?
   const firstEditLink = page
     .locator('[data-testid="report-action-button"]')
     .first();
   await firstEditLink.waitFor({ state: "visible" });
   await firstEditLink.click();
 
-  // I need to run it locally, but pretty sure this would be more stable when waiting for the loader
   const loader = page.getByRole("img", { name: LOADING_IMG_ALT_TEXT });
   if (await loader.isVisible().catch(() => false)) {
     await loader.waitFor({ state: "detached" });
