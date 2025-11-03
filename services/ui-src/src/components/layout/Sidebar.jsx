57d202bd5487f1fd9047bf831fd2ad9f8c5ee66a
Cypress Borg <cypress.borg@coforma.io>
2025-11-03 14:56:16 -0600

    Move sidebar skip and state image into vertical nav

--- services/ui-src/src/components/layout/Sidebar.jsx
+++ services/ui-src/src/components/layout/Sidebar.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import StateHeader from "./StateHeader";
 import TableOfContents from "./TableOfContents";
 
 const Sidebar = () => (
   <div className="sidebar ds-l-col--3">
-    <div className="skip-content">
-      <a href="#main-content">Skip to main content</a>
-    </div>
-    <StateHeader />
     <TableOfContents />
   </div>
 );
