cb8c3ec6d6d1a95a56e7a58eb43aadf5db861964
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-12-24 15:44:37 -0500

    Update utils.ts

--- cli/lib/utils.ts
+++ cli/lib/utils.ts
@@ -68,7 +68,9 @@ const buildUiEnvObject = (
 };
 
 export const runFrontendLocally = async (stage: string) => {
-  const outputs = await getCloudFormationStackOutputValues(`${project}-${stage}`);
+  const outputs = await getCloudFormationStackOutputValues(
+    `${project}-${stage}`
+  );
   const envVars = buildUiEnvObject(stage, outputs);
   await writeLocalUiEnvFile(envVars);
 
