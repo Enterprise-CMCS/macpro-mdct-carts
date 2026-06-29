8b96b24dbdf4c4bbc718897fc8be90c5cbe5818f
ntsummers1 <ntsummers1@proton.me>
2026-06-29 12:22:07 -0400

    Clean up comments

--- services/ui-src/src/components/utils/PageTitle.jsx
+++ services/ui-src/src/components/utils/PageTitle.jsx
@@ -4,8 +4,7 @@ import { shallowEqual, useSelector } from "react-redux";
 import { useUser } from "../../hooks/authHooks";
 import { getPageTitle } from "../../util/pageTitles";
 
-// Sets document.title for every route (WCAG 2.4.2). Mounted once near the app
-// root; forwards raw location/user/Redux values to the resolver in pageTitles.js.
+// Sets document.title for every route. Mounted once near the app root
 export const PageTitle = () => {
   const { pathname, search } = useLocation();
   const { user } = useUser();
@@ -29,7 +28,7 @@ export const PageTitle = () => {
     globalStateName,
     formYear,
   });
-  
+
   useEffect(() => {
     document.title = title;
   }, [title]);
