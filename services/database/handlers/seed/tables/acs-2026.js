b13657f116948f2f93cdcc624a358f221dc4e88a
brittbujacich <britt.bujacich@gmail.com>
2026-08-14 11:52:39 -0600

    2026 annual updates

--- services/database/handlers/seed/tables/acs-2026.js
+++
@@ -1,8 +0,0 @@
-const seed = {
-  name: "ACS 2026",
-  data: require("../../../data/seed/seed-acs-2026.json"),
-  tableNameBuilder: (stage) => `${stage}-acs`,
-  keys: ["stateId", "year"],
-};
-
-module.exports = seed;
